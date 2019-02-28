00ad7bb21b48ede48c3d52e90cc229c804fe56ad
Marsen <thisismysoul@gmail.com>
2019-02-28 18:50:35 +0800

    重構 STEP10.7 Move.Method 到合適的類別

--- src/Marsen.NetCore.Dojo/Refactoring_Improving_The_Design_of_Existing_Code/PriceState/Price.cs
+++ src/Marsen.NetCore.Dojo/Refactoring_Improving_The_Design_of_Existing_Code/PriceState/Price.cs
@@ -3,5 +3,29 @@
     public abstract class Price
     {
         public abstract int getPriceCode();
+
+        public double getCharge(int getDaysRented)
+        {
+            double result = 0;
+            switch (getPriceCode())
+            {
+                case 0: //// Movie.REGULAR:
+                    result += 2;
+                    if (getDaysRented > 2)
+
+                        result += (getDaysRented - 2) * 1.5;
+                    break;
+                case 1: //// Movie.NEW_RELEASE
+                    result += getDaysRented * 3;
+                    break;
+                case 2: //// Movie.CHILDRENS:
+                    result += 1.5;
+                    if (getDaysRented > 3)
+                        result += (getDaysRented - 3) * 1.5;
+                    break;
+            }
+
+            return result;
+        }
     }
 }
