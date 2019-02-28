dcd0617b622235f7b840f1a092eae0e5ccfc1214
marklin <marklin@nine-yi.com>
2019-02-28 13:55:50 +0800

    重構 STEP7 刪除未使用的 temp 變數

--- src/Marsen.NetCore.Dojo/Refactoring_Improving_The_Design_of_Existing_Code/Customer.cs
+++ src/Marsen.NetCore.Dojo/Refactoring_Improving_The_Design_of_Existing_Code/Customer.cs
@@ -37,7 +37,6 @@ namespace Marsen.NetCore.Dojo.Refactoring_Improving_The_Design_of_Existing_Code
             string result = "Rental Record for " + getName() + "\n";
             while (rentals.MoveNext())
             {
-                double thisAmount = 0;
                 Rental each = (Rental) rentals.Current;
 
                 frequentRenterPoints = each.getFrequentRenterPoints();
