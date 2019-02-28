95939d67b3414b1bfc905ccbc233bfef038ea223
marklin <marklin@nine-yi.com>
2019-02-28 18:50:35 +0800

    重構 STEP10.7 Move.Method 到合適的類別

--- src/Marsen.NetCore.Dojo/Refactoring_Improving_The_Design_of_Existing_Code/Movie.cs
+++ src/Marsen.NetCore.Dojo/Refactoring_Improving_The_Design_of_Existing_Code/Movie.cs
@@ -53,26 +53,7 @@ namespace Marsen.NetCore.Dojo.Refactoring_Improving_The_Design_of_Existing_Code
 
         public double getCharge(int getDaysRented)
         {
-            double result = 0;
-            switch (getPriceCode())
-            {
-                case 0: //// Movie.REGULAR:
-                    result += 2;
-                    if (getDaysRented > 2)
-
-                        result += (getDaysRented - 2) * 1.5;
-                    break;
-                case 1: //// Movie.NEW_RELEASE
-                    result += getDaysRented * 3;
-                    break;
-                case 2: //// Movie.CHILDRENS:
-                    result += 1.5;
-                    if (getDaysRented > 3)
-                        result += (getDaysRented - 3) * 1.5;
-                    break;
-            }
-
-            return result;
+            return _price.getCharge(getDaysRented);
         }
 
         public int getFrequentRenterPoints(int getDaysRented)
