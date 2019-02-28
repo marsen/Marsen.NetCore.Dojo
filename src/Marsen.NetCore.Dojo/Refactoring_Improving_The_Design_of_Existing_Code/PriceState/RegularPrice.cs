d602f956baac6630e308764715788f9dd4dd3176
Marsen <thisismysoul@gmail.com>
2019-02-28 20:02:09 +0800

    重構 STEP10.8.1 重構分支 將switch case 的語法移至實作類別
    c# 要記得使用 override 關鍵字 (父類要記得加上 virtual 關鍵字 )
    這裡需要一些變異測試的技巧，透過修改 override 方法取得紅燈確認方法有被正確呼叫

--- src/Marsen.NetCore.Dojo/Refactoring_Improving_The_Design_of_Existing_Code/PriceState/RegularPrice.cs
+++ src/Marsen.NetCore.Dojo/Refactoring_Improving_The_Design_of_Existing_Code/PriceState/RegularPrice.cs
@@ -6,5 +6,15 @@
         {
             return Movie.REGULAR;
         }
+
+        public override double getCharge(int daysRented)
+        {
+            double result = 0;
+            result += 2;
+            if (daysRented > 2)
+
+                result += (daysRented - 2) * 1.5;
+            return result;
+        }
     }
 }
