14550b9bf24c5c677fbe474622ab2bebaeeefae7
Marsen <thisismysoul@gmail.com>
2019-02-28 15:53:41 +0800

    重構 STEP9.1 刪除未使用的程式

--- src/Marsen.NetCore.Dojo/Refactoring_Improving_The_Design_of_Existing_Code/Rental.cs
+++ src/Marsen.NetCore.Dojo/Refactoring_Improving_The_Design_of_Existing_Code/Rental.cs
@@ -28,26 +28,6 @@ namespace Marsen.NetCore.Dojo.Refactoring_Improving_The_Design_of_Existing_Code
         public double getCharge()
         {
             return getMovie().getCharge(getDaysRented());
-            double result = 0;
-            switch (getMovie().getPriceCode())
-            {
-                case 0: //// Movie.REGULAR:
-                    result += 2;
-                    if (getDaysRented() > 2)
-
-                        result += (getDaysRented() - 2) * 1.5;
-                    break;
-                case 1: //// Movie.NEW_RELEASE
-                    result += getDaysRented() * 3;
-                    break;
-                case 2: //// Movie.CHILDRENS:
-                    result += 1.5;
-                    if (getDaysRented() > 3)
-                        result += (getDaysRented() - 3) * 1.5;
-                    break;
-            }
-
-            return result;
         }
 
         public int getFrequentRenterPoints()
