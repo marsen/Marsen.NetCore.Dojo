c8e2287125eb358b125b576a575645db552f8eb9
marklin <marklin@nine-yi.com>
2019-02-28 13:25:50 +0800

    重構 STEP4.1 刪除未使用到的 temp 參數

--- src/Marsen.NetCore.Dojo/Refactoring_Improving_The_Design_of_Existing_Code/Customer.cs
+++ src/Marsen.NetCore.Dojo/Refactoring_Improving_The_Design_of_Existing_Code/Customer.cs
@@ -39,8 +39,6 @@ namespace Marsen.NetCore.Dojo.Refactoring_Improving_The_Design_of_Existing_Code
             {
                 double thisAmount = 0;
                 Rental each = (Rental) rentals.Current;
-                //determine amounts for each line
-                thisAmount = each.getCharge();
 
                 // add frequent renter points
                 frequentRenterPoints++;
