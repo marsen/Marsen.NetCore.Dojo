6c96e7cf474980b5f1e0babab21e9148aae3fe4e
marklin <marklin@nine-yi.com>
2019-03-12 00:09:30 +0800

    1 is 1 pass

---
+++ src/Marsen.NetCore.Dojo/Kata_FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,10 @@
+﻿namespace Marsen.NetCore.Dojo.Kata_FizzBuzz
+{
+    public class FizzBuzz
+    {
+        public string Get(int input)
+        {
+            return "1";
+        }
+    }
+}
---
+++ test/Marsen.NetCore.Dojo.Tests/Kata_FizzBuzz/FizzBuzzTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Marsen.NetCore.Dojo.Kata_FizzBuzz;
+using Xunit;
+
+namespace Marsen.NetCore.Dojo.Tests.Kata_FizzBuzz
+{
+    public class FizzBuzzTests
+    {
+        [Fact]
+        public void one_is_1()
+        {
+            FizzBuzz fizzBuzz = new FizzBuzz();
+            var actual = fizzBuzz.Get(1);
+            Assert.Equal("1", actual);
+        }
+    }
+}
