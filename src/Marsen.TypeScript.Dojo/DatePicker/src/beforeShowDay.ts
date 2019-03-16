78ab6eb16537a17585a1a116a7f2728ffc1ddfe3
marklin <marklin@nine-yi.com>
2019-03-17 03:58:09 +0800

    TDD TypeScript pass

--- src/Marsen.TypeScript.Dojo/DatePicker/src/beforeShowDay.ts
+++ src/Marsen.TypeScript.Dojo/DatePicker/src/beforeShowDay.ts
@@ -4,8 +4,11 @@ export default class beforeShowDay {
     static IsShow(date: Date):Array<Boolean> {
         var day = date.getDay();
         var today = this.GetToday();
-        //// console.log('today is '+today);
+        console.log('today is '+today);
         
+        if(today.getDay()==5){
+            return [false];
+        }
         if(today.getHours() >= 12 &&  date.getDate() - today.getDate() < 2 ){
             return [false];
         }
