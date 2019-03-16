70046d8d161586bd4501f999825c4ee24b99664c
Marsen <thisismysoul@gmail.com>
2019-03-17 03:58:09 +0800

    TDD TypeScript pass

--- src/Marsen.TypeScript.Dojo/DatePicker/tests/beforeShowDay.test.ts
+++ src/Marsen.TypeScript.Dojo/DatePicker/tests/beforeShowDay.test.ts
@@ -1,9 +1,52 @@
 import { expect } from 'chai';
 import D from '../src/beforeShowDay';
-import mockDate from '../src/helper/dateHelper';
 
+describe('今天是 2019/3/22 號星期五 12:59', function() {
+  const originalDateNow = Date.now;
 
-describe('今天是 2019/3/18 號星期一上午12:00', function() {
+  beforeEach(function () {
+      Date.now = function(){
+        return new Date('2019-3-22 12:59').valueOf();
+      };
+  });
+
+  it('卡尼想選 2019/3/22 星期五 出貨 不能選,因為現在時間超過 12 點', function() {
+    let result = D.IsShow(new Date('2019-3-22'))[0];
+    expect(result).equal(false,'不能選,因為現在超過12點');
+  });
+
+  it('卡尼想選 2019/3/25 星期一 出貨 不能選,因為現在時間超過 12 點', function() {
+    let result = D.IsShow(new Date('2019-3-25'))[0];
+    expect(result).equal(false,'不能選,因為現在超過12點');
+  });
+
+  afterEach(function () {
+      Date.now = originalDateNow;
+  });
+  
+});
+
+describe('今天是 2019/3/21 號星期四 23:59', function() {
+  const originalDateNow = Date.now;
+
+  beforeEach(function () {
+      Date.now = function(){
+        return new Date('2019-3-21 23:59').valueOf();
+      };
+  });
+
+  it('卡尼想選 2019/3/21 星期四 出貨 不能選,因為現在時間超過 12 點', function() {
+    let result = D.IsShow(new Date('2019-3-21'))[0];
+    expect(result).equal(false,'不能選,因為現在是12點');
+  });
+
+  afterEach(function () {
+      Date.now = originalDateNow;
+  });
+  
+});
+
+describe('今天是 2019/3/18 號星期一12:00', function() {
   const originalDateNow = Date.now;
 
   beforeEach(function () {
@@ -12,14 +55,14 @@ describe('今天是 2019/3/18 號星期一上午12:00', function() {
       };
   });
 
-  it('卡尼想選 2019/3/18 星期一 出貨 不能選,因為現在是上午12點', function() {
+  it('卡尼想選 2019/3/18 星期一 出貨 不能選,因為現在是12點', function() {
     let result = D.IsShow(new Date('2019-3-18'))[0];
-    expect(result).equal(false,'不能選,因為現在是上午12點');
+    expect(result).equal(false,'不能選,因為現在是12點');
   });
   
-  it('卡尼想選 2019/3/19 星期二 出貨 不能選,因為現在是上午12點', function() {
+  it('卡尼想選 2019/3/19 星期二 出貨 不能選,因為現在是12點', function() {
     let result = D.IsShow(new Date('2019-3-19'))[0];
-    expect(result).equal(false,'不能選,因為現在是上午12點');
+    expect(result).equal(false,'不能選,因為現在是12點');
   });
   
   it('卡尼想選 2019/3/20 星期三 出貨 可以選', function() {
@@ -38,7 +81,7 @@ describe('今天是 2019/3/18 號星期一上午12:00', function() {
   
 });
 
-describe('今天是 2019/3/18 號星期一上午10:00 不能選,因為週日都不能選', function() {
+describe('今天是 2019/3/18 號星期一10:00', function() {
   it('卡尼想選 2019/3/24 星期日 出貨', function() {
     let result = D.IsShow(new Date('2019-3-24'))[0];
     expect(result).equal(false,'不能選,因為週日都不能選');
