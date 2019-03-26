4b9e58dcf3298e87653b61eeadde0d5be0372a5a
Marsen <thisismysoul@gmail.com>
2019-03-27 07:36:32 +0800

    Change the Parameter Type by IRule

--- src/Marsen.NetCore.Dojo/Kata_FizzBuzz/FizzBuzz.cs
+++ src/Marsen.NetCore.Dojo/Kata_FizzBuzz/FizzBuzz.cs
@@ -8,11 +8,11 @@ namespace Marsen.NetCore.Dojo.Kata_FizzBuzz
         {
             string result = string.Empty;
 
-            var fizzRule = new FizzRule();
+            IRule fizzRule = new FizzRule();
             result = fizzRule.Apply(input, result);
-            var buzzRule = new BuzzRule();
+            IRule buzzRule = new BuzzRule();
             result = buzzRule.Apply(input, result);
-            var normalRule = new NormalRule();
+            IRule normalRule = new NormalRule();
             result = normalRule.Apply(input, result);
 
             return result;
