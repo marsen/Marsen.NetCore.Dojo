4474cb984de4168dd5fd32e78cccaa63becf0483
marklin <marklin@nine-yi.com>
2019-03-27 06:41:41 +0800

    重構 Baby Step 加入 result

--- src/Marsen.NetCore.Dojo/Kata_FizzBuzz/FizzBuzz.cs
+++ src/Marsen.NetCore.Dojo/Kata_FizzBuzz/FizzBuzz.cs
@@ -1,9 +1,12 @@
-﻿namespace Marsen.NetCore.Dojo.Kata_FizzBuzz
+﻿using System;
+
+namespace Marsen.NetCore.Dojo.Kata_FizzBuzz
 {
     public class FizzBuzz
     {
         public string Get(int input)
         {
+            string result = string.Empty;
             if (input % 15 == 0)
             {
                 return "FizzBuzz";
@@ -19,6 +22,12 @@
                 return "Fizz";
             }
 
+            if (string.IsNullOrEmpty(result))
+            {
+                return input.ToString();
+            }
+
+            return result;
             return input.ToString();
         }
     }
