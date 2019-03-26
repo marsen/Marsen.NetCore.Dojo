2edd1fc4845240c87b991610d268cdf2413d84b4
marklin <marklin@nine-yi.com>
2019-03-27 07:25:12 +0800

    refactor FizzRule Class

---
+++ src/Marsen.NetCore.Dojo/Kata_FizzBuzz/FizzRule.cs
@@ -0,0 +1,15 @@
+﻿namespace Marsen.NetCore.Dojo.Kata_FizzBuzz
+{
+    public class FizzRule
+    {
+        public string Apply(int input, string result)
+        {
+            if (input % 3 == 0)
+            {
+                result += "Fizz";
+            }
+
+            return result;
+        }
+    }
+}
