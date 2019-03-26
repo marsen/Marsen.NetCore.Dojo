2ea27d0a0e35cb2d887a557b2f537a3cfa999f44
marklin <marklin@nine-yi.com>
2019-03-27 06:53:33 +0800

    重構 拆解邏輯

--- src/Marsen.NetCore.Dojo/Kata_FizzBuzz/FizzBuzz.cs
+++ src/Marsen.NetCore.Dojo/Kata_FizzBuzz/FizzBuzz.cs
@@ -7,7 +7,7 @@ namespace Marsen.NetCore.Dojo.Kata_FizzBuzz
         public string Get(int input)
         {
             string result = string.Empty;
-            if (input % 15 == 0)
+            if (input % 5 == 0 && input % 3 == 0)
             {
                 return "FizzBuzz";
             }
