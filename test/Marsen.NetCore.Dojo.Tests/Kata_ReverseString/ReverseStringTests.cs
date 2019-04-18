01834669d5ec42931f3ef7b55ad05469451ab081
Marsen <thisismysoul@gmail.com>
2019-04-18 18:47:29 +0800

    重構 測試

--- test/Marsen.NetCore.Dojo.Tests/Kata_ReverseString/ReverseStringTests.cs
+++ test/Marsen.NetCore.Dojo.Tests/Kata_ReverseString/ReverseStringTests.cs
@@ -7,19 +7,19 @@ namespace Marsen.NetCore.Dojo.Tests.Kata_ReverseString
 {
     public class ReverseStringTests
     {
+        readonly Reversal _reversal = new Reversal();
+
         [Fact]
         public void A_is_A()
         {
-            var reversal = new Reversal();
-            var actual = reversal.Do("A");
+            var actual = _reversal.Do("A");
             Assert.Equal("A", actual);
         }
 
         [Fact]
         public void BB_is_BB()
         {
-            var reversal = new Reversal();
-            var actual = reversal.Do("BB");
+            var actual = _reversal.Do("BB");
             Assert.Equal("BB", actual);
         }
     }
