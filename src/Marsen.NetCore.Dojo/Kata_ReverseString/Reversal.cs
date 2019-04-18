cf34090bc3dc992bd75ba99effef1fa64afd4593
Marsen <thisismysoul@gmail.com>
2019-04-18 18:31:45 +0800

    反轉字串 A is A 紅燈

---
+++ src/Marsen.NetCore.Dojo/Kata_ReverseString/Reversal.cs
@@ -0,0 +1,10 @@
+﻿namespace Marsen.NetCore.Dojo.Tests.Kata_ReverseString
+{
+    public class Reversal
+    {
+        public string Do(string input)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ test/Marsen.NetCore.Dojo.Tests/Kata_ReverseString/ReverseStringTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Marsen.NetCore.Dojo.Tests.Kata_ReverseString
+{
+    public class ReverseStringTests
+    {
+        [Fact]
+        public void A_is_A()
+        {
+            var reversal = new Reversal();
+            var actual = reversal.Do("A");
+            Assert.Equal("A", actual);
+        }
+    }
+}
