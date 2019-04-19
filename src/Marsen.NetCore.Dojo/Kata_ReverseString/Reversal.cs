4c170b1a0f5648ca5b993dc24b380577713c151c
marklin <marklin@nine-yi.com>
2019-04-19 10:15:20 +0800

    add array loop

--- src/Marsen.NetCore.Dojo/Kata_ReverseString/Reversal.cs
+++ src/Marsen.NetCore.Dojo/Kata_ReverseString/Reversal.cs
@@ -11,6 +11,15 @@ namespace Marsen.NetCore.Dojo.Tests.Kata_ReverseString
                 return input;
             }
 
+            var cArray = input.ToCharArray();
+            for (int i = 0; i < cArray.Length / 2; i++)
+            {
+                var temp = cArray[i];
+                cArray[i] = cArray[cArray.Length - 1 - i];
+                cArray[cArray.Length - 1 - i] = temp;
+//                return cArray.ToString();
+            }
+
             var result = string.Empty;
             for (int i = 0; i < input.Length; i++)
             {
