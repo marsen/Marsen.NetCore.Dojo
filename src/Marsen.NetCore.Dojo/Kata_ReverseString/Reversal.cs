5ca5e0fcd482836dc3733f4ec166583ce6248e98
Marsen <thisismysoul@gmail.com>
2019-04-19 09:58:58 +0800

    By Lambda

--- src/Marsen.NetCore.Dojo/Kata_ReverseString/Reversal.cs
+++ src/Marsen.NetCore.Dojo/Kata_ReverseString/Reversal.cs
@@ -6,6 +6,11 @@ namespace Marsen.NetCore.Dojo.Tests.Kata_ReverseString
     {
         public string Do(string input)
         {
+            return this.ByLambda(input);
+        }
+
+        private string ByLambda(string input)
+        {
             return input?.ToCharArray().Aggregate(string.Empty, (current, c) => c + current);
         }
     }
