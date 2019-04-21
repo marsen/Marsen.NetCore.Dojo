9df25acd9f06bccb47b547e888edbec0d40afd73
marklin <marklin@nine-yi.com>
2019-04-21 16:06:59 +0800

    refactory to different class

---
+++ src/Marsen.NetCore.Dojo/Kata_ReverseString/RecursionReversal.cs
@@ -0,0 +1,17 @@
+﻿namespace Marsen.NetCore.Dojo.Tests.Kata_ReverseString
+{
+    public class RecursionReversal : IStringReversal
+    {
+        public string Do(string input)
+        {
+            if (string.IsNullOrEmpty(input) || input.Length == 1)
+            {
+                return input;
+            }
+
+            var s1 = input.Substring(0, 1);
+            var s2 = input.Substring(1);
+            return Do(s2) + s1;
+        }
+    }
+}
