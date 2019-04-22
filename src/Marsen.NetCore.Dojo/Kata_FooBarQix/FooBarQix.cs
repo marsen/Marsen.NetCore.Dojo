ebd72980f59aada17b2ee828538ec2d466d3f86d
Marsen <thisismysoul@gmail.com>
2019-04-22 10:15:57 +0800

    first test red light

---
+++ src/Marsen.NetCore.Dojo/Kata_FooBarQix/FooBarQix.cs
@@ -0,0 +1,10 @@
+﻿namespace Marsen.NetCore.Dojo.Kata_FooBarQix
+{
+    public class FooBarQix
+    {
+        public string Get(int input)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
