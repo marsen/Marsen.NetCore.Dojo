3dd387828f64d89843d5e50b314db655d70844d8
marklin <marklin@nine-yi.com>
2019-04-22 10:15:57 +0800

    first test red light

---
+++ test/Marsen.NetCore.Dojo.Tests/Kata_FooBarQix/FooBarQixTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Marsen.NetCore.Dojo.Kata_FooBarQix;
+using Xunit;
+
+namespace Marsen.NetCore.Dojo.Tests.Kata_FooBarQix
+{
+    public class FooBarQixTests
+    {
+        [Fact]
+        public void one_is_1()
+        {
+            FooBarQix fooBarQix = new FooBarQix();
+            string expected = fooBarQix.Get(1);
+            Assert.Equal(expected, "1");
+        }
+    }
+}
