9ef2b056ac3053b6069251087962aa7cc2445be5
marklin <marklin@nine-yi.com>
2019-05-05 22:25:55 +0800

    refator tests

--- test/Marsen.NetCore.Dojo.Tests/Kata_FooBarQix/FooBarQixTests.cs
+++ test/Marsen.NetCore.Dojo.Tests/Kata_FooBarQix/FooBarQixTests.cs
@@ -8,20 +8,20 @@ namespace Marsen.NetCore.Dojo.Tests.Kata_FooBarQix
 {
     public class FooBarQixTests
     {
+        readonly FooBarQix _fooBarQix = new FooBarQix();
+
         [Fact]
         public void one_is_1()
         {
-            FooBarQix fooBarQix = new FooBarQix();
-            string expected = fooBarQix.Get(1);
-            Assert.Equal(expected, "1");
+            string actual = _fooBarQix.Get(1);
+            Assert.Equal("1", actual);
         }
 
         [Fact]
         public void two_is_2()
         {
-            FooBarQix fooBarQix = new FooBarQix();
-            string expected = fooBarQix.Get(2);
-            Assert.Equal(expected, "2");
+            string actual = _fooBarQix.Get(2);
+            Assert.Equal("2", actual);
         }
     }
 }
