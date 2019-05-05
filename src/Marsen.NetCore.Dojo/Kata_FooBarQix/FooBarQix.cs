88e35e7147e5735586666e201c250d0100ff8cb7
Marsen <thisismysoul@gmail.com>
2019-05-05 22:49:58 +0800

    baby step 1

--- src/Marsen.NetCore.Dojo/Kata_FooBarQix/FooBarQix.cs
+++ src/Marsen.NetCore.Dojo/Kata_FooBarQix/FooBarQix.cs
@@ -4,14 +4,20 @@
     {
         public string Get(int input)
         {
-            if (input == 6)
-            {
-                return "Foo";
-            }
-
             if (input == 5)
             {
                 return "BarBar";
+            }
+
+            string result = string.Empty;
+            if (input % 3 == 0)
+            {
+                result += "Foo";
+            }
+
+            if (input == 6)
+            {
+                return result;
             }
 
             if (input == 3)
