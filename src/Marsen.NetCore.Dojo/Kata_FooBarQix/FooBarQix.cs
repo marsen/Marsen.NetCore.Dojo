fa08f579286df20928352cbffa182dea4ca95397
marklin <marklin@nine-yi.com>
2019-05-07 15:30:28 +0800

    baby step 1

--- src/Marsen.NetCore.Dojo/Kata_FooBarQix/FooBarQix.cs
+++ src/Marsen.NetCore.Dojo/Kata_FooBarQix/FooBarQix.cs
@@ -6,7 +6,7 @@
         {
             if (input == 10)
             {
-                return "Bar";
+                // return "Bar";
             }
 
             if (input == 7)
@@ -16,10 +16,20 @@
 
             if (input == 5)
             {
-                return "BarBar";
+                ///return "BarBar";
             }
 
             var result = string.Empty;
+            if (IsDivisibleBy5(input))
+            {
+                result += "Bar";
+            }
+
+            if (IsContain5(input))
+            {
+                result += "Bar";
+            }
+
             if (IsDivisibleBy3(input))
             {
                 result += "Foo";
@@ -31,6 +41,16 @@
             }
 
             return string.IsNullOrEmpty(result) ? input.ToString() : result;
+        }
+
+        private bool IsContain5(int input)
+        {
+            return input.ToString().Contains("5");
+        }
+
+        private bool IsDivisibleBy5(int input)
+        {
+            return input % 5 == 0;
         }
 
         private bool IsDivisibleBy3(int input)
