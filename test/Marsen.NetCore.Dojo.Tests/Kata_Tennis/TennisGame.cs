45813ee9646e76f19d6dca3778cb961eaf6bb7ef
Marsen <thisismysoul@gmail.com>
2019-05-28 14:01:43 +0800

    tennis kata love all

---
+++ test/Marsen.NetCore.Dojo.Tests/Kata_Tennis/TennisGame.cs
@@ -0,0 +1,10 @@
+﻿namespace Marsen.NetCore.Dojo.Tests.Kata_Tennis
+{
+    public class TennisGame
+    {
+        public string Score()
+        {
+            return "Love All";
+        }
+    }
+}
---
+++ test/Marsen.NetCore.Dojo.Tests/Kata_Tennis/TennisGameTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Marsen.NetCore.Dojo.Tests.Kata_Tennis
+{
+    public class TennisGameTests
+    {
+        [Fact]
+        public void Love_All()
+        {
+            TennisGame tennisGame = new TennisGame();
+            var score = tennisGame.Score();
+            Assert.Equal("Love All", score);
+        }
+    }
+}
