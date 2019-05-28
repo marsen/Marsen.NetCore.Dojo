66b0b3e1eed46b91c237cfa19289d8553e175b38
Marsen <thisismysoul@gmail.com>
2019-05-28 14:12:54 +0800

    first player score

--- test/Marsen.NetCore.Dojo.Tests/Kata_Tennis/TennisGame.cs
+++ test/Marsen.NetCore.Dojo.Tests/Kata_Tennis/TennisGame.cs
@@ -2,9 +2,21 @@
 {
     public class TennisGame
     {
+        private int _firstPlayerScore;
+
         public string Score()
         {
+            if (_firstPlayerScore == 1)
+            {
+                return "Fifteen Love";
+            }
+
             return "Love All";
+        }
+
+        public void FirstPlayerScore()
+        {
+            _firstPlayerScore++;
         }
     }
 }
