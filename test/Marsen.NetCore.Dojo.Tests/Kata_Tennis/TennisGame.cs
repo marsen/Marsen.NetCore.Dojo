e1d9859d6f7f37a59ae9d3e0a6fb530e67576c84
marklin <marklin@nine-yi.com>
2019-05-29 01:08:49 +0800

    refator

--- test/Marsen.NetCore.Dojo.Tests/Kata_Tennis/TennisGame.cs
+++ test/Marsen.NetCore.Dojo.Tests/Kata_Tennis/TennisGame.cs
@@ -31,7 +31,7 @@ namespace Marsen.NetCore.Dojo.Tests.Kata_Tennis
 
         public string Score()
         {
-            if (_firstPlayerScore == _secondPlayerScore)
+            if (IsSameScore())
             {
                 if (_firstPlayerScore >= 3)
                 {
@@ -41,18 +41,32 @@ namespace Marsen.NetCore.Dojo.Tests.Kata_Tennis
                 return $"{_scoreLookup[_firstPlayerScore]} All";
             }
 
-            if (_firstPlayerScore > 3 || _secondPlayerScore > 3)
+            if (IsReadyForWin())
             {
-                var winner = _firstPlayerScore > _secondPlayerScore ? _firstPlayerName : _secondPlayerName;
-                if (Math.Abs(_firstPlayerScore - _secondPlayerScore) > 1)
-                {
-                    return $"{winner} Win";
-                }
-
-                return $"{winner} Adv";
+                return IsLeading2() ? $"{GetWinnerName()} Win" : $"{GetWinnerName()} Adv";
             }
 
             return $"{_scoreLookup[_firstPlayerScore]} {_scoreLookup[_secondPlayerScore]}";
+        }
+
+        private bool IsLeading2()
+        {
+            return Math.Abs(_firstPlayerScore - _secondPlayerScore) > 1;
+        }
+
+        private string GetWinnerName()
+        {
+            return _firstPlayerScore > _secondPlayerScore ? _firstPlayerName : _secondPlayerName;
+        }
+
+        private bool IsReadyForWin()
+        {
+            return _firstPlayerScore > 3 || _secondPlayerScore > 3;
+        }
+
+        private bool IsSameScore()
+        {
+            return _firstPlayerScore == _secondPlayerScore;
         }
 
         public void FirstPlayerScore()
