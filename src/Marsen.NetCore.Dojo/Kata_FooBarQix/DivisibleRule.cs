fd1d5f29f80a1c7e6a4d4910edf717b061447496
Marsen <thisismysoul@gmail.com>
2019-06-01 19:22:19 +0800

    add ctor

---
+++ src/Marsen.NetCore.Dojo/Kata_FooBarQix/DivisibleRule.cs
@@ -0,0 +1,29 @@
+﻿namespace Marsen.NetCore.Dojo.Kata_FooBarQix
+{
+    public class DivisibleRule
+    {
+        private readonly int _divisor;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DivisibleRule" /> class.
+        /// </summary>
+        public DivisibleRule(int divisor)
+        {
+            _divisor = divisor;
+        }
+        public string Apply(int input, string result)
+        {
+            if (IsDivisibleBy(this._divisor, input))
+            {
+                result += "Foo";
+            }
+
+            return result;
+        }
+
+        private bool IsDivisibleBy(int i, int input)
+        {
+            return input % i == 0;
+        }
+    }
+}
