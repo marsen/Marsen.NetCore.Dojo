e624d052a2fe77ea687a13b9929923df8f2662cc
marklin <marklin@nine-yi.com>
2019-06-01 19:34:08 +0800

    add lookup

--- src/Marsen.NetCore.Dojo/Kata_FooBarQix/DivisibleRule.cs
+++ src/Marsen.NetCore.Dojo/Kata_FooBarQix/DivisibleRule.cs
@@ -1,8 +1,16 @@
-﻿namespace Marsen.NetCore.Dojo.Kata_FooBarQix
+﻿using System.Collections.Generic;
+
+namespace Marsen.NetCore.Dojo.Kata_FooBarQix
 {
     public class DivisibleRule
     {
         private readonly int _divisor;
+
+        Dictionary<int, string> _lookup = new Dictionary<int, string>
+        {
+            {3, "Foo"},
+            {5, "Bar"},
+        };
 
         /// <summary>
         /// Initializes a new instance of the <see cref="DivisibleRule" /> class.
@@ -11,11 +19,12 @@
         {
             _divisor = divisor;
         }
+
         public string Apply(int input, string result)
         {
             if (IsDivisibleBy(this._divisor, input))
             {
-                result += "Foo";
+                result += _lookup[_divisor];
             }
 
             return result;
