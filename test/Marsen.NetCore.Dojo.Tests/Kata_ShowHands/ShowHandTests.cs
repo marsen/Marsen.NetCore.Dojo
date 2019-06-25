65f0d915af1c6737fe0663e43f8c8dbbc4af3a29
Marsen <thisismysoul@gmail.com>
2019-06-26 01:18:29 +0800

    第一個紅燈

---
+++ test/Marsen.NetCore.Dojo.Tests/Kata_ShowHands/ShowHandTests.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using Marsen.NetCore.Dojo.Kata_ShowHands;
+using Xunit;
+
+namespace Marsen.NetCore.Dojo.Tests.Kata_ShowHands
+{
+    public class ShowHandTests
+    {
+        [Fact]
+        public void FourOfAKind_ThreeOfAKind()
+        {
+            ShowHand showHand = new ShowHand();
+            var actual = showHand.Deal("S3,C3,D3,H3,H7", "S3,C3,D3,H7,H8");
+            Assert.Equal("Tom Win, Because Four Of a Kind", actual);
+        }
+    }
+}
