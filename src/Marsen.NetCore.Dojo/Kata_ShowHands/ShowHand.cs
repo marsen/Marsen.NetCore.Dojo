9c6fb8c333f49a05096a3ef2cbfcbd2453d21054
marklin <marklin@nine-yi.com>
2019-06-26 01:18:29 +0800

    第一個紅燈

---
+++ src/Marsen.NetCore.Dojo/Kata_ShowHands/ShowHand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Marsen.NetCore.Dojo.Kata_ShowHands
+{
+    public class ShowHand
+    {
+        public string Deal(string firstPlayerCard, string secondPlayerCard)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
