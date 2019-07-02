be6b04538704a28954fbcc0b610c09f9425277c8
marklin <marklin@nine-yi.com>
2019-07-03 00:24:18 +0800

    move type to other file

---
+++ src/Marsen.NetCore.Dojo/Kata_ShowHands/HandCard.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Marsen.NetCore.Dojo.Kata_ShowHands
+{
+    public class HandCard
+    {
+        public HandCard(List<Card> parse)
+        {
+            this.Category = this.GetCategory(parse);
+        }
+
+        public Category Category { get; }
+
+        private Category GetCategory(List<Card> cardList)
+        {
+            var groupCards = cardList
+                .GroupBy(x => x.Rank)
+                .Select(g => new {Count = g.Count()});
+            if (groupCards.Any(x => x.Count == 4))
+            {
+                return Category.FourOfAKind;
+            }
+
+            if (groupCards.Any(x => x.Count == 3))
+            {
+                return Category.ThreeOfAKind;
+            }
+
+            return Category.TwoPair;
+        }
+    }
+}
--- src/Marsen.NetCore.Dojo/Kata_ShowHands/ShowHand.cs
+++ src/Marsen.NetCore.Dojo/Kata_ShowHands/ShowHand.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 
 namespace Marsen.NetCore.Dojo.Kata_ShowHands
 {
@@ -47,34 +46,6 @@ namespace Marsen.NetCore.Dojo.Kata_ShowHands
             }
 
             return $"{winner} Win, Because {winnerCategory}{highCard}";
-        }
-    }
-
-    public class HandCard
-    {
-        public HandCard(List<Card> parse)
-        {
-            this.Category = this.GetCategory(parse);
-        }
-
-        public Category Category { get; }
-
-        private Category GetCategory(List<Card> cardList)
-        {
-            var groupCards = cardList
-                .GroupBy(x => x.Rank)
-                .Select(g => new { Count = g.Count() });
-            if (groupCards.Any(x => x.Count == 4))
-            {
-                return Category.FourOfAKind;
-            }
-
-            if (groupCards.Any(x => x.Count == 3))
-            {
-                return Category.ThreeOfAKind;
-            }
-
-            return Category.TwoPair;
         }
     }
 }
