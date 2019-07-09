c05f2be62f8501a4dbfa120f0ebd22648b9ed408
Marsen <thisismysoul@gmail.com>
2019-07-10 02:31:24 +0800

    add test of two pair VS two pair

--- test/Marsen.NetCore.Dojo.Tests/Kata_ShowHands/ShowHandTests.cs
+++ test/Marsen.NetCore.Dojo.Tests/Kata_ShowHands/ShowHandTests.cs
@@ -24,7 +24,7 @@ namespace Marsen.NetCore.Dojo.Tests.Kata_ShowHands
         }
 
         [Fact]
-        public void ThreeOfAKind_ThreeOfAKind_With_High_Card_6()
+        public void ThreeOfAKind_ThreeOfAKind_With_Key_Card_6()
         {
             var actual = showHand.Duel("S3,C3,D5,H5,C5", "S3,C3,D6,H6,C6");
             Assert.Equal("Lee Win, Because Three Of a Kind, Key Card 6", actual);
@@ -42,6 +42,13 @@ namespace Marsen.NetCore.Dojo.Tests.Kata_ShowHands
         {
             var actual = showHand.Duel("S4,C4,D5,H5,C7", "S4,C4,D5,S5,S9");
             Assert.Equal("Lee Win, Because Two Pair, Key Card 9", actual);
+        }
+
+        [Fact]
+        public void TwoPair_TwoPair()
+        {
+            var actual = showHand.Duel("S4,C4,D5,H5,C7", "S4,C4,D5,S5,S7");
+            Assert.Equal("End in a tie", actual);
         }
     }
 }
