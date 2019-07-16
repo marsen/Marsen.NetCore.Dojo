2252db084ab702f888d0a008a618708d55e405f1
Marsen <thisismysoul@gmail.com>
2019-07-17 01:26:22 +0800

    add HandCardTest

---
+++ test/Marsen.NetCore.Dojo.Tests/Kata_ShowHands/HandCardTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Marsen.NetCore.Dojo.Kata_ShowHands;
+using Xunit;
+
+namespace Marsen.NetCore.Dojo.Tests.Kata_ShowHands
+{
+    public class HandCardTests
+    {
+        [Fact]
+        public void FourOfAKind()
+        {
+            var target = new HandCard(new List<Card>
+            {
+                new Card {Rank = 6, Suit = SuitEnum.S},
+                new Card {Rank = 6, Suit = SuitEnum.H},
+                new Card {Rank = 6, Suit = SuitEnum.C},
+                new Card {Rank = 6, Suit = SuitEnum.D},
+                new Card {Rank = 8, Suit = SuitEnum.S},
+            });
+            var actual = target.GetCategory();
+            Category expected = Category.FourOfAKind;
+            Assert.Equal(expected, actual);
+        }
+    }
+}
