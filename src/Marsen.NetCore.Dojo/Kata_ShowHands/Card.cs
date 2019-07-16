6b9adf5d7f85570dc62f6e9270fb4a2477f769dd
marklin <marklin@nine-yi.com>
2019-07-16 23:29:28 +0800

    改 a 壞 b

--- src/Marsen.NetCore.Dojo/Kata_ShowHands/Card.cs
+++ src/Marsen.NetCore.Dojo/Kata_ShowHands/Card.cs
@@ -3,5 +3,14 @@
     public class Card
     {
         public int Rank { get; set; }
+        public SuitEnum Suit { get; set; }
+    }
+
+    public enum SuitEnum
+    {
+        C,
+        S,
+        D,
+        H
     }
 }
--- src/Marsen.NetCore.Dojo/Kata_ShowHands/HandCard.cs
+++ src/Marsen.NetCore.Dojo/Kata_ShowHands/HandCard.cs
@@ -24,6 +24,11 @@ namespace Marsen.NetCore.Dojo.Kata_ShowHands
                 .GroupBy(x => x.Rank)
                 .Select(g => new {Count = g.Count(), Rank = g.Key});
             this.KeyCard = groupCards.OrderBy(x => x.Count).Select(x => x.Rank).ToList();
+            if (this._cardList.GroupBy(x=>x.Suit).Count()==5)
+            {
+                return Category.Straight;
+            }
+
             if (groupCards.Any(x => x.Count == 4))
             {
                 return Category.FourOfAKind;
