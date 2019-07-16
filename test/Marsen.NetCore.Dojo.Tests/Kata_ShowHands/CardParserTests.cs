32598a27a4c8ca039341b581b8ac90e3eb0f6244
Marsen <thisismysoul@gmail.com>
2019-07-17 00:32:07 +0800

    重構 測試  讓變異點集中一處

--- test/Marsen.NetCore.Dojo.Tests/Kata_ShowHands/CardParserTests.cs
+++ test/Marsen.NetCore.Dojo.Tests/Kata_ShowHands/CardParserTests.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using ExpectedObjects;
 using Marsen.NetCore.Dojo.Kata_ShowHands;
 using Xunit;
@@ -9,35 +11,31 @@ namespace Marsen.NetCore.Dojo.Tests.Kata_ShowHands
     {
         private readonly CardParser _cardParser = new CardParser();
 
+        private ExpectedObject Parser(string input)
+        {
+            return input.Split(',').Select(x => new Card
+            {
+                Rank = int.Parse(x.Substring(1)),
+                Suit = Enum.Parse<SuitEnum>(x.Substring(0, 1))
+            }).ToList().ToExpectedObject();
+        }
+
         [Fact]
         public void Parse_Four_Of_a_Kind()
         {
             var input = "S3,C3,D3,H3,H7";
             var actual = _cardParser.Parse(input);
-            var expected = new List<Card>
-            {
-                new Card {Rank = 3, Suit = SuitEnum.S},
-                new Card {Rank = 3, Suit = SuitEnum.C},
-                new Card {Rank = 3, Suit = SuitEnum.D},
-                new Card {Rank = 3, Suit = SuitEnum.H},
-                new Card {Rank = 7, Suit = SuitEnum.H},
-            }.ToExpectedObject();
+            var expected = this.Parser(input);
             expected.ShouldEqual(actual);
         }
+
 
         [Fact]
         public void Parse_Three_Of_a_Kind()
         {
             var input = "S3,C3,D3,H7,H8";
             var actual = _cardParser.Parse(input);
-            var expected = new List<Card>
-            {
-                new Card {Rank = 3, Suit = SuitEnum.S},
-                new Card {Rank = 3, Suit = SuitEnum.C},
-                new Card {Rank = 3, Suit = SuitEnum.D},
-                new Card {Rank = 7, Suit = SuitEnum.H},
-                new Card {Rank = 8, Suit = SuitEnum.H},
-            }.ToExpectedObject();
+            var expected = this.Parser(input);
             expected.ShouldEqual(actual);
         }
     }
