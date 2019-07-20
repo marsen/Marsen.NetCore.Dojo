533af8506adf3eea77eb7abb2e417c1550315b54
Marsen <thisismysoul@gmail.com>
2019-07-20 14:03:36 +0800

    fullhouse test case

--- test/Marsen.NetCore.Dojo.Tests/Kata_ShowHands/HandCardTests.cs
+++ test/Marsen.NetCore.Dojo.Tests/Kata_ShowHands/HandCardTests.cs
@@ -38,5 +38,22 @@ namespace Marsen.NetCore.Dojo.Tests.Kata_ShowHands
             Category expected = Category.StraightFlush;
             Assert.Equal(expected, actual);
         }
+
+
+        [Fact]
+        public void FullHouse()
+        {
+            var target = new HandCard(new List<Card>
+            {
+                new Card {Rank = 6, Suit = SuitEnum.S},
+                new Card {Rank = 6, Suit = SuitEnum.C},
+                new Card {Rank = 6, Suit = SuitEnum.H},
+                new Card {Rank = 9, Suit = SuitEnum.C},
+                new Card {Rank = 9, Suit = SuitEnum.S},
+            });
+            var actual = target.GetCategory();
+            Category expected = Category.FullHouse;
+            Assert.Equal(expected, actual);
+        }
     }
 }
