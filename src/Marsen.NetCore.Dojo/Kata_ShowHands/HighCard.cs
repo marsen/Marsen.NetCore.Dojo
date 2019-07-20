414d55bf5d23bba249e1578bf07b7e85f40d8629
marklin <marklin@nine-yi.com>
2019-07-20 13:27:25 +0800

    move classes

---
+++ src/Marsen.NetCore.Dojo/Kata_ShowHands/HighCard.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Marsen.NetCore.Dojo.Kata_ShowHands
+{
+    public class HighCard : ICategoryRule
+    {
+        public bool Apply(List<Card> cardList)
+        {
+            return true;
+        }
+
+        public Category Category => Category.HighCard;
+    }
+}
