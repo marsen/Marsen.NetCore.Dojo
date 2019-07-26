198070b9f59fe6e628aa19a687a02da9f33b143a
marklin <marklin@nine-yi.com>
2019-07-26 18:33:58 +0800

    測試處理失敗

--- src/Marsen.NetCore.Dojo/Kata_ShowHands/Straight.cs
+++ src/Marsen.NetCore.Dojo/Kata_ShowHands/Straight.cs
@@ -5,7 +5,7 @@ namespace Marsen.NetCore.Dojo.Kata_ShowHands
 {
     public class Straight : ICategoryRule
     {
-        private const string AllCard = "1,2,3,4,5,6,7,8,9,10,J,Q,K";
+        private const string AllCard = "1,2,3,4,5,6,7,8,9,10,11,12,13,14";
 
 
         public bool Apply(List<Card> cardList)
