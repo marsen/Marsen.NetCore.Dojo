b4e5a99f8eea9bec3fdf88ca3ad84963b137ebd4
marklin <marklin@nine-yi.com>
2019-07-26 20:21:26 +0800

    重構

--- src/Marsen.NetCore.Dojo/Kata_ShowHands/CardParser.cs
+++ src/Marsen.NetCore.Dojo/Kata_ShowHands/CardParser.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 
 namespace Marsen.NetCore.Dojo.Kata_ShowHands
@@ -17,6 +18,20 @@ namespace Marsen.NetCore.Dojo.Kata_ShowHands
 
         private int ParseRank(string x)
         {
+            if (int.TryParse(x, out int result) == false)
+            {
+                var rankLookup = new Dictionary<string, int>
+                {
+                    {"A", 14},
+                    {"K", 13},
+                    {"Q", 12},
+                    {"J", 11},
+                };
+
+                return rankLookup[x];
+            }
+
+            return result;
             if (x == "A")
             {
                 return 14;
