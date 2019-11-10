bd1f036e323d93954752dc214642237d1aa0f87d
Marsen <thisismysoul@gmail.com>
2019-11-10 18:52:44 +0800

    fix case

--- test/Marsen.NetCore.Dojo.Tests/Kata_ShowHands/ShowHandTests.cs
+++ test/Marsen.NetCore.Dojo.Tests/Kata_ShowHands/ShowHandTests.cs
@@ -159,7 +159,7 @@ namespace Marsen.NetCore.Dojo.Tests.Kata_ShowHands
         [Fact(DisplayName = "兩對 vs 兩對, Key Card 黑桃大於梅花")]
         public void TwoPairs_Spade_Greater_Than_Club()
         {
-            var actual = showHand.Duel("D9,C9,S6,H6,C7", "D6,S6,C9,D8,S7");
+            var actual = showHand.Duel("D9,C9,S6,H6,C7", "D6,S6,C9,D9,S7");
             Assert.Equal("Lee Win, Because Two Pairs, And Spade", actual);
         }
     }
