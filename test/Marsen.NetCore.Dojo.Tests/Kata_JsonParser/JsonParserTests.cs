ca58d18f05279fa592eb287c7a9a1d794dee4981
Marsen <thisismysoul@gmail.com>
2019-12-28 13:13:37 +0800

    green light

---
+++ test/Marsen.NetCore.Dojo.Tests/Kata_JsonParser/JsonParserTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using Xunit;
+
+namespace Marsen.NetCore.Dojo.Tests.Kata_JsonParser
+{
+    public class JsonParserTests
+    {
+        [Fact]
+        public void CovertFirstName()
+        {
+            ////Arrange
+            var json = @"
+            { 
+                'FirstName': 'Tian',
+                'LastName': 'Tank',
+                'BirthDate': '1989/06/04'
+            }";
+            var parser = new Parser();
+            ////Act
+            var actual = parser.Parse(json);
+            ////Assert
+            var expected = new PersonEntity
+            {
+                Age = 30,
+                Name = "Tian Tank"
+            };
+            actual.Should().BeEquivalentTo(expected);
+        }
+    }
+
+    public class PersonEntity
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    public class Parser
+    {
+        public PersonEntity Parse(string json)
+        {
+            return new PersonEntity
+            {
+                Age = 30,
+                Name = "Tian Tank"
+            };
+        }
+    }
+}
