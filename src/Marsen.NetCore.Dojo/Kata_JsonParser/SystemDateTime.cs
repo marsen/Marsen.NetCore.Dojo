23c0378ead4aac50788532aafe22d3a1456a5f41
Marsen <thisismysoul@gmail.com>
2019-12-28 19:17:02 +0800

    green light

---
+++ src/Marsen.NetCore.Dojo/Kata_JsonParser/SystemDateTime.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+[assembly:InternalsVisibleTo("Marsen.NetCore.Dojo.Tests")]
+namespace Marsen.NetCore.Dojo.Kata_JsonParser
+{
+    public struct SystemDateTime
+    {
+        private static DateTime? _mockDateTime = null;
+
+        public static DateTime Now
+        {
+            get => _mockDateTime ?? DateTime.Now;
+            internal set => _mockDateTime = value;
+        }
+    }
+}
