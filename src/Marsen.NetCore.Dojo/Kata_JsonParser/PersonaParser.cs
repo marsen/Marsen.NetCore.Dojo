71eb350528964322802fdb32d5c80276badc1879
Marsen <thisismysoul@gmail.com>
2019-12-28 14:48:12 +0800

    Convert Name Parse

--- src/Marsen.NetCore.Dojo/Kata_JsonParser/PersonaParser.cs
+++ src/Marsen.NetCore.Dojo/Kata_JsonParser/PersonaParser.cs
@@ -1,14 +1,24 @@
-﻿namespace Marsen.NetCore.Dojo.Kata_JsonParser
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Marsen.NetCore.Dojo.Kata_JsonParser
 {
     public class PersonaParser
     {
         public PersonaEntity Parse(string json)
         {
+            var originEntity = JsonSerializer.Deserialize<PersonaOriginEntity>(json);
             return new PersonaEntity
             {
                 Age = 30,
-                Name = "Tian Tank"
+                Name = originEntity.FirstName +" "+ originEntity.LastName
             };
         }
+    }
+
+    public class PersonaOriginEntity
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
     }
 }
