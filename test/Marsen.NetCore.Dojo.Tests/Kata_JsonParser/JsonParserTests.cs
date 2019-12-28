ed43948410c373d17bb9de23cd5c6f556784534a
Marsen <thisismysoul@gmail.com>
2019-12-28 14:56:39 +0800

    all green

--- test/Marsen.NetCore.Dojo.Tests/Kata_JsonParser/JsonParserTests.cs
+++ test/Marsen.NetCore.Dojo.Tests/Kata_JsonParser/JsonParserTests.cs
@@ -10,7 +10,28 @@ namespace Marsen.NetCore.Dojo.Tests.Kata_JsonParser
     public class JsonParserTests
     {
         [Fact]
-        public void CovertFirstName()
+        public void CovertName()
+        {
+            ////Arrange
+            var json = "{" +
+                       "\"FirstName\": \"Tian\"," +
+                       "\"LastName\": \"Tank\"," +
+                       "\"BirthDate\": \"1989/06/04\"" +
+                       "}";
+            var parser = new PersonaParser();
+            ////Act
+            var actual = parser.Parse(json).Name;
+            ////Assert
+            var expected = new PersonaEntity
+            {
+                Age = 30,
+                Name = "Tian Tank"
+            };
+            actual.Should().BeEquivalentTo(expected.Name);
+        }
+
+        [Fact]
+        public void CovertAge()
         {
             ////Arrange
             var json = "{" +
