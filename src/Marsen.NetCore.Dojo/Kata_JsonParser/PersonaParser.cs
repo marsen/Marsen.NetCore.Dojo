d454acd8a7d034378f2d7fe1327930579d85615d
Marsen <thisismysoul@gmail.com>
2019-12-28 15:19:39 +0800

    green light

--- src/Marsen.NetCore.Dojo/Kata_JsonParser/PersonaParser.cs
+++ src/Marsen.NetCore.Dojo/Kata_JsonParser/PersonaParser.cs
@@ -1,4 +1,6 @@
-﻿using System.Text.Json;
+﻿using System;
+using System.Globalization;
+using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace Marsen.NetCore.Dojo.Kata_JsonParser
@@ -7,9 +9,14 @@ namespace Marsen.NetCore.Dojo.Kata_JsonParser
     {
         public PersonaEntity Parse(string json)
         {
-            var originEntity = JsonSerializer.Deserialize<PersonaOriginEntity>(json);
+            var serializeOptions = new JsonSerializerOptions();
+            serializeOptions.Converters.Add(new DateTimeOffsetConverter());
+            serializeOptions.WriteIndented = true;
+            var originEntity = JsonSerializer.Deserialize<PersonaOriginEntity>(json,serializeOptions);
+            var age = DateTime.Now.Year - originEntity.BirthDate.Year;
             return new PersonaEntity
             {
+                Age = age,
                 Name = originEntity.FirstName + " " + originEntity.LastName
             };
         }
@@ -19,5 +26,22 @@ namespace Marsen.NetCore.Dojo.Kata_JsonParser
     {
         public string LastName { get; set; }
         public string FirstName { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+
+    public class DateTimeOffsetConverter : JsonConverter<DateTime>
+    {
+        public override DateTime Read(
+            ref Utf8JsonReader reader,
+            Type typeToConvert,
+            JsonSerializerOptions options) =>
+            DateTime.ParseExact(reader.GetString(), "yyyy/MM/dd",CultureInfo.InvariantCulture);
+
+        public override void Write(
+            Utf8JsonWriter writer,
+            DateTime value,
+            JsonSerializerOptions options) =>
+            writer.WriteStringValue(value.ToString(
+                "yyyy/MM/dd", CultureInfo.InvariantCulture));
     }
 }
