01968aa38d046a8b739949decc292aad2cdcb330
marklin <marklin@gmail.com>
2020-01-14 02:12:06 +0800

    crate readme & test project

---
+++ test/Marsen.NetCore.Dojo.Tests/Kata_Api_Pay/PaymentServiceTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Marsen.NetCore.Dojo.Tests.Kata_Api_Pay
+{
+    public class PaymentServiceTests
+    {
+        [Fact]
+        public void test()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
