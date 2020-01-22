a860edbcd8ee9d168e1df27e27a84f8b2b0e479b
Marsen <thisismysoul@gmail.com>
2020-01-22 17:46:12 +0800

    create interface

--- test/Marsen.NetCore.Dojo.Tests/Kata_Api_Pay/PaymentServiceTests.cs
+++ test/Marsen.NetCore.Dojo.Tests/Kata_Api_Pay/PaymentServiceTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using NSubstitute;
 using Xunit;
 
 namespace Marsen.NetCore.Dojo.Tests.Kata_Api_Pay
@@ -10,14 +11,27 @@ namespace Marsen.NetCore.Dojo.Tests.Kata_Api_Pay
         [Fact]
         public void pay_should_Get_requestId()
         {
-            var target = new PaymentService();
+            IHttpClient httpClient = null;
+            var target = new PaymentService(httpClient);
             target.Pay();
             httpClient.Received().GetAsync("https://testing.url/api/v1/requestId");
         }
     }
 
+    public interface IHttpClient
+    {
+        void GetAsync(string uri);
+    }
+
     public class PaymentService
     {
+        private readonly IHttpClient _httpClient;
+
+        public PaymentService(IHttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
         public void Pay()
         {
             throw new NotImplementedException();
