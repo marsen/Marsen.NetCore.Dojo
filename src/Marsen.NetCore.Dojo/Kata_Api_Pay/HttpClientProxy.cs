a9bb3a339f1803926e593296cad7cadc830b9a5c
Marsen <thisismysoul@gmail.com>
2020-01-22 19:20:54 +0800

    move files

---
+++ src/Marsen.NetCore.Dojo/Kata_Api_Pay/HttpClientProxy.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Marsen.NetCore.Dojo.Tests.Kata_Api_Pay
+{
+    public class HttpClientProxy : IHttpClient
+    {
+        public Task<HttpResponseMessage> GetAsync(string uri)
+        {
+            return new HttpClient().GetAsync(uri);
+        }
+    }
+}
---
+++ src/Marsen.NetCore.Dojo/Kata_Api_Pay/PaymentService.cs
@@ -0,0 +1,17 @@
+﻿namespace Marsen.NetCore.Dojo.Tests.Kata_Api_Pay
+{
+    public class PaymentService
+    {
+        private readonly IHttpClient _httpClient;
+
+        public PaymentService(IHttpClient httpClient)
+        {
+            this._httpClient = httpClient;
+        }
+
+        public void Pay()
+        {
+            this._httpClient.GetAsync("https://testing.url/api/v1/requestId");
+        }
+    }
+}
