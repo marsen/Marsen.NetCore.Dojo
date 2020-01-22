213851dbe9807646722406b0cd4d08d57fa7cefc
Marsen <thisismysoul@gmail.com>
2020-01-22 19:49:24 +0800

    refator test

--- test/Marsen.NetCore.Dojo.Tests/Kata_Api_Pay/PaymentServiceTests.cs
+++ test/Marsen.NetCore.Dojo.Tests/Kata_Api_Pay/PaymentServiceTests.cs
@@ -10,22 +10,38 @@ namespace Marsen.NetCore.Dojo.Tests.Kata_Api_Pay
 {
     public class PaymentServiceTests
     {
+        IHttpClient httpClient;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PaymentServiceTests" /> class.
+        /// </summary>
+        public PaymentServiceTests()
+        {
+            httpClient = Substitute.For<IHttpClient>();
+        }
+
         [Fact]
         public void pay_should_Get_requestId()
         {
-            IHttpClient httpClient = Substitute.For<IHttpClient>();
-            var target = new PaymentService(httpClient);
-            target.Pay();
+            //IHttpClient httpClient = Substitute.For<IHttpClient>();
+            //var target = new PaymentService(httpClient);
+            //target.Pay();
+            WhenPay();
             httpClient.Received().GetAsync("https://testing.url/api/v1/requestId");
         }
 
         [Fact]
         public void pay_should_Post_Pay_CreditCard()
         {
-            IHttpClient httpClient = Substitute.For<IHttpClient>();
+            WhenPay();
+            httpClient.Received().PostAsync("https://testing.url/api/v1/pay/CreditCard", Arg.Any<HttpContent>());
+        }
+
+        private void WhenPay()
+        {
+            //IHttpClient httpClient = Substitute.For<IHttpClient>();
             var target = new PaymentService(httpClient);
             target.Pay();
-            httpClient.Received().PostAsync("https://testing.url/api/v1/pay/CreditCard", Arg.Any<HttpContent>());
         }
     }
 }
