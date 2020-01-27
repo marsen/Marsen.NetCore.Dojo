7cf7a3b7a8e3d75ca9e7f9e7f1689f270e609614
Marsen <thisismysoul@gmail.com>
2020-01-28 04:10:33 +0800

    紅燈

--- src/Marsen.NetCore.Dojo/Kata_Api_Pay/PaymentService.cs
+++ src/Marsen.NetCore.Dojo/Kata_Api_Pay/PaymentService.cs
@@ -6,15 +6,18 @@ namespace Marsen.NetCore.Dojo.Kata_Api_Pay
     public class PaymentService
     {
         private readonly IHttpClient _httpClient;
+        private readonly IConfigure _configure;
 
-        public PaymentService(IHttpClient httpClient)
+        public PaymentService(IHttpClient httpClient, IConfigure configure)
         {
             this._httpClient = httpClient;
+            this._configure = configure;
         }
 
         public void Pay(PayEntity payEntity)
         {
-            var apiUrl = "https://testing.url/api/v1/";
+            //var apiUrl = "https://testing.url/api/v1/";
+            var apiUrl = this._configure.Setting("PayService.Url");
             var requestId = this._httpClient.GetAsync($"{apiUrl}requestId").Result.Content
                 .ReadAsStringAsync().Result;
             HttpContent content = new StringContent(
@@ -23,5 +26,10 @@ namespace Marsen.NetCore.Dojo.Kata_Api_Pay
 
             this._httpClient.PostAsync($"{apiUrl}pay/CreditCard", content);
         }
+    }
+
+    public interface IConfigure
+    {
+        string Setting(string key);
     }
 }
