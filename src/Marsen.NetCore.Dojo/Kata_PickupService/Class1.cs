12ab636d33ac1e34e935fd70fc2997d03c20c04d
Marsen <thisismysoul@gmail.com>
2020-02-12 20:42:25 +0800

    step 0 initial legacy code

---
+++ src/Marsen.NetCore.Dojo/Kata_PickupService/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Marsen.NetCore.Dojo.Kata_PickupService
+{
+    public class PickupService : IQueryStatus
+    {
+        public List<ShippingOrderUpdateEntity> GetUpdateStatus(long storeId, List<string> waybillNo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Marsen.NetCore.Dojo/Kata_PickupService/StatusEnum.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace Marsen.NetCore.Dojo.Kata_PickupService
+{
+    public enum StatusEnum
+    {
+        Finish,
+        Processing,
+        Arrived,
+        Abnormal
+    }
+}
+
