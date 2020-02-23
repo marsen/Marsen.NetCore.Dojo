f62973775c8d09c847e03cec7b22513872f19fb7
Marsen <thisismysoul@gmail.com>
2020-02-23 23:14:12 +0800

    move files

---
+++ src/Marsen.NetCore.Dojo/Kata_PickupService/Item.cs
@@ -0,0 +1,12 @@
+﻿namespace Marsen.NetCore.Dojo.Kata_PickupService
+{
+    public class Item
+    {
+        public string commodity { get; set; }
+        public int noOfItem { get; set; }
+        public int commercialValue { get; set; }
+        public string currency { get; set; }
+        public string coo { get; set; }
+        public string hsCode { get; set; }
+    }
+}
--- src/Marsen.NetCore.Dojo/Kata_PickupService/PickupService.cs
+++ src/Marsen.NetCore.Dojo/Kata_PickupService/PickupService.cs
@@ -2,7 +2,6 @@
 using System.Net.Http;
 using System.Text;
 using System.Text.Json;
-using System.Text.Json.Serialization;
 
 namespace Marsen.NetCore.Dojo.Kata_PickupService
 {
@@ -56,54 +55,5 @@ namespace Marsen.NetCore.Dojo.Kata_PickupService
 
             return result;
         }
-    }
-
-    public class ResponseEntity
-    {
-        public string result { get; set; }
-
-        [JsonPropertyName("content")]
-        public List<Content> Content { get; set; }
-    }
-
-    public class Item
-    {
-        public string commodity { get; set; }
-        public int noOfItem { get; set; }
-        public int commercialValue { get; set; }
-        public string currency { get; set; }
-        public string coo { get; set; }
-        public string hsCode { get; set; }
-    }
-
-    public class Content
-    {
-        public string merchantId { get; set; }
-        public string merchantRef { get; set; }
-        public string waybillNo { get; set; }
-        public string locationId { get; set; }
-        public string pudoRef { get; set; }
-        public string pudoVerifyCode { get; set; }
-        public string senderId { get; set; }
-        public string consigneeId { get; set; }
-        public string customerName { get; set; }
-        public string customerAddress1 { get; set; }
-        public string customerAddress2 { get; set; }
-        public string customerAddress3 { get; set; }
-        public string customerAddress4 { get; set; }
-        public string feedbackURL { get; set; }
-        public string eta { get; set; }
-        public string codAmt { get; set; }
-        public string sizeCode { get; set; }
-        public List<Item> item { get; set; }
-
-        [JsonPropertyName("lastStatusId")]
-        public string Status { get; set; }
-
-        public string lastStatusDescription { get; set; }
-        public string lastStatusDate { get; set; }
-        public string lastStatusTime { get; set; }
-        public string customerMobile { get; set; }
-        public string customerEmail { get; set; }
     }
 }
