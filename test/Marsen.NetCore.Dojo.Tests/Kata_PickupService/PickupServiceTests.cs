17bf0413606b0baf9b1c480c554e1adf942d262c
Marsen <thisismysoul@gmail.com>
2020-03-03 17:57:57 +0800

    add test

--- test/Marsen.NetCore.Dojo.Tests/Kata_PickupService/PickupServiceTests.cs
+++ test/Marsen.NetCore.Dojo.Tests/Kata_PickupService/PickupServiceTests.cs
@@ -1,4 +1,12 @@
-﻿using Xunit;
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using Marsen.NetCore.Dojo.Kata_PickupService;
+using Marsen.NetCore.Dojo.Kata_PickupService.Entity;
+using Marsen.NetCore.Dojo.Kata_PickupService.Interface;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+using Xunit;
 
 namespace Marsen.NetCore.Dojo.Tests.Kata_PickupService
 {
@@ -7,7 +15,21 @@ namespace Marsen.NetCore.Dojo.Tests.Kata_PickupService
         [Fact]
         public void GetUpdateStatusTest()
         {
-            Assert.True(false, "This test needs an implementation");
+            ILogger logger = Substitute.For<ILogger>();
+            IStoreSettingService storeSettingService = Substitute.For<IStoreSettingService>();
+            IConfigService configService = Substitute.For<IConfigService>();
+            var target = new PickupService(configService, storeSettingService, logger);
+
+            var actual = target.GetUpdateStatus(2, new List<string> {"TestWayBillNo"});
+            actual.Should().BeEquivalentTo(new List<ShippingOrderUpdateEntity>
+            {
+                new ShippingOrderUpdateEntity
+                {
+                    AcceptTime = new DateTime(2020, 03, 03, 17, 51, 20),
+                    OuterCode = "TestWayBillNo",
+                    Status = StatusEnum.Finish
+                }
+            });
         }
     }
 }
