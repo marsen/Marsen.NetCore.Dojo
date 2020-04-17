64986f0b41bac75e6aca9d7b278e44d3087d9ec6
Marsen <thisismysoul@gmail.com>
2020-04-17 17:17:44 +0800

    add E2E tests

---
+++ test/Marsen.NetCore.Dojo.E2E.Tests/E2E.Tests.Console/JoeyClass_AOP_and_DI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Marsen.E2E.Tests.JoeyClass_AOP_and_DI
+{
+    public class Startup
+    {
+        public static void Start()
+        {
+            Console.WriteLine("AOP & DI");
+        }
+    }
+}
