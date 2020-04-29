92a6ce78282362b32ce419e960c1519cd66cc198
Marsen <thisismysoul@gmail.com>
2020-04-29 17:00:54 +0800

    add asnyc unit tests with xunit

---
+++ src/Marsen.NetCore.Dojo/Classes/Microsoft/AsyncCalculator.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+
+namespace Marsen.NetCore.Dojo.Classes.Microsoft
+{
+    public sealed class AsyncCalculator
+    {
+        public async Task<int> Add(int num1, int num2)
+        {
+            await Task.Delay(10);
+            return num1 + num2;
+        }
+
+        public async Task<int> Subtract(int num1, int num2)
+        {
+            await Task.Delay(10);
+            return num1 - num2;
+        }
+
+        public async Task<int> Multiply(int num1, int num2)
+        {
+            await Task.Delay(10);
+            return num1 * num2;
+        }
+
+        public async Task<(int divisor, int remainder)> Divide(int num1, int num2)
+        {
+            await Task.Delay(10);
+            return (num1 / num2, num1 % num2);
+        }
+    }
+}
---
+++ test/Marsen.NetCore.Dojo.Tests/Classes/Microsoft/AsyncCalculatorTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Marsen.NetCore.Dojo.Classes.Microsoft;
+using Xunit;
+
+namespace Marsen.NetCore.Dojo.Tests.Classes.Microsoft
+{
+    public class AsyncCalculatorTests
+    {
+        private readonly AsyncCalculator _target = new AsyncCalculator();
+
+        [Fact]
+        public async Task Add()
+        {
+            Assert.Equal(3, await _target.Add(1, 2));
+        }
+
+
+        [Fact]
+        public async Task Subtract()
+        {
+            Assert.Equal(1, await _target.Subtract(3, 2));
+        }
+
+        [Fact]
+        public async Task Multiply()
+        {
+            Assert.Equal(21, await _target.Multiply(3, 7));
+        }
+
+        [Fact]
+        public async Task Divide()
+        {
+            var (divisor, remainder) = await _target.Divide(7, 3);
+            Assert.Equal(2, divisor);
+            Assert.Equal(1, remainder);
+        }
+
+        [Fact]
+        public void DivideWithZero()
+        {
+            Func<Task> act = async () => await _target.Divide(7, 0);
+            act.Should().Throw<DivideByZeroException>();
+        }
+    }
+}
