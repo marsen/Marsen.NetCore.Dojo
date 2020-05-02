86bb083e13b5055322a1625af3d7c59977aa5080
Marsen <thisismysoul@gmail.com>
2020-05-02 20:24:49 +0800

    refactor with chars

--- src/Marsen.NetCore.Dojo/Classes/Joey/Alphabet/AlphabetGame.cs
+++ src/Marsen.NetCore.Dojo/Classes/Joey/Alphabet/AlphabetGame.cs
@@ -1,10 +1,18 @@
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Marsen.NetCore.Dojo.Classes.Joey.Alphabet
 {
     public class AlphabetGame
     {
-        public string Generate(string s)
+        public string Generate(string input)
         {
-            return "A";
+            var chars = new List<char>
+            {
+                'A'
+            };
+            
+            return string.Join('-', chars);
         }
     }
 }
