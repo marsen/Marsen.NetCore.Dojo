5e7faf6b4afcf198f6525607baedb43a60f2870b
Marsen <thisismysoul@gmail.com>
2020-05-03 09:00:44 +0800

    move files

--- src/Marsen.NetCore.Dojo/Classes/Joey/TddAndRefactor2005/AlphabetGame.cs
+++ src/Marsen.NetCore.Dojo/Classes/Joey/TddAndRefactor2005/AlphabetGame.cs
@@ -1,8 +1,6 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
 
-namespace Marsen.NetCore.Dojo.Classes.Joey.Alphabet
+namespace Marsen.NetCore.Dojo.Classes.Joey.TddAndRefactor2005
 {
     public class AlphabetGame
     {
--- test/Marsen.NetCore.Dojo.Tests/Classes/Joey/TddAndRefactor2005/AlphabetTests.cs
+++ test/Marsen.NetCore.Dojo.Tests/Classes/Joey/TddAndRefactor2005/AlphabetTests.cs
@@ -1,9 +1,8 @@
-using System.Collections.Generic;
 using FluentAssertions;
-using Marsen.NetCore.Dojo.Classes.Joey.Alphabet;
+using Marsen.NetCore.Dojo.Classes.Joey.TddAndRefactor2005;
 using Xunit;
 
-namespace Marsen.NetCore.Dojo.Tests.Classes.Joey.Alphabet
+namespace Marsen.NetCore.Dojo.Tests.Classes.Joey.TddAndRefactor2005
 {
     public class AlphabetTests
     {
