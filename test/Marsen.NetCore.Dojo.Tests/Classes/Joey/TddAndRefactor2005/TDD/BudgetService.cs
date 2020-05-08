e40f47df63efec4f94b203d0568aefd6bf631fab
Marsen <thisismysoul@gmail.com>
2020-05-08 21:56:45 +0800

    move logic

--- test/Marsen.NetCore.Dojo.Tests/Classes/Joey/TddAndRefactor2005/TDD/BudgetService.cs
+++ test/Marsen.NetCore.Dojo.Tests/Classes/Joey/TddAndRefactor2005/TDD/BudgetService.cs
@@ -19,18 +19,20 @@ namespace Marsen.NetCore.Dojo.Tests.Classes.Joey.TddAndRefactor2005.TDD
             {
                 var period = new Period(start, end);
                 var budget = budgets.First();
-                if (period.End<budget.FirstDay() || budget.EndDay()<period.Start)
-                {
-                    return 0;
-                }
 
                 return 1 * OverlapDays(budget, period);
             }
+
             return 0;
         }
 
         private static int OverlapDays(Budget budget, Period period)
         {
+            if (period.End < budget.FirstDay() || budget.EndDay() < period.Start)
+            {
+                return 0;
+            }
+
             var overlapStartDay = budget.FirstDay() < period.Start ? period.Start : budget.FirstDay();
             var overlapEndDay = budget.EndDay() < period.End ? budget.EndDay() : period.End;
             return (overlapEndDay - overlapStartDay).Days + 1;
