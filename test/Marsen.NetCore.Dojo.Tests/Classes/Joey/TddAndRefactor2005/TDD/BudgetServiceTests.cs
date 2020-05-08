688ab0a868ae6508a7ed64a4449525b8fd621929
Marsen <thisismysoul@gmail.com>
2020-05-08 16:31:25 +0800

    refactor test

--- test/Marsen.NetCore.Dojo.Tests/Classes/Joey/TddAndRefactor2005/TDD/BudgetServiceTests.cs
+++ test/Marsen.NetCore.Dojo.Tests/Classes/Joey/TddAndRefactor2005/TDD/BudgetServiceTests.cs
@@ -1,14 +1,36 @@
+using System;
+using FluentAssertions;
 using Xunit;
 
 namespace Marsen.NetCore.Dojo.Tests.Classes.Joey.TddAndRefactor2005.TDD
 {
     public class BudgetServiceTests
     {
+        private BudgetService _budgetService;
+
         [Fact]
         public void NoBudget()
         {
-            var budgetService = new BudgetService();
-             Assert.Equal(0,budgetService.Query("20200401","20200401"));
+            _budgetService = new BudgetService();
+            Between("20200401").And("20200401").ShouldBe(0);
+        }
+
+        private (BudgetService target, string start) Between(string start)
+        {
+            return (_budgetService,start);
+        }
+    }
+    internal static class BudgetServiceTestsExtenstion
+    {
+        public static (BudgetService service, string start, string end) And(this (BudgetService service,string start) target,string end)
+        {
+            return (target.service, target.start, end);
+        }
+        
+        public static void ShouldBe(this (BudgetService service,string start,string end) target,int expected)
+        {
+            var actual = target.service.Query(target.start, target.end);
+            Assert.Equal(expected,actual);
         }
     }
 }
