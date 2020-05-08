687d946d46e2c54ba65a0b28b3a8cbd1fda8cf5a
Marsen <thisismysoul@gmail.com>
2020-05-08 15:12:41 +0800

    pass and move class

---
+++ test/Marsen.NetCore.Dojo.Tests/Classes/Joey/TddAndRefactor2005/TDD/BudgetService.cs
@@ -0,0 +1,10 @@
+namespace Marsen.NetCore.Dojo.Tests.Classes.Joey.TddAndRefactor2005.TDD
+{
+    public class BudgetService
+    {
+        public int Query(string start, string end)
+        {
+            return 0;
+        }
+    }
+}
--- test/Marsen.NetCore.Dojo.Tests/Classes/Joey/TddAndRefactor2005/TDD/BudgetServiceTests.cs
+++ test/Marsen.NetCore.Dojo.Tests/Classes/Joey/TddAndRefactor2005/TDD/BudgetServiceTests.cs
@@ -5,9 +5,11 @@ namespace Marsen.NetCore.Dojo.Tests.Classes.Joey.TddAndRefactor2005.TDD
     public class BudgetServiceTests
     {
         [Fact]
-        public void Test()
+        public void NoBudget()
         {
-
+            var budgetService = new BudgetService();
+             Assert.Equal(0,budgetService.Query("20200401","20200401"));
         }
     }
 }
+
