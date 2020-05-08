636e285059a3633ee72236167c093f7a1bed219e
Marsen <thisismysoul@gmail.com>
2020-05-08 16:48:50 +0800

    refactor test

--- test/Marsen.NetCore.Dojo.Tests/Classes/Joey/TddAndRefactor2005/TDD/BudgetServiceTests.cs
+++ test/Marsen.NetCore.Dojo.Tests/Classes/Joey/TddAndRefactor2005/TDD/BudgetServiceTests.cs
@@ -11,26 +11,13 @@ namespace Marsen.NetCore.Dojo.Tests.Classes.Joey.TddAndRefactor2005.TDD
         [Fact]
         public void NoBudget()
         {
-            _budgetService = new BudgetService();
-            Between("20200401").And("20200401").ShouldBe(0);
+            BudgetAmount().Between("20200401").And("20200401").ShouldBe(0);
         }
 
-        private (BudgetService target, string start) Between(string start)
+        private BudgetService BudgetAmount()
         {
-            return (_budgetService,start);
-        }
-    }
-    internal static class BudgetServiceTestsExtenstion
-    {
-        public static (BudgetService service, string start, string end) And(this (BudgetService service,string start) target,string end)
-        {
-            return (target.service, target.start, end);
-        }
-        
-        public static void ShouldBe(this (BudgetService service,string start,string end) target,int expected)
-        {
-            var actual = target.service.Query(target.start, target.end);
-            Assert.Equal(expected,actual);
+            _budgetService = new BudgetService();
+            return _budgetService;
         }
     }
 }
---
+++ test/Marsen.NetCore.Dojo.Tests/Classes/Joey/TddAndRefactor2005/TDD/BudgetServiceTestsExtenstion.cs
@@ -0,0 +1,22 @@
+using Xunit;
+
+namespace Marsen.NetCore.Dojo.Tests.Classes.Joey.TddAndRefactor2005.TDD
+{
+    internal static class BudgetServiceTestsExtenstion
+    {
+        public static (BudgetService service, string start) Between(this BudgetService service,string start)
+        {
+            return (service, start);
+        }
+        public static (BudgetService service, string start, string end) And(this (BudgetService service,string start) target,string end)
+        {
+            return (target.service, target.start, end);
+        }
+        
+        public static void ShouldBe(this (BudgetService service,string start,string end) target,int expected)
+        {
+            var actual = target.service.Query(target.start, target.end);
+            Assert.Equal(expected,actual);
+        }
+    }
+}
