ad8455f65e6225a1f8f0c3564b740da9b0454dcc
Marsen <thisismysoul@gmail.com>
2020-05-08 17:41:59 +0800

    parameter object

--- test/Marsen.NetCore.Dojo.Tests/Classes/Joey/TddAndRefactor2005/TDD/BudgetService.cs
+++ test/Marsen.NetCore.Dojo.Tests/Classes/Joey/TddAndRefactor2005/TDD/BudgetService.cs
@@ -3,6 +3,18 @@ using System.Linq;
 
 namespace Marsen.NetCore.Dojo.Tests.Classes.Joey.TddAndRefactor2005.TDD
 {
+    public class Period
+    {
+        public Period(string start, string end)
+        {
+            Start = start;
+            End = end;
+        }
+
+        public string Start { get; private set; }
+        public string End { get; private set; }
+    }
+
     public class BudgetService
     {
         private readonly IBudgetRepo _budgetRepo;
@@ -17,15 +29,15 @@ namespace Marsen.NetCore.Dojo.Tests.Classes.Joey.TddAndRefactor2005.TDD
             var budgets = _budgetRepo.GetAll();
             if (budgets.Any())
             {
-                return Days(start, end);
+                return Days(new Period(start, end));
             }
             return 0;
         }
 
-        private static int Days(string start, string end)
+        private static int Days(Period period)
         {
-            var startDate = DateTime.ParseExact(start, "yyyyMMdd", null);
-            var endDate = DateTime.ParseExact(end, "yyyyMMdd", null);
+            var startDate = DateTime.ParseExact(period.Start, "yyyyMMdd", null);
+            var endDate = DateTime.ParseExact(period.End, "yyyyMMdd", null);
             return (startDate - endDate).Days + 1;
         }
     }
