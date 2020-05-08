29103c21e73f0287e1d5c3969a36e47b3cd801e6
Marsen <thisismysoul@gmail.com>
2020-05-08 11:13:55 +0800

    readme

---
+++ test/Marsen.NetCore.Dojo.Tests/Classes/Joey/TddAndRefactor2005/TDD/BudgetServiceTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Marsen.NetCore.Dojo.Tests.Classes.Joey.TddAndRefactor2005.TDD
+{
+    public class BudgetServiceTests
+    {
+        [Fact]
+        public void Test()
+        {
+
+        }
+    }
+}
