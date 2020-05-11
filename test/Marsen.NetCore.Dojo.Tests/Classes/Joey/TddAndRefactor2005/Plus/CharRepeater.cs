ac7f62afc27884f9965f21b478e41e0f7f52a47c
Marsen <thisismysoul@gmail.com>
2020-05-11 17:56:40 +0800

    refactor inline

--- test/Marsen.NetCore.Dojo.Tests/Classes/Joey/TddAndRefactor2005/Plus/CharRepeater.cs
+++ test/Marsen.NetCore.Dojo.Tests/Classes/Joey/TddAndRefactor2005/Plus/CharRepeater.cs
@@ -9,18 +9,14 @@ namespace Marsen.NetCore.Dojo.Tests.Classes.Joey.TddAndRefactor2005.Plus
         public string Repeat(string input)
         {
             var list = new List<string>();
-            var result = input.Substring(0,1).ToUpper();
             list.Add(input.Substring(0,1).ToUpper());
             for (var i = 1; i < input.Length; i++)
             {
-                var substring = input.Substring(i, 1);
-                var lower = RepeatLower(substring,i);
-                list.Add(substring.ToUpper()+lower);
-                result += "-" + substring.ToUpper() + lower;
+                var lower = RepeatLower(input.Substring(i, 1),i);
+                list.Add(input.Substring(i, 1).ToUpper()+lower);
             }
 
             return string.Join('-', list);
-            return result;
         }
 
         private string RepeatLower(string substring,int times)
