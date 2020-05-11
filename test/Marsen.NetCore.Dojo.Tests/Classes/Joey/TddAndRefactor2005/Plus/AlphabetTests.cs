00c1b52bc181ad2cba6fce5798a4e32d84c1fb7f
Marsen <thisismysoul@gmail.com>
2020-05-11 15:45:15 +0800

    namespace

--- test/Marsen.NetCore.Dojo.Tests/Classes/Joey/TddAndRefactor2005/Plus/AlphabetTests.cs
+++ test/Marsen.NetCore.Dojo.Tests/Classes/Joey/TddAndRefactor2005/Plus/AlphabetTests.cs
@@ -1,9 +1,8 @@
 using FluentAssertions;
-using Marsen.NetCore.Dojo.Classes.Joey.TddAndRefactor2005;
 using Marsen.NetCore.Dojo.Classes.Joey.TddAndRefactor2005.Plus;
 using Xunit;
 
-namespace Marsen.NetCore.Dojo.Tests.Classes.Joey.TddAndRefactor2005
+namespace Marsen.NetCore.Dojo.Tests.Classes.Joey.TddAndRefactor2005.Plus
 {
     public class AlphabetTests
     {
