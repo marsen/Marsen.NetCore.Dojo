6c71852a31f97dbff15cd21347220dc8939ad373
Marsen <thisismysoul@gmail.com>
2020-05-11 17:17:13 +0800

    refactor test

--- test/Marsen.NetCore.Dojo.Tests/Classes/Joey/TddAndRefactor2005/Plus/CharRepeaterTest.cs
+++ test/Marsen.NetCore.Dojo.Tests/Classes/Joey/TddAndRefactor2005/Plus/CharRepeaterTest.cs
@@ -13,25 +13,25 @@ namespace Marsen.NetCore.Dojo.Tests.Classes.Joey.TddAndRefactor2005.Plus
         }
 
         [Fact]
-        public void The_A_GetA()
+        public void The_A_Should_Be_A()
         {
             AfterRepeat("A").ShouldBe("A");
         }
 
         [Fact]
-        public void The_b_GetB()
+        public void The_b_Should_Be_B()
         {
             AfterRepeat("b").ShouldBe("B");
         }
 
         [Fact]
-        public void The_bb_GetB_Bb()
+        public void The_bb_Should_Be_B_Bb()
         {
             AfterRepeat("bb").ShouldBe("B-Bb");
         }
 
         [Fact]
-        public void The_ab_GetA_Bb()
+        public void The_ab_Should_Be_A_Bb()
         {
             AfterRepeat("ab").ShouldBe("A-Bb");
         }
