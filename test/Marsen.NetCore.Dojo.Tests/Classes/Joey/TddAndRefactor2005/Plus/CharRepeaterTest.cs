75e88bd50f6d01ad8d6ec206d4535771888d06f3
Marsen <thisismysoul@gmail.com>
2020-05-11 16:36:06 +0800

    test A should return A Pass

--- test/Marsen.NetCore.Dojo.Tests/Classes/Joey/TddAndRefactor2005/Plus/CharRepeaterTest.cs
+++ test/Marsen.NetCore.Dojo.Tests/Classes/Joey/TddAndRefactor2005/Plus/CharRepeaterTest.cs
@@ -19,7 +19,7 @@ namespace Marsen.NetCore.Dojo.Tests.Classes.Joey.TddAndRefactor2005.Plus
     {
         public string Repeat(string s)
         {
-            throw new System.NotImplementedException();
+            return "A";
         }
     }
 }
