1ef3fa267b16b5e0531d19e110dbf397334c9866
Marsen <thisismysoul@gmail.com>
2020-05-11 17:06:50 +0800

    test bb should be Be failed but use for loop

--- test/Marsen.NetCore.Dojo.Tests/Classes/Joey/TddAndRefactor2005/Plus/CharRepeater.cs
+++ test/Marsen.NetCore.Dojo.Tests/Classes/Joey/TddAndRefactor2005/Plus/CharRepeater.cs
@@ -1,9 +1,18 @@
-﻿namespace Marsen.NetCore.Dojo.Tests.Classes.Joey.TddAndRefactor2005.Plus
+﻿using NSubstitute.Core;
+
+namespace Marsen.NetCore.Dojo.Tests.Classes.Joey.TddAndRefactor2005.Plus
 {
     public class CharRepeater
     {
         public string Repeat(string input)
-        { 
+        {
+            var result = string.Empty;
+            for (var i = 0; i < input.Length; i++)
+            {
+                result += input[i].ToString().ToUpper();
+            }
+
+            return result;
             return input.ToUpper();
         }
     }
