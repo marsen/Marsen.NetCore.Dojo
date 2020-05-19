1842cb7bc0c8bd527982171453fea93d4000acff
Marsen <thisismysoul@gmail.com>
2020-05-19 15:30:20 +0800

    a aa should be 2 Red

--- test/Marsen.NetCore.Dojo.Tests/LeetCode/JewelsAndStones/JewelsAndStonesTests.cs
+++ test/Marsen.NetCore.Dojo.Tests/LeetCode/JewelsAndStones/JewelsAndStonesTests.cs
@@ -19,12 +19,22 @@ namespace Marsen.NetCore.Dojo.Tests.LeetCode.JewelsAndStones
         {
             ShouldBe("a", "b", 0);
         }
+        
         [Fact]
         public void a_a_ShouldBe_1()
         {
             ShouldBe("a", "a", 1);
         }
+        
+        [Fact]
+        public void a_aa_ShouldBe_2()
+        {
+            ShouldBe("a", "aa", 2);
+        }
 
+
+
+        
         private void ShouldBe(string jewels, string stones, int expected)
         {
             Assert.Equal(expected, _jewelsSelector.Filter(jewels, stones));
