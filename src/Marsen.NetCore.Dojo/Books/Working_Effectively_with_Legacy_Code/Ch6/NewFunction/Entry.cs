ac67b2eae3630f647815f1c825b184f49580036c
Marsen <thisismysoul@gmail.com>
2020-05-26 18:10:48 +0800

    Ch6 new function

---
+++ src/Marsen.NetCore.Dojo/Books/Working_Effectively_with_Legacy_Code/Ch6/NewFunction/Entry.cs
@@ -0,0 +1,9 @@
+﻿namespace Marsen.NetCore.Dojo.Books.Working_Effectively_with_Legacy_Code.Ch6.NewFunction
+{
+    public class Entry
+    {
+        public void postData()
+        {
+        }
+    }
+}
---
+++ src/Marsen.NetCore.Dojo/Books/Working_Effectively_with_Legacy_Code/Ch6/NewFunction/TransactionGate.Legacy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Marsen.NetCore.Dojo.Books.Working_Effectively_with_Legacy_Code.Ch6.NewFunction
+{
+    public class TransactionGate
+    {
+        public void postEntries(List<Entry> entries)
+        {
+            foreach (var entry in entries)
+            {
+                entry.postData();
+            }
+        }
+    }
+}
