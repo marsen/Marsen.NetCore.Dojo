81aadec064726e457fd31d910e14b6414da09f26
Marsen <thisismysoul@gmail.com>
2020-06-08 10:58:34 +0800

    delete

--- src/Marsen.NetCore.Dojo/Books/Working_Effectively_with_Legacy_Code/Ch6/NewFunction/Entry.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Marsen.NetCore.Dojo.Books.Working_Effectively_with_Legacy_Code.Ch6.NewFunction
-{
-    public class Entry
-    {
-        public void postData()
-        {
-        }
-    }
-}
