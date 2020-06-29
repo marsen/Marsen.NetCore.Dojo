8e8f5a38544b0c861466e1fb8d41ba4bfd5dd911
Marsen <thisismysoul@gmail.com>
2020-06-29 22:39:02 +0800

    refactor

--- test/Marsen.NetCore.Dojo.Tests/LeetCode/FindTheMedian/FindTheMedianTests.cs
+++ test/Marsen.NetCore.Dojo.Tests/LeetCode/FindTheMedian/FindTheMedianTests.cs
@@ -1,22 +1,35 @@
-using System;
 using Xunit;
 
 namespace Marsen.NetCore.Dojo.Tests.LeetCode.FindTheMedian
 {
     public class FindTheMedianTests
     {
-        readonly ArrayFinder _target = new ArrayFinder();
+        private readonly ArrayFinder _target = new ArrayFinder();
+
+        private int[] _array;
+
+        private void GiveTheArrayIs(int[] array)
+        {
+            _array = array;
+        }
+
+        private void ShouldBe(int median)
+        {
+            Assert.Equal(median, _target.Median(_array));
+        }
 
         [Fact]
         public void arr_1_should_be_1()
         {
-            Assert.Equal(1, _target.Median(new[] {1}));
+            GiveTheArrayIs(new[] {1});
+            ShouldBe(1);
         }
 
         [Fact]
         public void arr_2_should_be_2()
         {
-            Assert.Equal(2, actual: _target.Median(new[] {2}));
+            GiveTheArrayIs(new[] {2});
+            ShouldBe(2);
         }
     }
 
