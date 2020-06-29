73f0da06ced66036919f31b416e437257c659b5d
Marsen <thisismysoul@gmail.com>
2020-06-29 22:14:16 +0800

    arr [2] should be 2

--- test/Marsen.NetCore.Dojo.Tests/LeetCode/FindTheMedian/FindTheMedianTests.cs
+++ test/Marsen.NetCore.Dojo.Tests/LeetCode/FindTheMedian/FindTheMedianTests.cs
@@ -12,6 +12,12 @@ namespace Marsen.NetCore.Dojo.Tests.LeetCode.FindTheMedian
         {
             Assert.Equal(1, _target.Median(new int[1]));
         }
+
+        [Fact]
+        public void arr_2_should_be_2()
+        {
+            Assert.Equal(2, _target.Median(new int[2]));
+        }
     }
 
     public class ArrayFinder
