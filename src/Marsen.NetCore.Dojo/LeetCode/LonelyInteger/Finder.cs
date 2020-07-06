856a12bc31c690f7f1c2983920c62ce5411dba27
Marsen <thisismysoul@gmail.com>
2020-07-06 11:30:37 +0800

    lonely finder reafactory

--- src/Marsen.NetCore.Dojo/LeetCode/LonelyInteger/Finder.cs
+++ src/Marsen.NetCore.Dojo/LeetCode/LonelyInteger/Finder.cs
@@ -6,7 +6,9 @@ namespace Marsen.NetCore.Dojo.LeetCode.LonelyInteger
     {
         public int Get(int[] array)
         {
-            return array.GroupBy(x => x).First(x => x.Count() == 1).Key;
+            return array.GroupBy(x => x)
+                .First(x => x.Count() == 1)
+                .Key;
         }
     }
 }
