ed2b325b8e3fe5827e268d3618f9ded40a7642d8
Marsen <thisismysoul@gmail.com>
2020-07-06 11:14:22 +0800

    refactor

--- test/Marsen.NetCore.Dojo.Tests/LeetCode/LonelyInteger/FinderTests.cs
+++ test/Marsen.NetCore.Dojo.Tests/LeetCode/LonelyInteger/FinderTests.cs
@@ -11,45 +11,44 @@ namespace Marsen.NetCore.Dojo.Tests.LeetCode.LonelyInteger
         [Fact]
         public void arr_1_should_be_1()
         {
-            GivenArrayAs(new[] {1});
+            GivenArrayAs(1);
             ShouldBe(1);
         }
 
         [Fact]
         public void arr_2_should_be_2()
         {
-            GivenArrayAs(new[] {2});
+            GivenArrayAs(2);
             ShouldBe(2);
         }
 
         [Fact]
         public void arr_2_2_3_should_be_3()
         {
-            GivenArrayAs(new[] {2, 2, 3});
+            GivenArrayAs(2, 2, 3);
             ShouldBe(3);
         }
 
         [Fact]
         public void arr_2_3_2_should_be_3()
         {
-            GivenArrayAs(new[] {2, 3, 2});
+            GivenArrayAs(2, 3, 2);
             ShouldBe(3);
         }
 
         [Fact]
         public void arr_1_2_3_4_3_2_1_should_be_4()
         {
-            GivenArrayAs(new[] {1, 2, 3, 4, 3, 2, 1});
+            GivenArrayAs(1, 2, 3, 4, 3, 2, 1);
             ShouldBe(4);
         }
-
 
         private void ShouldBe(int expected)
         {
             Assert.Equal(expected, _finder.Get(_array));
         }
 
-        private void GivenArrayAs(int[] array)
+        private void GivenArrayAs(params int[] array)
         {
             _array = array;
         }
