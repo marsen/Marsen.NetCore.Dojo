2d19e772846edaf6a04e00c6115b5d9a0cf4f5d1
Marsen <thisismysoul@gmail.com>
2020-07-06 11:15:49 +0800

    refactor parameters

--- test/Marsen.NetCore.Dojo.Tests/LeetCode/FindTheMedian/FindTheMedianTests.cs
+++ test/Marsen.NetCore.Dojo.Tests/LeetCode/FindTheMedian/FindTheMedianTests.cs
@@ -9,7 +9,7 @@ namespace Marsen.NetCore.Dojo.Tests.LeetCode.FindTheMedian
 
         private int[] _array;
 
-        private void GiveTheArrayIs(int[] array)
+        private void GiveArrayAs(params int[] array)
         {
             _array = array;
         }
@@ -22,35 +22,35 @@ namespace Marsen.NetCore.Dojo.Tests.LeetCode.FindTheMedian
         [Fact]
         public void arr_1_should_be_1()
         {
-            GiveTheArrayIs(new[] {1});
+            GiveArrayAs(1);
             ShouldBe(1);
         }
 
         [Fact]
         public void arr_2_should_be_2()
         {
-            GiveTheArrayIs(new[] {2});
+            GiveArrayAs(2);
             ShouldBe(2);
         }
 
         [Fact]
         public void arr_1_2_3_should_be_2()
         {
-            GiveTheArrayIs(new[] {1, 2, 3});
+            GiveArrayAs(1, 2, 3);
             ShouldBe(2);
         }
 
         [Fact]
         public void arr_2_3_1_should_be_2()
         {
-            GiveTheArrayIs(new[] {2, 3, 1});
+            GiveArrayAs(2, 3, 1);
             ShouldBe(2);
         }
 
         [Fact]
         public void arr_0_1_2_4_6_5_3_should_be_3()
         {
-            GiveTheArrayIs(new[] {0, 1, 2, 4, 6, 5, 3});
+            GiveArrayAs(0, 1, 2, 4, 6, 5, 3);
             ShouldBe(3);
         }
     }
