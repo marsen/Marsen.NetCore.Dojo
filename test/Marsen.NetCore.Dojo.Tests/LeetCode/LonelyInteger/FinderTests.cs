829e8ea53ec966554a65bf33acdc5d3c85811791
Marsen <thisismysoul@gmail.com>
2020-07-06 10:27:30 +0800

    arr 1 should be 1

---
+++ test/Marsen.NetCore.Dojo.Tests/LeetCode/LonelyInteger/FinderTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using Xunit;
+
+namespace Marsen.NetCore.Dojo.Tests.LeetCode.LonelyInteger
+{
+    public class FinderTests
+    {
+        [Fact]
+        public void arr_1_should_be_1()
+        {
+            var arr = new[] {1};
+            var finder = new Finder();
+            int result = finder.Get(arr);
+            Assert.Equal(1,result);
+        }
+    }
+
+    public class Finder
+    {
+        public int Get(int[] arr)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
