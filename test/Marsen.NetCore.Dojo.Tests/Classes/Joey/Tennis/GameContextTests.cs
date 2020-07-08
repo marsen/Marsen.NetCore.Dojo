62896b80ba170d494d5c9fc63360580062df1fb3
Marsen <thisismysoul@gmail.com>
2020-07-08 16:20:00 +0800

    Fifteen Love Red

---
+++ test/Marsen.NetCore.Dojo.Tests/Classes/Joey/Tennis/GameContextTests.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+
+namespace Marsen.NetCore.Dojo.Tests.Classes.Joey.Tennis
+{
+    public class GameContextTests
+    {
+        [Fact]
+        public void FifteenLove()
+        {
+            GameContext game = new GameContext();
+            string result = game.ServerScore();
+            Assert.Equal("Fifteen Love",result);
+        }
+    }
+
+    public class GameContext
+    {
+        public string ServerScore()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
