66dd5220344979741c5a7b2e39e1bf82192866ef
Marsen <thisismysoul@gmail.com>
2020-07-17 17:58:57 +0800

    Mark win refactor

--- test/Marsen.NetCore.Dojo.Tests/Classes/Joey/Tennis/States/AdvState.cs
+++ test/Marsen.NetCore.Dojo.Tests/Classes/Joey/Tennis/States/AdvState.cs
@@ -33,23 +33,4 @@ namespace Marsen.NetCore.Dojo.Tests.Classes.Joey.Tennis.States
             this.Context.ChangeState(state);
         }
     }
-
-    internal class WinState : State
-    {
-        public override string Score()
-        {
-            return $"{Winner()} Win";
-        }
-
-
-        public override void ServerScore()
-        {
-            throw new NotImplementedException();
-        }
-
-        public override void ReceiverScore()
-        {
-            throw new NotImplementedException();
-        }
-    }
 }
---
+++ test/Marsen.NetCore.Dojo.Tests/Classes/Joey/Tennis/States/WinState.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Marsen.NetCore.Dojo.Tests.Classes.Joey.Tennis.States
+{
+    public class WinState : State
+    {
+        public override string Score()
+        {
+            return $"{Winner()} Win";
+        }
+
+
+        public override void ServerScore()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ReceiverScore()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
