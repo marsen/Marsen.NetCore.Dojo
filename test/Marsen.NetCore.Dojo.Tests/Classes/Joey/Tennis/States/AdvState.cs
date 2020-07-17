e7b349c2cdea4137674d68837e71e24068b3a2b9
Marsen <thisismysoul@gmail.com>
2020-07-17 17:55:22 +0800

    Iris win pass

--- test/Marsen.NetCore.Dojo.Tests/Classes/Joey/Tennis/States/AdvState.cs
+++ test/Marsen.NetCore.Dojo.Tests/Classes/Joey/Tennis/States/AdvState.cs
@@ -1,4 +1,6 @@
-﻿namespace Marsen.NetCore.Dojo.Tests.Classes.Joey.Tennis.States
+﻿using System;
+
+namespace Marsen.NetCore.Dojo.Tests.Classes.Joey.Tennis.States
 {
     public class AdvState : State
     {
@@ -29,8 +31,30 @@
         private void ChangeState()
         {
             State state = (Context.ServerPoint == Context.ReceiverPoint) ? (State) new DeuceState() : new AdvState();
+            if (Math.Abs(Context.ServerPoint-Context.ReceiverPoint)==2)
+            {
+                state = new WinState();
+            }
             state.SetContext(this.Context);
             this.Context.ChangeState(state);
+        }
+    }
+
+    internal class WinState : State
+    {
+        public override string Score()
+        {
+            return "Iris Win";
+        }
+
+        public override void ServerScore()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ReceiverScore()
+        {
+            throw new NotImplementedException();
         }
     }
 }
