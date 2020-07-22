62351bd79bead80f852a7b74896bba0da7cb406b
Marsen <thisismysoul@gmail.com>
2020-07-22 15:05:44 +0800

    add loading and empty

--- src/marsen.react/src/components/TaskList.js
+++ src/marsen.react/src/components/TaskList.js
@@ -8,12 +8,38 @@ function TaskList({ loading, tasks, onPinTask, onArchiveTask }) {
     onArchiveTask,
   };
 
-  if (loading) {
-    return <div className="list-items">loading</div>;
+  const LoadingRow = (
+    <div className="loading-item">
+      <span className="glow-checkbox" />
+      <span className="glow-text">
+        <span>Loading</span> <span>cool</span> <span>state</span>
+      </span>
+    </div>
+  );
+
+  if (loading) {    
+    return (
+      <div className="list-items">
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+      </div>
+    );
   }
 
   if (tasks.length === 0) {
-    return <div className="list-items">empty</div>;
+    return (
+        <div className="list-items">
+            <div className="wrapper-message">
+                <span className="icon-check" />
+                <div className="title-message">You have no tasks</div>
+                <div className="subtitle-message">Sit back and relax</div>
+            </div>
+        </div>
+    );
   }
 
   return (
