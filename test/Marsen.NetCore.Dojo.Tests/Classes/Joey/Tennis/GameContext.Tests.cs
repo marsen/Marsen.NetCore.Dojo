d792b2ebf1d94e588aff4033f30927ec29024da0
Marsen <thisismysoul@gmail.com>
2020-07-23 11:23:24 +0800

    Love All Fail

---
+++ test/Marsen.NetCore.Dojo.Tests/Classes/Joey/Tennis/GameContext.Tests.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace Marsen.NetCore.Dojo.Tests.Classes.Joey.Tennis
+{
+    public class GameContextTests
+    {
+        [Fact]
+        public void LoveAll()
+        {
+            var context = new GameContext();
+            Assert.Equal("Love All", context.Score());
+        }
+
+    }
+
+    public class GameContext
+    {
+        public string Score()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
