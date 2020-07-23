14f9ed39cb308e0c16eb48da5ee6f1d30d778ad9
Marsen <thisismysoul@gmail.com>
2020-07-23 14:59:38 +0800

    Fifteen Love Pass

--- test/Marsen.NetCore.Dojo.Tests/Classes/Joey/Tennis/GameContext.Tests.cs
+++ test/Marsen.NetCore.Dojo.Tests/Classes/Joey/Tennis/GameContext.Tests.cs
@@ -20,6 +20,14 @@ namespace Marsen.NetCore.Dojo.Tests.Classes.Joey.Tennis
             ScoreShouldBe("Love Fifteen");
         }
 
+        [Fact]
+        public void FifteenLove()
+        {
+            _context.ServerScore();
+            ScoreShouldBe("Fifteen Love");
+        }
+
+
         private void ScoreShouldBe(string expected)
         {
             Assert.Equal(expected, _context.Score());
@@ -37,6 +45,10 @@ namespace Marsen.NetCore.Dojo.Tests.Classes.Joey.Tennis
 
         public string Score()
         {
+            if (ServerPoint == 1)
+            {
+                return "Fifteen Love";
+            }
             if (ReceiverPoint == 1)
             {
                 _state = new NormalState();
@@ -52,6 +64,13 @@ namespace Marsen.NetCore.Dojo.Tests.Classes.Joey.Tennis
         }
 
         private int ReceiverPoint { get; set; }
+
+        public void ServerScore()
+        {
+            ServerPoint++;
+        }
+
+        private int ServerPoint { get; set; }
     }
 
     public class NormalState : IState
