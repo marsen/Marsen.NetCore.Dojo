3a86ab4662d6fc1b4a9bc5ab035d6673a37c5788
Marsen <thisismysoul@gmail.com>
2020-07-27 10:25:30 +0800

    Refactor

--- test/Marsen.NetCore.Dojo.Tests/Classes/Joey/Tennis/GameContext.Tests.cs
+++ test/Marsen.NetCore.Dojo.Tests/Classes/Joey/Tennis/GameContext.Tests.cs
@@ -283,7 +283,7 @@ namespace Marsen.NetCore.Dojo.Tests.Classes.Joey.Tennis
 
         protected override void ChangeState()
         {
-            if (Context.ReceiverPoint - Context.ServerPoint >= 2 || Context.ServerPoint - Context.ReceiverPoint >= 2)
+            if (Math.Abs(Context.ServerPoint - Context.ReceiverPoint) >= 2)
             {
                 Context.ChangeState(new WinState());
             }
