8a93b074a2a6e8a87c9919311d31dafbbea6f1a9
Marsen <thisismysoul@gmail.com>
2020-07-30 13:44:28 +0800

    refactor iswin

--- src/Marsen.TypeScript.Dojo/src/Kata/NormalState.ts
+++ src/Marsen.TypeScript.Dojo/src/Kata/NormalState.ts
@@ -5,16 +5,18 @@ import { DeuceState } from "./DeuceState";
 
 export class NormalState extends State {
     ChangeState() {
-
         if (this.IsSame()) {
             this.Context.State = this.Context.ServerPoint < 3 ? new SameState() : new DeuceState();        
         }
 
-        if (this.Context.ServerPoint > 3 || this.Context.ReceiverPoint > 3) {
+        if (this.IsWin()) {
             this.Context.State = new WinState();
         }
     }
 
+    private IsWin() {
+        return this.Context.ServerPoint > 3 || this.Context.ReceiverPoint > 3;
+    }
 
     Score(): string {
         return `${this.ScoreLookup.get(this.Context.ServerPoint)} ${this.ScoreLookup.get(this.Context.ReceiverPoint)}`;
