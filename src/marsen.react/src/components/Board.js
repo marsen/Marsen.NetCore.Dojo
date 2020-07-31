8189945316707ecbb7da7929f81389789e2d71d3
Marsen <thisismysoul@gmail.com>
2020-07-31 17:55:35 +0800

    pass prop

--- src/marsen.react/src/components/Board.js
+++ src/marsen.react/src/components/Board.js
@@ -2,8 +2,24 @@ import React from 'react';
 import Square from './Square';
 
 export default class Board extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+        };
+    }
+
     renderSquare(i) {
-        return <Square value={i} />;
+        return <Square 
+            value={this.state.squares[i]} 
+            onClick={() => this.handleClick(i)}
+            />;
+    }
+
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares: squares});
     }
 
     render() {
@@ -13,19 +29,13 @@ export default class Board extends React.Component {
             <div>
                 <div className="status">{status}</div>
                 <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
+                    {this.renderSquare(0)}{this.renderSquare(1)}{this.renderSquare(2)}
                 </div>
                 <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
+                    {this.renderSquare(3)}{this.renderSquare(4)}{this.renderSquare(5)}
                 </div>
                 <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
+                    {this.renderSquare(6)}{this.renderSquare(7)}{this.renderSquare(8)}
                 </div>
             </div>
         );
--- src/marsen.react/src/components/Square.js
+++ src/marsen.react/src/components/Square.js
@@ -3,7 +3,9 @@ import React from 'react';
 export default class Square extends React.Component {
     render() {
       return (
-        <button className="square">          
+        <button 
+        className="square" 
+        onClick={()=> this.props.onClick() }>
             {this.props.value}      
         </button>
       );
