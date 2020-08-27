3c57ca20dee085ec295885771cfbbb00fe2ff6ba
Marsen <thisismysoul@gmail.com>
2020-08-27 09:53:01 +0800

    Add Test Case HandsetAddressBook Run

--- src/Marsen.NetCore.Dojo/Books/TalkAboutDesignPattern/BridgePattern/LegacyHandsetSoft/HandsetAddressBook.cs
+++ src/Marsen.NetCore.Dojo/Books/TalkAboutDesignPattern/BridgePattern/LegacyHandsetSoft/HandsetAddressBook.cs
@@ -1,12 +1,15 @@
 using System;
+using Marsen.NetCore.TestingToolkit;
 
 namespace Marsen.NetCore.Dojo.Books.TalkAboutDesignPattern.BridgePattern.LegacyHandsetSoft
 {
     public class HandsetAddressBook : HandsetSoft
     {
+        public readonly SystemConsole Console = new SystemConsole();
+
         public override void Run(string brand)
         {
-            Console.WriteLine($"Run {brand} Address Book"); 
+            Console.WriteLine($"Run {brand} Address Book");
         }
     }
 }
---
+++ test/Marsen.NetCore.Dojo.Tests/Books/TalkAboutDesignPattern/LegacyHandsetSoftTests.cs
@@ -0,0 +1,23 @@
+﻿using Marsen.NetCore.Dojo.Books.TalkAboutDesignPattern.BridgePattern.LegacyHandsetSoft;
+using Marsen.NetCore.TestingToolkit;
+using Xunit;
+
+namespace Marsen.NetCore.Dojo.Tests.Books.TalkAboutDesignPattern
+{
+    public class LegacyHandsetSoftTests
+    {
+        [Fact]
+        public void HandsetAddressBook_Run()
+        {
+            var target = new HandsetAddressBook();
+            target.Run("Mark");
+            ConsoleWriteLineShouldBeCall(target.Console,"Run Mark Address Book");
+        }
+        
+        private void ConsoleWriteLineShouldBeCall(SystemConsole console, string message)
+        {
+            Assert.Equal(message, console.Message);
+            Assert.Equal(1, console.WriteLineTimes);
+        }
+    }
+}
