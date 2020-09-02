097c1e5b8f175d689284b75f3864e45408eea254
Marsen <thisismysoul@gmail.com>
2020-09-02 17:00:46 +0800

    woman class statusLookup

--- test/Marsen.NetCore.Dojo.E2E.Tests/Program.cs
+++ test/Marsen.NetCore.Dojo.E2E.Tests/Program.cs
@@ -26,7 +26,6 @@ namespace Marsen.NetCore.Dojo.E2E.Tests
             Console.WriteLine("女人失敗時，眼淚汪汪，誰也勸不了");
             man.Status = "戀愛";
             Console.WriteLine($"男人{man.Status}時，{man.GetConclusion()}");
-            // Console.WriteLine("男人戀愛時，凡事不懂也要裝懂");
             Console.WriteLine("女人戀愛時，遇事懂也裝作不懂");
         }
     }
@@ -35,7 +34,7 @@ namespace Marsen.NetCore.Dojo.E2E.Tests
     {
         private readonly Dictionary<string, string> _statusLookup = new Dictionary<string, string>
         {
-            {"成功", "背後多半有一個偉大的女人"},
+            {"成功", "背後多半有一個不成功的男人"},
             {"失敗", "悶頭喝酒，誰也不用勸"},
             {"戀愛", "凡事不懂也要裝懂"},
         };
