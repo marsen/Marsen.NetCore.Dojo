0d823a38e6f575f775c1852e5d741b6a6d4adf4e
Marsen <thisismysoul@gmail.com>
2020-09-02 15:37:45 +0800

    console.writeline version

--- test/Marsen.NetCore.Dojo.E2E.Tests/Program.cs
+++ test/Marsen.NetCore.Dojo.E2E.Tests/Program.cs
@@ -1,4 +1,5 @@
-﻿using Marsen.NetCore.Dojo.E2E.Tests.Books.TalkAboutDesignPattern.Bridge;
+﻿using System;
+using Marsen.NetCore.Dojo.E2E.Tests.Books.TalkAboutDesignPattern.Bridge;
 
 namespace Marsen.NetCore.Dojo.E2E.Tests
 {
@@ -6,8 +7,12 @@ namespace Marsen.NetCore.Dojo.E2E.Tests
     {
         private static void Main(string[] args)
         {
-            // Brand.Start();
-            Soft.Start();
+            Console.WriteLine("男人成功時，背後多半有一個偉大的女人");
+            Console.WriteLine("女人成功時，背後多半有一個不成功的男人");
+            Console.WriteLine("男人失敗時，悶頭喝酒，誰也不用勸");
+            Console.WriteLine("女人失敗時，眼淚汪汪，誰也勸不了");
+            Console.WriteLine("男人戀愛時，凡事不懂也要裝懂");
+            Console.WriteLine("女人戀愛時，遇事懂也裝作不懂");
         }
     }
 }
