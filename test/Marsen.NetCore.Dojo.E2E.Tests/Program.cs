926a716c6a5f14618a37ca10e7505bf9011e8131
Marsen <thisismysoul@gmail.com>
2020-09-02 15:59:21 +0800

    create man class

--- test/Marsen.NetCore.Dojo.E2E.Tests/Program.cs
+++ test/Marsen.NetCore.Dojo.E2E.Tests/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Marsen.NetCore.Dojo.E2E.Tests.Books.TalkAboutDesignPattern.Bridge;
 
 namespace Marsen.NetCore.Dojo.E2E.Tests
@@ -13,6 +14,35 @@ namespace Marsen.NetCore.Dojo.E2E.Tests
             Console.WriteLine("女人失敗時，眼淚汪汪，誰也勸不了");
             Console.WriteLine("男人戀愛時，凡事不懂也要裝懂");
             Console.WriteLine("女人戀愛時，遇事懂也裝作不懂");
+            Console.WriteLine("-----");
+            var man = new Man();
+            man.Status = "成功";
+            Console.WriteLine($"男人{man.Status}時，{man.GetConclusion()}");
+            Console.WriteLine("女人成功時，背後多半有一個不成功的男人");
+            var man2 = new Man();
+            man2.Status = "失敗";
+            Console.WriteLine($"男人{man2.Status}時，悶頭喝酒，誰也不用勸");
+            Console.WriteLine("女人失敗時，眼淚汪汪，誰也勸不了");
+            var man3 = new Man();
+            man3.Status = "戀愛";
+            Console.WriteLine("男人戀愛時，凡事不懂也要裝懂");
+            Console.WriteLine("女人戀愛時，遇事懂也裝作不懂");
+        }
+    }
+
+    internal class Man
+    {
+        Dictionary<string, string> StatusLookup = new Dictionary<string, string> {
+            {
+                "成功","背後多半有一個偉大的女人"
+            }
+        };
+
+        public string Status { get; set; }
+
+        public string GetConclusion()
+        {
+            return StatusLookup[Status];
         }
     }
 }
