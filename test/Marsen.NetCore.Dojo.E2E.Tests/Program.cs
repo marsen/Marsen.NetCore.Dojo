14057ae15d694bd4a809f0e161239c67e280011a
Marsen <thisismysoul@gmail.com>
2020-09-02 16:58:04 +0800

    woman class

--- test/Marsen.NetCore.Dojo.E2E.Tests/Program.cs
+++ test/Marsen.NetCore.Dojo.E2E.Tests/Program.cs
@@ -18,14 +18,33 @@ namespace Marsen.NetCore.Dojo.E2E.Tests
             var man = new Man();
             man.Status = "成功";
             Console.WriteLine($"男人{man.Status}時，{man.GetConclusion()}");
-            Console.WriteLine("女人成功時，背後多半有一個不成功的男人");
+            var woman = new Woman();
+            woman.Status = "成功";
+            Console.WriteLine($"女人{woman.Status}時，{woman.GetConclusion()}");
             man.Status = "失敗";
             Console.WriteLine($"男人{man.Status}時，{man.GetConclusion()}");
             Console.WriteLine("女人失敗時，眼淚汪汪，誰也勸不了");
-            var man3 = new Man();
-            man3.Status = "戀愛";
-            Console.WriteLine("男人戀愛時，凡事不懂也要裝懂");
+            man.Status = "戀愛";
+            Console.WriteLine($"男人{man.Status}時，{man.GetConclusion()}");
+            // Console.WriteLine("男人戀愛時，凡事不懂也要裝懂");
             Console.WriteLine("女人戀愛時，遇事懂也裝作不懂");
+        }
+    }
+
+    internal class Woman
+    {
+        private readonly Dictionary<string, string> _statusLookup = new Dictionary<string, string>
+        {
+            {"成功", "背後多半有一個偉大的女人"},
+            {"失敗", "悶頭喝酒，誰也不用勸"},
+            {"戀愛", "凡事不懂也要裝懂"},
+        };
+
+        public string Status { get; set; }
+
+        public string GetConclusion()
+        {
+            return _statusLookup[Status];
         }
     }
 
@@ -35,6 +54,7 @@ namespace Marsen.NetCore.Dojo.E2E.Tests
         {
             {"成功", "背後多半有一個偉大的女人"},
             {"失敗", "悶頭喝酒，誰也不用勸"},
+            {"戀愛", "凡事不懂也要裝懂"},
         };
 
         public string Status { get; set; }
