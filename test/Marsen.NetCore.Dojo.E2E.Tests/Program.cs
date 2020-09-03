2fd004d7864b546b3dddea68f45b7b560e0b6024
Marsen <thisismysoul@gmail.com>
2020-09-03 11:08:56 +0800

    override field

--- test/Marsen.NetCore.Dojo.E2E.Tests/Program.cs
+++ test/Marsen.NetCore.Dojo.E2E.Tests/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using Marsen.NetCore.Dojo.E2E.Tests.Books.TalkAboutDesignPattern.Bridge;
 
 namespace Marsen.NetCore.Dojo.E2E.Tests
 {
@@ -15,10 +14,10 @@ namespace Marsen.NetCore.Dojo.E2E.Tests
             Console.WriteLine("男人戀愛時，凡事不懂也要裝懂");
             Console.WriteLine("女人戀愛時，遇事懂也裝作不懂");
             Console.WriteLine("-----");
-            var man = new Man();
+            Person man = new Man();
             man.Status = "成功";
             Console.WriteLine($"男人{man.Status}時，{man.GetConclusion()}");
-            var woman = new Woman();
+            Person woman = new Woman();
             woman.Status = "成功";
             Console.WriteLine($"女人{woman.Status}時，{woman.GetConclusion()}");
             man.Status = "失敗";
@@ -32,37 +31,36 @@ namespace Marsen.NetCore.Dojo.E2E.Tests
         }
     }
 
-    internal class Woman
+    internal class Woman : Person
     {
-        private readonly Dictionary<string, string> _statusLookup = new Dictionary<string, string>
-        {
-            {"成功", "背後多半有一個不成功的男人"},
-            {"失敗", "眼淚汪汪，誰也勸不了"},
-            {"戀愛", "遇事懂也裝作不懂"},
-        };
+        protected override Dictionary<string, string> StatusLookup =>
+            new Dictionary<string, string>
+            {
+                {"成功", "背後多半有一個不成功的男人"},
+                {"失敗", "眼淚汪汪，誰也勸不了"},
+                {"戀愛", "遇事懂也裝作不懂"},
+            };
+    }
 
+    internal abstract class Person
+    {
+        protected abstract Dictionary<string, string> StatusLookup { get; }
         public string Status { get; set; }
 
         public string GetConclusion()
         {
-            return _statusLookup[Status];
+            return StatusLookup[Status];
         }
     }
 
-    internal class Man
+    internal class Man : Person
     {
-        private readonly Dictionary<string, string> _statusLookup = new Dictionary<string, string>
-        {
-            {"成功", "背後多半有一個偉大的女人"},
-            {"失敗", "悶頭喝酒，誰也不用勸"},
-            {"戀愛", "凡事不懂也要裝懂"},
-        };
-
-        public string Status { get; set; }
-
-        public string GetConclusion()
-        {
-            return _statusLookup[Status];
-        }
+        protected override Dictionary<string, string> StatusLookup =>
+            new Dictionary<string, string>
+            {
+                {"成功", "背後多半有一個偉大的女人"},
+                {"失敗", "悶頭喝酒，誰也不用勸"},
+                {"戀愛", "凡事不懂也要裝懂"},
+            };
     }
 }
