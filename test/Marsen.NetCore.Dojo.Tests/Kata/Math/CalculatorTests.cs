63f6c8baa4a9cfb2506139136bf251c883d903ed
Marsen <thisismysoul@gmail.com>
2020-10-14 11:05:36 +0800

    Calculator red

---
+++ test/Marsen.NetCore.Dojo.Tests/Kata/Math/CalculatorTests.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+
+namespace Marsen.NetCore.Dojo.Tests.Kata.Math
+{
+    public class CalculatorTests
+    {
+
+        [Fact]
+        public void Add_1_1_is_2()
+        {
+            var target = new Calculator();
+            Assert.Equal(2,target.Add(1,1));
+        }
+    }
+
+    public class Calculator
+    {
+        public int Add(int first, int second)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
