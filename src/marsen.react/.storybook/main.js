5a64ad4be20496f47e1f36d8fcd78036c5ac257d
Marsen <thisismysoul@gmail.com>
2020-10-15 16:36:21 +0800

    rename files

--- src/marsen.react/.storybook/main.js
+++ src/marsen.react/.storybook/main.js
@@ -1,8 +1,8 @@
 module.exports = {
-  stories: ['../src/components/*.stories.js','../src/components/TicTacToe/*.stories.js'],
+  stories: ['../src/components/*.stories.jsx','../src/components/TicTacToe/*.stories.jsx'],
   addons: [
     '@storybook/preset-create-react-app',
     '@storybook/addon-actions',
-    '@storybook/addon-links',
+    '@storybook/addon-links',    
   ],
 };
