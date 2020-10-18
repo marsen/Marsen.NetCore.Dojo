3cf1c6385c5d98726e351a7cbaec54cee8da5de2
Marsen <thisismysoul@gmail.com>
2020-10-18 16:21:21 +0800

    field

--- test/Marsen.NetCore.Dojo.Tests/Kata/Math/CalculatorTests.cs
+++ test/Marsen.NetCore.Dojo.Tests/Kata/Math/CalculatorTests.cs
@@ -4,25 +4,24 @@ namespace Marsen.NetCore.Dojo.Tests.Kata.Math
 {
     public class CalculatorTests
     {
+        private readonly Calculator _target = new Calculator();
+
         [Fact]
         public void Add_1_1_is_2()
         {
-            var target = new Calculator();
-            Assert.Equal(2, target.Add(1, 1));
+            Assert.Equal(2, _target.Add(1, 1));
         }
 
         [Fact]
         public void Add_2_1_is_3()
         {
-            var target = new Calculator();
-            Assert.Equal(3, target.Add(2, 1));
+            Assert.Equal(3, _target.Add(2, 1));
         }
 
         [Fact]
         public void Subtract_2_1_is_3()
         {
-            var target = new Calculator();
-            Assert.Equal(1, target.Subtract(2, 1));
+            Assert.Equal(1, _target.Subtract(2, 1));
         }
     }
 
