fe5480ba3b276a65ad8ccd08be5afb831b3b078a
Marsen <thisismysoul@gmail.com>
2020-11-12 17:13:34 +0800

    add test red

---
+++ test/Marsen.NetCore.Dojo.Integration.Tests/Classes/GOOS/GreetingServerE2ETest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Http;
+using Xunit;
+
+namespace Marsen.NetCore.Dojo.Integration.Tests.Classes.GOOS
+{
+    public class GreetingServerE2ETest
+    {
+        [Fact]
+        public void Should_Greet_With_Hello_World()
+        {
+            GreetingServer.main();
+            var httpClient = new HttpClient {BaseAddress = new Uri("http://localhost:8080/")};
+            var response = httpClient.GetAsync("posts").Result;
+            response.EnsureSuccessStatusCode();
+            var result = response.Content.ReadAsStringAsync().Result;
+            Assert.Equal("Hello World",result);
+        }
+        
+    }
+
+    public static class GreetingServer
+    {
+        public static void main(params string[] args)
+        {
+        }
+    }
+}
