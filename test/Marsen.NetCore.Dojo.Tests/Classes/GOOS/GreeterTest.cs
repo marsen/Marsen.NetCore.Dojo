315952e63512315b15f65aa8e999af0de68e0019
Marsen <thisismysoul@gmail.com>
2020-11-18 17:48:16 +0800

    add Unit test SleepAt14 (Red)

--- test/Marsen.NetCore.Dojo.Tests/Classes/GOOS/GreeterTest.cs
+++ test/Marsen.NetCore.Dojo.Tests/Classes/GOOS/GreeterTest.cs
@@ -9,7 +9,13 @@ namespace Marsen.NetCore.Dojo.Tests.Classes.GOOS
         [Fact]
         public void GreetByName()
         {
-            Assert.Equal("Hello Jones", new Greeter().Invoke("Jones","9"));
+            Assert.Equal("Hello Jones", new Greeter().Invoke("Jones", "9"));
+        }
+
+        [Fact]
+        public void SleepAt14()
+        {
+            Assert.Equal("Zzz", new Greeter().Invoke("Jones", "14"));
         }
     }
 }
