4420f8fd120332196485487c2861c251f5a6bf96
Marsen <thisismysoul@gmail.com>
2020-11-23 17:10:51 +0800

    add commend

--- src/marsen.react/src/components/Task/TaskList.jsx
+++ src/marsen.react/src/components/Task/TaskList.jsx
@@ -55,9 +55,13 @@ export function PureTaskList({ loading, tasks, onPinTask, onArchiveTask }) {
 }
 
 PureTaskList.propTypes = {
+  /** Checks if it's in loading state */
   loading: PropTypes.bool,
+  /** The list of tasks */
   tasks: PropTypes.arrayOf(Task.propTypes.task).isRequired,
+  /** Event to change the task to pinned */
   onPinTask: PropTypes.func.isRequired,
+  /** Event to change the task to archived */
   onArchiveTask: PropTypes.func.isRequired,
 };
 
