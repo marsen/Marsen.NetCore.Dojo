0ade98bb27b6659b947dad6f641acd5d6e42b45d
Marsen <thisismysoul@gmail.com>
2020-11-23 10:59:34 +0800

    change task

--- src/marsen.react/.storybook/main.js
+++ src/marsen.react/.storybook/main.js
@@ -1,5 +1,8 @@
 module.exports = {
-  stories: ['../src/components/*.stories.jsx','../src/components/TicTacToe/*.stories.jsx'],
+  stories: [
+    '../src/components/*.stories.jsx',
+    '../src/components/Task/*.stories.jsx',
+    '../src/components/TicTacToe/*.stories.jsx'],
   addons: [
     '@storybook/preset-create-react-app',
     '@storybook/addon-actions',
