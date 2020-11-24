0e8c30faf8090feeaa222b57dbb07365efd2d388
Marsen <thisismysoul@gmail.com>
2020-11-24 16:29:51 +0800

    modify App.js

--- src/marsen.react/src/App.js
+++ src/marsen.react/src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
 
+import React from 'react';
+import { Provider } from 'react-redux';
+import store from './lib/redux';
+
+import InboxScreen from './components/Task/InboxScreen';
+
+import './index.css';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <InboxScreen />
+    </Provider>
   );
 }
-
 export default App;
