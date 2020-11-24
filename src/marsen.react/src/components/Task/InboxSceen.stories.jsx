bbefc5e37d43fdb85e3b10fb3f331693db197ac5
Marsen <thisismysoul@gmail.com>
2020-11-24 16:37:24 +0800

    js to jsx

---
+++ src/marsen.react/src/components/Task/InboxSceen.stories.jsx
@@ -0,0 +1,19 @@
+// src/components/InboxScreen.stories.js
+
+import React from 'react';
+
+import { PureInboxScreen } from './InboxScreen';
+
+export default {
+  component: PureInboxScreen,
+  title: 'InboxScreen',
+};
+
+const Template = args => <PureInboxScreen {...args} />;
+
+export const Default = Template.bind({});
+
+export const Error = Template.bind({});
+Error.args = {
+  error: 'Something',
+};
