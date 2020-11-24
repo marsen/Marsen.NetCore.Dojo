d317f133fbf71fd9d6662c75030a3e4ab44a6f98
Marsen <thisismysoul@gmail.com>
2020-11-24 17:06:25 +0800

    Supplying context with decorators

--- src/marsen.react/src/components/Task/InboxSceen.stories.jsx
+++ src/marsen.react/src/components/Task/InboxSceen.stories.jsx
@@ -1,11 +1,25 @@
-// src/components/InboxScreen.stories.js
+ //src/components/InboxScreen.stories.js
 
 import React from 'react';
-
+import { Provider } from 'react-redux';
+import { action } from '@storybook/addon-actions';
 import { PureInboxScreen } from './InboxScreen';
+import * as TaskListStories from './TaskList.stories';
+
+// A super-simple mock of a redux store
+const store = {
+  getState: () => {
+    return {
+      tasks: TaskListStories.Default.args.tasks,
+    };
+  },
+  subscribe: () => 0,
+  dispatch: action('dispatch'),
+};
 
 export default {
   component: PureInboxScreen,
+  decorators: [story => <Provider store={store}>{story()}</Provider>],
   title: 'InboxScreen',
 };
 
