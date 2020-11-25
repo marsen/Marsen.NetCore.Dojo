6e6daf90794d94e34084c393f6d85b571fc2dd2e
Marsen <thisismysoul@gmail.com>
2020-11-25 12:48:25 +0800

    yarn build-storybook

---
+++ src/marsen.react/storybook-static/precache-manifest.02f6968278b47cb3d0579a71c8afd98b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "112390965ccf4c9d73c4d9bf23dec6e5",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.e6835de88d36dc0d6979.bundle.js"
+  },
+  {
+    "url": "runtime~main.e6835de88d36dc0d6979.bundle.js"
+  },
+  {
+    "revision": "3589935c5c92a1d7efe9",
+    "url": "static/css/main.c10023bf.chunk.css"
+  },
+  {
+    "url": "vendors~main.e6835de88d36dc0d6979.bundle.js"
+  },
+  {
+    "url": "vendors~main.e6835de88d36dc0d6979.bundle.js.LICENSE.txt"
+  }
+]);
