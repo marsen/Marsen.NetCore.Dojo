37eec9558a0b54c3935c58200f6ea42bfcd148f7
Marsen <admin@marsen.me>
2020-12-21 17:19:08 +0800

    ignore inbox story

--- src/marsen.react/.storybook/main.js
+++ src/marsen.react/.storybook/main.js
@@ -1,6 +1,7 @@
 module.exports = {
   stories: [
-    '../src/components/Task/*.stories.tsx',
+    '../src/components/Task/Task.stories.tsx',
+    '../src/components/Task/TaskList.stories.tsx',
     '../src/components/TicTacToe/*.stories.tsx',
     '../src/components/*.stories.tsx'],
   addons: [    
