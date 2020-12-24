adc64fee10124eb6424cd99d094026fd3376372f
Marsen <admin@marsen.me>
2020-12-24 09:37:11 +0800

    TaskState Enum

--- src/marsen.react/src/components/Task/TaskList.stories.tsx
+++ src/marsen.react/src/components/Task/TaskList.stories.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import { PureTaskList, TaskListProps } from './TaskList';
-import { TaskItem } from './Task';
+import { TaskItem, TaskState } from './Task';
 import { Story } from '@storybook/react/types-6-0';
 
 export default {
@@ -17,19 +17,19 @@ const Template:Story<TaskListProps> = args => <PureTaskList {...args} />
 var defaultItem:TaskItem = { 
   id:'1',
   title:'Test Task',
-  state:'TASK_INBOX',
+  state:TaskState.Inbox,
   updatedAt: new Date(2018, 0, 1, 9, 0)
 };
 
 export const Default = Template.bind({});
 Default.args = { 
   tasks: [
-    { defaultItem, id: '1', title: 'Task 1' },
-    { defaultItem, id: '2', title: 'Task 2' },
-    { defaultItem, id: '3', title: 'Task 3' },
-    { defaultItem, id: '4', title: 'Task 4' },
-    { defaultItem, id: '5', title: 'Task 5' },
-    { defaultItem, id: '6', title: 'Task 6' },
+    { ...defaultItem, id: '1', title: 'Task 1' },
+    { ...defaultItem, id: '2', title: 'Task 2' },
+    { ...defaultItem, id: '3', title: 'Task 3' },
+    { ...defaultItem, id: '4', title: 'Task 4' },
+    { ...defaultItem, id: '5', title: 'Task 5' },
+    { ...defaultItem, id: '6', title: 'Task 6' },
   ],
 };
 
@@ -37,7 +37,7 @@ export const WithPinnedTasks = Template.bind({});
 WithPinnedTasks.args = {  
   tasks: [
     ...Default.args.tasks!.slice(0,5),
-    { id: '6', title: 'Task 6 (pinned)', state: 'TASK_PINNED' },
+    { id: '6', title: 'Task 6 (pinned)', state: TaskState.Pinned },
   ],
 };
 
