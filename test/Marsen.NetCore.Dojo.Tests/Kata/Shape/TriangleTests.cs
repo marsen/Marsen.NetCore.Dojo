511385c2d7ac25222d68d8c30798805523711281
Marsen <admin@marsen.me>
2021-01-25 11:31:25 +0800

    red, 333 is can be triangle

---
+++ test/Marsen.NetCore.Dojo.Tests/Kata/Shape/TriangleTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xunit;
+
+namespace Marsen.NetCore.Dojo.Tests.Kata.Shape
+{
+    public class TriangleTests
+    {
+        [Fact]
+        public void The_3_3_3_Can_be_Triangle()
+        {
+            Triangle triangle = new Triangle();
+            Assert.True(triangle.IsTriangle(3, 3, 3));
+        }
+    }
+
+    public class Triangle
+    {
+        public bool IsTriangle(int line1, int line2, int line3)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
