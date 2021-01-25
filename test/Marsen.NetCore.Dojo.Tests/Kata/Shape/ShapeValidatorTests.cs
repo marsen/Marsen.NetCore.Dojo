e38573bcbee3f82473d39b225d1fbdf5173b0e85
Marsen <admin@marsen.me>
2021-01-25 13:33:12 +0800

    test all pass

---
+++ test/Marsen.NetCore.Dojo.Tests/Kata/Shape/ShapeValidatorTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Marsen.NetCore.Dojo.Tests.Kata.Shape
+{
+    public class ShapeValidatorTests
+    {
+        private ShapeValidator _validator;
+
+        [Fact]
+        public void The_3_3_3_Can_be_Triangle()
+        {
+            _validator = new ShapeValidator();
+            Assert.True(_validator.IsTriangle(3, 3, 3));
+        }
+
+        [Fact]
+        public void The_6_3_3_Can_NOT_be_Triangle()
+        {
+            _validator = new ShapeValidator();
+            Assert.False(_validator.IsTriangle(6, 3, 3));
+        }
+    }
+
+    public class ShapeValidator
+    {
+        public bool IsTriangle(int line1, int line2, int line3)
+        {
+            return line2 + line3 > line1;
+        }
+    }
+}
