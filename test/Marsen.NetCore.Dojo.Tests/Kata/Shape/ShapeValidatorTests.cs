8392903550a52732d9a01da5689ac5e0c4939474
Marsen <admin@marsen.me>
2021-01-25 13:42:00 +0800

    refactor test

--- test/Marsen.NetCore.Dojo.Tests/Kata/Shape/ShapeValidatorTests.cs
+++ test/Marsen.NetCore.Dojo.Tests/Kata/Shape/ShapeValidatorTests.cs
@@ -7,20 +7,28 @@ namespace Marsen.NetCore.Dojo.Tests.Kata.Shape
 {
     public class ShapeValidatorTests
     {
-        private ShapeValidator _validator;
+        private readonly ShapeValidator _validator = new ShapeValidator();
 
         [Fact]
         public void The_3_3_3_Can_be_Triangle()
         {
-            _validator = new ShapeValidator();
-            Assert.True(_validator.IsTriangle(3, 3, 3));
+            CanBeTriangle(3, 3, 3);
         }
 
         [Fact]
         public void The_6_3_3_Can_NOT_be_Triangle()
         {
-            _validator = new ShapeValidator();
-            Assert.False(_validator.IsTriangle(6, 3, 3));
+            CanBeNotTriangle(6, 3, 3);
+        }
+
+        private void CanBeNotTriangle(int edge1, int edge2, int edge3)
+        {
+            Assert.False(_validator.IsTriangle(edge1, edge2, edge3));
+        }
+
+        private void CanBeTriangle(int edge1, int edge2, int edge3)
+        {
+            Assert.True(_validator.IsTriangle(edge1, edge2, edge3));
         }
     }
 
