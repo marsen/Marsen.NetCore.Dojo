4aaacc64e80a64e40965b07a3948a80e86440c91
Marsen <admin@marsen.me>
2021-01-28 16:19:25 +0800

    add qty is 1 case pass

--- test/Marsen.NetCore.Dojo.Tests/Kata/ShopMall/ProductTests.cs
+++ test/Marsen.NetCore.Dojo.Tests/Kata/ShopMall/ProductTests.cs
@@ -31,10 +31,15 @@ namespace Marsen.NetCore.Dojo.Tests.Kata.ShopMall
 
     public class Product
     {
+        private readonly int _price;
+        private readonly int _qty;
+
         public Product(string name, int price, int qty)
         {
+            _price = price;
+            _qty = qty;
         }
 
-        public int SubTotal { get; set; }
+        public int SubTotal => _price * _qty;
     }
 }
