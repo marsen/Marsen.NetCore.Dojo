3cc3ccf6e4222b70a55215cb851356eaef369d9b
Marsen <admin@marsen.me>
2021-01-28 16:37:26 +0800

    add test case pass

---
+++ test/Marsen.NetCore.Dojo.Tests/Kata/ShopMall/CartServiceTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xunit;
+
+namespace Marsen.NetCore.Dojo.Tests.Kata.ShopMall
+{
+    public class CartServiceTests
+    {
+        [Fact]
+        public void No_product_cart_TOTAL_PRICE_should_be_0()
+        {
+            CartService cartService = new CartService();
+            Assert.Equal(cartService.TotalPrice, 0);
+        }
+    }
+
+    public class CartService
+    {
+        public int TotalPrice { get; set; }
+    }
+}
