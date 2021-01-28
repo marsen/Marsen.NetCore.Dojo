16d7bfe580402f3f9fd88153401af00313a66a17
Marsen <admin@marsen.me>
2021-01-28 16:15:05 +0800

    add qty is 1 case

--- test/Marsen.NetCore.Dojo.Tests/Kata/ShopMall/ProductTests.cs
+++ test/Marsen.NetCore.Dojo.Tests/Kata/ShopMall/ProductTests.cs
@@ -15,6 +15,14 @@ namespace Marsen.NetCore.Dojo.Tests.Kata.ShopMall
             _product.SubTotal.Should().Be(0);
         }
 
+        [Fact]
+        public void When_Product_A_Price_is_10_Qty_is_1_SubTotal_Should_Be_10()
+        {
+            GivePriceAndQty(10, 1);
+            _product.SubTotal.Should().Be(10);
+        }
+
+
         private void GivePriceAndQty(int price, int qty)
         {
             _product = new Product("A", price, qty);
