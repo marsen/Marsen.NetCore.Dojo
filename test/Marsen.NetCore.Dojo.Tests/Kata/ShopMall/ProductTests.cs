279f1208eacfaa933a5d3955064b7022f16f5955
Marsen <admin@marsen.me>
2021-01-28 15:53:59 +0800

    When Product A Price is 10 Qty is 0 then Subtotal Should be 0

---
+++ test/Marsen.NetCore.Dojo.Tests/Kata/ShopMall/ProductTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Xunit;
+
+namespace Marsen.NetCore.Dojo.Tests.Kata.ShopMall
+{
+    public class ProductTests
+    {
+        [Fact]
+        public void When_Product_A_Price_is_10_Qty_is_0_SubTotal_Should_Be_0()
+        {
+            Product product = new Product("A", 10, 0);
+            Assert.Equal(product.SubTotal,0);
+        }
+    }
+
+    public class Product
+    {
+        public Product(string s, int i, int i1)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int SubTotal { get; set; }
+    }
+}
