272ba3e6bba230ae0da40ed6285b529f1114d75d
Marsen <admin@marsen.me>
2021-01-28 16:54:30 +0800

    add test case one product A in the Cart price should be 10 pass

--- test/Marsen.NetCore.Dojo.Tests/Kata/ShopMall/CartServiceTests.cs
+++ test/Marsen.NetCore.Dojo.Tests/Kata/ShopMall/CartServiceTests.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using FluentAssertions;
+using Marsen.NetCore.Dojo.Kata.ShopMall;
 using Xunit;
 
 namespace Marsen.NetCore.Dojo.Tests.Kata.ShopMall
@@ -13,10 +16,23 @@ namespace Marsen.NetCore.Dojo.Tests.Kata.ShopMall
         {
             _cartService.TotalPrice.Should().Be(0);
         }
+
+        [Fact]
+        public void There_is_one_Product_A_in_cart_TOTAL_PRICE_should_be_10()
+        {
+            _cartService.Add(new Product("A", 10, 1));
+            _cartService.TotalPrice.Should().Be(10);
+        }
     }
 
     public class CartService
     {
-        public int TotalPrice { get; set; }
+        private List<Product> _products = new();
+        public int TotalPrice => _products.Sum(x => x.SubTotal);
+
+        public void Add(Product product)
+        {
+            _products = new List<Product> {product};
+        }
     }
 }
