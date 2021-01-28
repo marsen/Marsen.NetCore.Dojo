d0e13a89fd396b4ed113cc71d884579f87b46772
Marsen <admin@marsen.me>
2021-01-28 16:13:52 +0800

    refactor test

--- test/Marsen.NetCore.Dojo.Tests/Kata/ShopMall/ProductTests.cs
+++ test/Marsen.NetCore.Dojo.Tests/Kata/ShopMall/ProductTests.cs
@@ -6,17 +6,24 @@ namespace Marsen.NetCore.Dojo.Tests.Kata.ShopMall
 {
     public class ProductTests
     {
+        private Product _product;
+
         [Fact]
         public void When_Product_A_Price_is_10_Qty_is_0_SubTotal_Should_Be_0()
         {
-            Product product = new Product("A", 10, 0);
-            product.SubTotal.Should().Be(0);
+            GivePriceAndQty(10, 0);
+            _product.SubTotal.Should().Be(0);
+        }
+
+        private void GivePriceAndQty(int price, int qty)
+        {
+            _product = new Product("A", price, qty);
         }
     }
 
     public class Product
     {
-        public Product(string s, int i, int i1)
+        public Product(string name, int price, int qty)
         {
         }
 
