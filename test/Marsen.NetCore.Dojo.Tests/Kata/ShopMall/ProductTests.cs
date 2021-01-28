53b26ffe34006f0b0cca7636a8f4c4417c8807b0
Marsen <admin@marsen.me>
2021-01-28 16:04:20 +0800

    refactory

--- test/Marsen.NetCore.Dojo.Tests/Kata/ShopMall/ProductTests.cs
+++ test/Marsen.NetCore.Dojo.Tests/Kata/ShopMall/ProductTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using FluentAssertions;
 using Xunit;
 
 namespace Marsen.NetCore.Dojo.Tests.Kata.ShopMall
@@ -9,7 +10,7 @@ namespace Marsen.NetCore.Dojo.Tests.Kata.ShopMall
         public void When_Product_A_Price_is_10_Qty_is_0_SubTotal_Should_Be_0()
         {
             Product product = new Product("A", 10, 0);
-            Assert.Equal(product.SubTotal,0);
+            product.SubTotal.Should().Be(0);
         }
     }
 
