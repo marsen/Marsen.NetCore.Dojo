c3a53952410ac03061bbc9a65ea08c3351526d05
Marsen <admin@marsen.me>
2021-01-29 09:36:45 +0800

    rename for read

--- test/Marsen.NetCore.Dojo.Tests/Kata/ShopMall/CartServiceTests.cs
+++ test/Marsen.NetCore.Dojo.Tests/Kata/ShopMall/CartServiceTests.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using FluentAssertions;
 using Marsen.NetCore.Dojo.Kata.ShopMall;
@@ -10,8 +9,6 @@ namespace Marsen.NetCore.Dojo.Tests.Kata.ShopMall
     public class CartServiceTests
     {
         private readonly CartService _cart = new();
-        private readonly Product _10PriceProduct = new("A", 10, 1);
-        private readonly Product _7PriceProduct = new("B", 7, 1);
 
         [Fact]
         public void No_product_cart_TOTAL_PRICE_should_be_0()
@@ -22,31 +19,40 @@ namespace Marsen.NetCore.Dojo.Tests.Kata.ShopMall
         [Fact]
         public void There_is_one_Product_A_in_cart_TOTAL_PRICE_should_be_10()
         {
-            _cart.Add(_10PriceProduct);
+            GivenProductA(1);
             _cart.TotalPrice.Should().Be(10);
         }
 
         [Fact]
         public void There_is_one_Product_B_in_cart_TOTAL_PRICE_should_be_7()
         {
-            _cart.Add(_7PriceProduct);
+            GivenProductB(1);
             _cart.TotalPrice.Should().Be(7);
         }
 
         [Fact]
         public void There_is_one_Product_A_and_Product_B_in_cart_TOTAL_PRICE_should_be_17()
         {
-            _cart.Add(_10PriceProduct);
-            _cart.Add(_7PriceProduct);
+            GivenProductA(1);
+            GivenProductB(1);
             _cart.TotalPrice.Should().Be(17);
         }
 
         [Fact]
         public void There_is_2_Product_B_in_cart_TOTAL_PRICE_should_be_14()
         {
-            _cart.Add(_7PriceProduct);
-            _cart.Add(_7PriceProduct);
+            GivenProductB(2);
             _cart.TotalPrice.Should().Be(14);
+        }
+        
+        private void GivenProductA(int qty)
+        {
+            _cart.Add(new("A", 10, qty));
+        }
+
+        private void GivenProductB(int qty)
+        {
+            _cart.Add(new("B", 7, qty));
         }
     }
 
