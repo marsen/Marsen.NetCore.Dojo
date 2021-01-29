cacdbbae31d2f3bfdd8e6ac79db438bacc7d895b
Marsen <admin@marsen.me>
2021-01-29 09:17:54 +0800

    rename

--- test/Marsen.NetCore.Dojo.Tests/Kata/ShopMall/CartServiceTests.cs
+++ test/Marsen.NetCore.Dojo.Tests/Kata/ShopMall/CartServiceTests.cs
@@ -9,36 +9,44 @@ namespace Marsen.NetCore.Dojo.Tests.Kata.ShopMall
 {
     public class CartServiceTests
     {
-        private readonly CartService _cartService = new();
+        private readonly CartService _cart = new();
         private readonly Product _10PriceProduct = new("A", 10, 1);
         private readonly Product _7PriceProduct = new("B", 7, 1);
 
         [Fact]
         public void No_product_cart_TOTAL_PRICE_should_be_0()
         {
-            _cartService.TotalPrice.Should().Be(0);
+            _cart.TotalPrice.Should().Be(0);
         }
 
         [Fact]
         public void There_is_one_Product_A_in_cart_TOTAL_PRICE_should_be_10()
         {
-            _cartService.Add(_10PriceProduct);
-            _cartService.TotalPrice.Should().Be(10);
+            _cart.Add(_10PriceProduct);
+            _cart.TotalPrice.Should().Be(10);
         }
 
         [Fact]
         public void There_is_one_Product_B_in_cart_TOTAL_PRICE_should_be_7()
         {
-            _cartService.Add(_7PriceProduct);
-            _cartService.TotalPrice.Should().Be(7);
+            _cart.Add(_7PriceProduct);
+            _cart.TotalPrice.Should().Be(7);
         }
 
         [Fact]
         public void There_is_one_Product_A_and_Product_B_in_cart_TOTAL_PRICE_should_be_17()
         {
-            _cartService.Add(_10PriceProduct);
-            _cartService.Add(_7PriceProduct);
-            _cartService.TotalPrice.Should().Be(17);
+            _cart.Add(_10PriceProduct);
+            _cart.Add(_7PriceProduct);
+            _cart.TotalPrice.Should().Be(17);
+        }
+
+        [Fact]
+        public void There_is_2_Product_B_in_cart_TOTAL_PRICE_should_be_14()
+        {
+            _cart.Add(_7PriceProduct);
+            _cart.Add(_7PriceProduct);
+            _cart.TotalPrice.Should().Be(14);
         }
     }
 
