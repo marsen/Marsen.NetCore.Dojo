517ce0b1d995f23b03a9203be95c63c8ed99e081
Marsen <admin@marsen.me>
2021-02-01 15:46:51 +0800

    call prop function

--- src/marsen.react/src/components/Cart/NumberSelector.tsx
+++ src/marsen.react/src/components/Cart/NumberSelector.tsx
@@ -5,9 +5,9 @@ import { Box, Input } from '@material-ui/core';
 export default function NumberSelector(prop:any) {    
       return (        
           <Box width={24}>
-            <ArrowDropUp onClick={()=>{ document.getElementById('mock_id').value++; }}/>            
-            <Input id="mock_id" value="1" inputProps={{min:0,style:{textAlign:'center'}}} />
-            <ArrowDropDown onClick={()=>{ document.getElementById('mock_id').value--;}} />
+            <ArrowDropUp onClick={()=>{ prop.add() }}/>            
+            <Input id="prop.selectorId" value="1" inputProps={{min:0,style:{textAlign:'center'}}} />
+            <ArrowDropDown onClick={()=>{ prop.minus() }} />
           </Box>
       );
 }
