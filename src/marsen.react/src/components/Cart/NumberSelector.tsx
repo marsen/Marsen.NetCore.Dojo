5422257d530d9195f37f8ec0cfccfcbb521f3fb4
Marsen <admin@marsen.me>
2021-02-02 10:12:21 +0800

    CSSProperties

--- src/marsen.react/src/components/Cart/NumberSelector.tsx
+++ src/marsen.react/src/components/Cart/NumberSelector.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { ArrowDropDown, ArrowDropUp } from '@material-ui/icons';
 import { Box, Input } from '@material-ui/core';
+import { CSSProperties } from '@material-ui/core/styles/withStyles';
+
+const style : CSSProperties =  { 
+  textAlign: 'center'
+};
 
 export default function NumberSelector(prop:any) {    
       return (        
           <Box width={24}>
             <ArrowDropUp onClick={()=>{ prop.add() }}/>            
-            <Input id="prop.selectorId" value="1" inputProps={{min:0,style:{textAlign:'center'}}} />
+            <Input id="prop.selectorId" value="1" inputProps={{min:0,style:style}} />
             <ArrowDropDown onClick={()=>{ prop.minus() }} />
           </Box>
       );
