f99c0b622b664b6dc36b218303e3d551ecc89f6b
Marsen <admin@marsen.me>
2021-02-08 14:20:48 +0800

    add milk

--- test/Marsen.NetCore.Dojo.Tests/Kata/ShopMall/CartServiceTests.cs
+++ test/Marsen.NetCore.Dojo.Tests/Kata/ShopMall/CartServiceTests.cs
@@ -67,9 +67,9 @@ namespace Marsen.NetCore.Dojo.Tests.Kata.ShopMall
         [Fact]
         public void Add_Milk_Cart_Should_Contain_Milk()
         {
-            CartProduct cartProduct = new(Milk, 10, 1);
-            _cart.ProductList.Add(cartProduct);
-            Assert.Contains(cartProduct, _cart.ProductList.Where(x => x.Name == Milk));
+            CartProduct milk = new(Milk, 10, 1);
+            _cart.ProductList.Add(milk);
+            Assert.Contains(milk, _cart.ProductList.Where(x => x.Name == Milk));
         }
 
 
