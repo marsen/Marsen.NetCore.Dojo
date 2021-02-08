b94afbb097e9ec5a3076c9eb2558db7f47d0cb66
Marsen <admin@marsen.me>
2021-02-08 13:47:07 +0800

    rename method

--- test/Marsen.NetCore.Dojo.Tests/Kata/ShopMall/CartServiceTests.cs
+++ test/Marsen.NetCore.Dojo.Tests/Kata/ShopMall/CartServiceTests.cs
@@ -20,37 +20,37 @@ namespace Marsen.NetCore.Dojo.Tests.Kata.ShopMall
         [Fact]
         public void There_is_one_Product_A_in_cart_TOTAL_PRICE_should_be_10()
         {
-            GivenProductA(1);
+            GivenAddMilk(1);
             _cart.TotalPrice.Should().Be(10);
         }
 
         [Fact]
         public void There_is_one_Product_B_in_cart_TOTAL_PRICE_should_be_7()
         {
-            GivenProductB(1);
+            GivenAddOil(1);
             _cart.TotalPrice.Should().Be(7);
         }
 
         [Fact]
         public void There_is_one_Product_A_and_Product_B_in_cart_TOTAL_PRICE_should_be_17()
         {
-            GivenProductA(1);
-            GivenProductB(1);
+            GivenAddMilk(1);
+            GivenAddOil(1);
             _cart.TotalPrice.Should().Be(17);
         }
 
         [Fact]
         public void There_is_2_Product_B_in_cart_TOTAL_PRICE_should_be_14()
         {
-            GivenProductB(2);
+            GivenAddOil(2);
             _cart.TotalPrice.Should().Be(14);
         }
 
         [Fact]
         public void There_is_One_Product_A_and_2_Product_B_in_cart_TOTAL_PRICE_should_be_44()
         {
-            GivenProductA(3);
-            GivenProductB(2);
+            GivenAddMilk(3);
+            GivenAddOil(2);
             _cart.TotalPrice.Should().Be(44);
         }
 
@@ -71,12 +71,12 @@ namespace Marsen.NetCore.Dojo.Tests.Kata.ShopMall
         }
 
 
-        private void GivenProductA(int qty)
+        private void GivenAddMilk(int qty)
         {
             _cart.ProductList.Add(new("A", 10, qty));
         }
 
-        private void GivenProductB(int qty)
+        private void GivenAddOil(int qty)
         {
             _cart.ProductList.Add(new("B", 7, qty));
         }
