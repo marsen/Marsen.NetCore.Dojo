cf7ec2ca8c6f8598169db5c190a880e3ccef4641
Marsen <admin@marsen.me>
2021-03-03 16:05:43 +0800

    add tests

--- test/Marsen.NetCore.Dojo.Tests/Kata/ShopMall/CartServiceTests.cs
+++ test/Marsen.NetCore.Dojo.Tests/Kata/ShopMall/CartServiceTests.cs
@@ -6,6 +6,24 @@ namespace Marsen.NetCore.Dojo.Tests.Kata.ShopMall
 {
     public class CartServiceTests
     {
+        readonly CartService _cartService = new();
+        /// <summary>
+        /// Test Data
+        /// </summary>
+        private Product milk = new Product {Name = "Milk", Price = 10};
 
+        [Fact]
+        public void TestCartTotal()
+        {
+            _cartService.Add(new CartProduct(milk, 1));
+            _cartService.TotalPrice.Should().Be(10);
+        }
+
+        [Fact]
+        public void TestCartSubtotal()
+        {
+            CartProduct cartProduct = new CartProduct(milk, 1);
+            Assert.Equal(cartProduct.SubTotal, 10);
+        }
     }
 }
