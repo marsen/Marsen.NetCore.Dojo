e5215aa0263bbf6485cd288e6cda74b24756abd7
Marsen <admin@marsen.me>
2021-03-03 17:13:02 +0800

    Revert "use test data"
    This reverts commit e3e4bc6fa311660ec5a22c5ec65216829c4b0b5c.

--- test/Marsen.NetCore.Dojo.Tests/Kata/ShopMall/CartServiceTests.cs
+++ test/Marsen.NetCore.Dojo.Tests/Kata/ShopMall/CartServiceTests.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using FluentAssertions;
+﻿using FluentAssertions;
 using Marsen.NetCore.Dojo.Kata.ShopMall;
 using Xunit;
 
@@ -9,40 +7,23 @@ namespace Marsen.NetCore.Dojo.Tests.Kata.ShopMall
     public class CartServiceTests
     {
         readonly CartService _cartService = new();
-
-        #region Test Data
         /// <summary>
         /// Test Data
         /// </summary>
-        private Product milk = new() {Name = "Milk", Price = 10};
-        
-        private readonly Product oil = new() {Name = "Oil", Price = 7};
-        
-        private readonly Product bun = new() {Name = "Bun", Price = 6};
-        #endregion
+        private Product milk = new Product {Name = "Milk", Price = 10};
 
-        [Theory]
-        [InlineData(1,0,0,10)]
-        [InlineData(1,1,0,17)]
-        [InlineData(1,0,1,16)]
-        [InlineData(1,1,1,23)]
-        [InlineData(2,3,4,65)]
-        public void TestCartTotal(int qtyOfMilk,int qtyOfOil,int qtyOfBun,int expected)
+        [Fact]
+        public void TestCartTotal()
         {
-            _cartService.Add(new CartProduct(milk, qtyOfMilk));
-            _cartService.Add(new CartProduct(oil, qtyOfOil));
-            _cartService.Add(new CartProduct(bun, qtyOfBun));
-            _cartService.TotalPrice.Should().Be(expected);
+            _cartService.Add(new CartProduct(milk, 1));
+            _cartService.TotalPrice.Should().Be(10);
         }
 
-        [Theory]
-        [InlineData(1,10)]
-        [InlineData(2,20)]
-        [InlineData(3,30)]
-        public void TestCartSubtotal(int qty, int expected)
+        [Fact]
+        public void TestCartSubtotal()
         {
-            CartProduct cartProduct = new CartProduct(milk, qty);
-            Assert.Equal(cartProduct.SubTotal, expected);
+            CartProduct cartProduct = new CartProduct(milk, 1);
+            Assert.Equal(cartProduct.SubTotal, 10);
         }
     }
 }
