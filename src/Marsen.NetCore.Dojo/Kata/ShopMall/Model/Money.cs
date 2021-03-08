4d26bc7dbb25444d2794031b2e5cda31a6bcb17c
Marsen <admin@marsen.me>
2021-03-08 16:09:56 +0800

    move to new files

---
+++ src/Marsen.NetCore.Dojo/Kata/ShopMall/Model/Money.cs
@@ -0,0 +1,9 @@
+﻿namespace Marsen.NetCore.Dojo.Kata.ShopMall.Model
+{
+    public struct Money
+    {
+        public int number { get; init; }
+        public string symbol { get; init; }
+        public override string ToString() => $"{number} {symbol}";
+    }
+}
--- src/Marsen.NetCore.Dojo/Kata/ShopMall/Model/Product.cs
+++ src/Marsen.NetCore.Dojo/Kata/ShopMall/Model/Product.cs
@@ -5,11 +5,4 @@
         public Money Price;
         public string Name;
     }
-
-    public struct Money
-    {
-        public int number { get; init; }
-        public string symbol { get; init; }
-        public override string ToString() => $"{number} {symbol}";
-    }
 }
