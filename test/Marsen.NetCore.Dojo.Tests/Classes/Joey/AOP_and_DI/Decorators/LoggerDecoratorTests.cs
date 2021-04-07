4ef3423783f8f4af035d57373686224b079ba9f5
Marsen <admin@marsen.me>
2021-04-07 16:50:16 +0800

    rename and refactor tests

---
+++ test/Marsen.NetCore.Dojo.Tests/Classes/Joey/AOP_and_DI/Decorators/LoggerDecoratorTests.cs
@@ -0,0 +1,28 @@
+﻿using Marsen.NetCore.Dojo.Classes.Joey.AOP_and_DI.Decorators;
+using Marsen.NetCore.Dojo.Classes.Joey.AOP_and_DI.Interface;
+using NSubstitute;
+using Xunit;
+
+namespace Marsen.NetCore.Dojo.Tests.Classes.Joey.AOP_and_DI.Decorators
+{
+    public class LoggerDecoratorTests
+    {
+        readonly IAuthentication _authentication = Substitute.For<IAuthentication>();
+        readonly ILogger _logger = Substitute.For<ILogger>();
+        readonly IAccountService _account = Substitute.For<IAccountService>();
+
+        [Fact]
+        public void TestVerifyTrueDidNotLog()
+        {
+            GivenVerifyResultIs(true);
+            var target = new LoggerDecorator(_authentication, _logger, _account);
+            target.Verify("account", "password", "OTP");
+            _logger.DidNotReceiveWithAnyArgs().Log(Arg.Any<string>());
+        }
+
+        private void GivenVerifyResultIs(bool result)
+        {
+            _authentication.Verify("account", "password", "OTP").Returns(result);
+        }
+    }
+}
