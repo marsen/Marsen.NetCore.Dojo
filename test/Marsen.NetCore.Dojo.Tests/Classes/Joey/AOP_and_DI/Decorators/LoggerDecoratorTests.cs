733ed4986bfddc77a03c00e11e8b9989b4a541e5
Marsen <admin@marsen.me>
2021-04-08 10:13:40 +0800

    refactor test

--- test/Marsen.NetCore.Dojo.Tests/Classes/Joey/AOP_and_DI/Decorators/LoggerDecoratorTests.cs
+++ test/Marsen.NetCore.Dojo.Tests/Classes/Joey/AOP_and_DI/Decorators/LoggerDecoratorTests.cs
@@ -11,13 +11,17 @@ namespace Marsen.NetCore.Dojo.Tests.Classes.Joey.AOP_and_DI.Decorators
         readonly IAuthentication _authentication = Substitute.For<IAuthentication>();
         readonly ILogger _logger = Substitute.For<ILogger>();
         readonly IAccountService _accountService = Substitute.For<IAccountService>();
+        private string _account = "account";
+        private string _password = "password";
+        private string _otp = "OTP";
+        private LoggerDecorator _decorator;
 
         [Fact]
         public void TestVerifyTrueDidNotLog()
         {
             GivenVerifyResultIs(true);
-            var target = new LoggerDecorator(_authentication, _logger, _accountService);
-            target.Verify("account", "password", "OTP");
+            GivenDecorator();
+            _decorator.Verify(_account, _password, _otp);
             _logger.DidNotReceiveWithAnyArgs().Log(Arg.Any<string>());
         }
 
@@ -25,15 +29,19 @@ namespace Marsen.NetCore.Dojo.Tests.Classes.Joey.AOP_and_DI.Decorators
         public void TestVerifyFalseShouldLog()
         {
             GivenVerifyResultIs(false);
-            var target = new LoggerDecorator(_authentication, _logger, _accountService);
-            target.Verify("account", "password", "OTP").Should().Be(false);
-            _logger.ReceivedWithAnyArgs(1).Log(Arg.Any<string>());
+            GivenDecorator();
+            _decorator.Verify(_account, _password, _otp).Should().Be(false);
+            _logger.Received().Log(Arg.Is<string>(s => s.StartsWith($@"accountId:{_account}")));
         }
 
+        private void GivenDecorator()
+        {
+            _decorator = new LoggerDecorator(_authentication, _logger, _accountService);
+        }
 
         private void GivenVerifyResultIs(bool result)
         {
-            _authentication.Verify("account", "password", "OTP").Returns(result);
+            _authentication.Verify(_account, _password, _otp).Returns(result);
         }
     }
 }
