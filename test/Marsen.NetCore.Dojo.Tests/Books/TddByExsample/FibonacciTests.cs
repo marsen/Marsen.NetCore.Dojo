285075b0d66b30a8ffabd9245857f01a6dc0470f
Marsen <thisismysoul@gmail.com>
2021-04-08 19:25:56 +0800

    add case Fib(2) =1

--- test/Marsen.NetCore.Dojo.Tests/Books/TddByExsample/FibonacciTests.cs
+++ test/Marsen.NetCore.Dojo.Tests/Books/TddByExsample/FibonacciTests.cs
@@ -6,21 +6,20 @@ namespace Marsen.NetCore.Dojo.Tests.Books.TDDbyExsample
 {
     public class FibonacciTests
     {
+        private readonly Dictionary<int, int> _cases = new()
+        {
+            {0, 0},
+            {1, 1},
+            {2, 1}
+        };
+
         [Fact]
         public void TestFibonacci()
         {
-            var cases = new Dictionary<int, int>
+            for (int i = 0; i < _cases.Count; i++)
             {
-                {0, 0},
-                {1, 1}
-            };
-            for (int i = 0; i < cases.Count; i++)
-            {
-                Assert.Equal(cases[i], Fibonacci(i));
+                Assert.Equal(_cases[i], Fibonacci(i));
             }
-
-            //Assert.Equal(cases[0], Fibonacci(0));
-            //Assert.Equal(cases[1], Fibonacci(1));
         }
 
         private int Fibonacci(int input)
