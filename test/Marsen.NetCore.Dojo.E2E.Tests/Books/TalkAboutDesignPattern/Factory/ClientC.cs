87d2d7c6eb1da6688408e2731c918c5fb606acdb
Marsen <admin@marsen.me>
2021-04-12 17:21:33 +0800

    Use VoiceCallFactory (Remove parameter)

--- test/Marsen.NetCore.Dojo.E2E.Tests/Books/TalkAboutDesignPattern/Factory/ClientC.cs
+++ test/Marsen.NetCore.Dojo.E2E.Tests/Books/TalkAboutDesignPattern/Factory/ClientC.cs
@@ -6,8 +6,17 @@ namespace Marsen.NetCore.Dojo.E2E.Tests.Books.TalkAboutDesignPattern.Factory
     {
         public static void Run()
         {
-            var notify = new SimpleFactory().Create("VoiceCall");
+            var notify = new VoiceCallFactory().Create();
+            //// var notify = new SimpleFactory().Create("VoiceCall");
             notify.Send("warning client C");
+        }
+    }
+
+    public class VoiceCallFactory
+    {
+        public INotification Create()
+        {
+            return new VoiceCallNotify {Conditional = "Voice Conditional"};
         }
     }
 }
