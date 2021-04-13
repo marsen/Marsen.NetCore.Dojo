592a670cf86a021f6d276892ddb4f5adc1c7bfef
Marsen <admin@marsen.me>
2021-04-13 15:09:57 +0800

    refactor tests

--- test/Marsen.NetCore.Dojo.Tests/Books/TalkAboutDesignPattern/Factory/NotificationTests.cs
+++ test/Marsen.NetCore.Dojo.Tests/Books/TalkAboutDesignPattern/Factory/NotificationTests.cs
@@ -7,23 +7,24 @@ namespace Marsen.NetCore.Dojo.Tests.Books.TalkAboutDesignPattern.Factory
 {
     public class NotificationTests
     {
-        private INotification _notify;
         private INotificationFactory _factory;
 
         [Fact]
         public void TestCreateEmailNotify()
         {
             _factory = new EmailFactory();
-            _notify = _factory.Create();
-            _notify.Should().BeOfType<EmailNotify>("Because we send notify via Email");
+            _factory.Create()
+                .Should()
+                .BeOfType<EmailNotify>("Because we send notify via Email");
         }
 
         [Fact]
         public void TestCreateSNSNotify()
         {
             _factory = new SnsFactory();
-            _notify = _factory.Create();
-            _notify.Should().BeOfType<SnsNotify>("Because we send notify via SNS");
+            _factory.Create()
+                .Should()
+                .BeOfType<SnsNotify>("Because we send notify via SNS");
         }
 
         [Fact]
