4eb188a345f0cee65db379073d5125ba80108516
Marsen <admin@marsen.me>
2021-04-13 11:03:56 +0800

    Test Case Create SNS Notify

--- test/Marsen.NetCore.Dojo.Tests/Books/TalkAboutDesignPattern/Factory/Notification.cs
+++ test/Marsen.NetCore.Dojo.Tests/Books/TalkAboutDesignPattern/Factory/Notification.cs
@@ -11,6 +11,17 @@ namespace Marsen.NetCore.Dojo.Tests.Books.TalkAboutDesignPattern.Factory
             var notify = new EmailNotify();
             notify.Should().BeOfType<EmailNotify>("Because we send notify via Email");
         }
+
+        [Fact]
+        public void TestCreateSNSNotify()
+        {
+            var notify = new SNSNotify();
+            notify.Should().BeOfType<SNSNotify>("Because we send notify via SNS");
+        }
+    }
+
+    public class SNSNotify
+    {
     }
 
     public class EmailNotify
