805a93b74c92c516bae586841e84cc44f791834d
Marsen <admin@marsen.me>
2021-04-13 11:02:27 +0800

    Test Create Email Notify

---
+++ test/Marsen.NetCore.Dojo.Tests/Books/TalkAboutDesignPattern/Factory/Notification.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Marsen.NetCore.Dojo.Tests.Books.TalkAboutDesignPattern.Factory
+{
+    public class NotificationTests
+    {
+        [Fact]
+        public void TestCreateEmailNotify()
+        {
+            var notify = new EmailNotify();
+            notify.Should().BeOfType<EmailNotify>("Because we send notify via Email");
+        }
+    }
+
+    public class EmailNotify
+    {
+    }
+}
