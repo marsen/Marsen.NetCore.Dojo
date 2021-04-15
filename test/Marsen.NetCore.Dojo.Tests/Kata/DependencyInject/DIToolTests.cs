3809c94e1355cadb0e81654ba4b72ff36c5c0d10
Marsen <admin@marsen.me>
2021-04-15 16:45:26 +0800

    add test

---
+++ test/Marsen.NetCore.Dojo.Tests/Kata/DependencyInject/DIToolTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace Marsen.NetCore.Dojo.Tests.Kata.DependencyInject
+{
+    public class DIToolTests
+    {
+        [Fact]
+        public void CreateObjectViaNew()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
