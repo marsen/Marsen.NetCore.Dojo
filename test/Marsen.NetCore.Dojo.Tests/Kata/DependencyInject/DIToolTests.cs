5522d50460d9cb0ee931be640d8f115fed0b6c69
Marsen <admin@marsen.me>
2021-04-15 16:59:50 +0800

    add Register and Resolve Test Case

--- test/Marsen.NetCore.Dojo.Tests/Kata/DependencyInject/DIToolTests.cs
+++ test/Marsen.NetCore.Dojo.Tests/Kata/DependencyInject/DIToolTests.cs
@@ -12,6 +12,28 @@ namespace Marsen.NetCore.Dojo.Tests.Kata.DependencyInject
             var service = new MockService();
             service.Should().BeOfType<MockService>();
         }
+
+        [Fact]
+        public void CreateObjectViaRegisterAndResolve()
+        {
+            var target = new DIService();
+            target.Register<MockService>();
+            var service =  target.Resolve<MockService>();
+            service.Should().BeOfType<MockService>();
+        }
+    }
+
+    public class DIService
+    {
+        public void Register<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Resolve<T>()
+        {
+            throw new NotImplementedException();
+        }
     }
 
     public class MockService
