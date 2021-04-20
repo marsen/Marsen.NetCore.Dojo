f4a520afd92c5ca2334e78acf7606d440f6fbc58
Marsen <admin@marsen.me>
2021-04-20 16:55:16 +0800

    refactor move files

---
+++ test/Marsen.NetCore.Dojo.Tests/Kata/DependencyInject/DIService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Marsen.NetCore.Dojo.Tests.Kata.DependencyInject
+{
+    public class DIService
+    {
+        readonly Dictionary<Type, object> instanceLookup = new();
+        readonly Dictionary<Type, Func<object>> instanceFuncLookup = new();
+
+        public void Register<T>()
+        {
+            //// instanceLookup.Add(typeof(T), Activator.CreateInstance(typeof(T)));
+            if (instanceFuncLookup.ContainsKey(typeof(T)) == false)
+            {
+                instanceFuncLookup.Add(typeof(T), () => Activator.CreateInstance(typeof(T)));
+            }
+        }
+
+        public void Register<S, T>()
+        {
+            //// instanceLookup.Add(typeof(S), Activator.CreateInstance(typeof(T)));
+            instanceFuncLookup.Add(typeof(S), () => Activator.CreateInstance(typeof(T)));
+        }
+
+        public T Resolve<T>()
+        {
+            var func = instanceFuncLookup.SingleOrDefault(x => x.Key == typeof(T)).Value;
+            return (T) func.Invoke();
+            ////  return (T) instanceLookup.SingleOrDefault(x => x.Key == typeof(T)).Value;
+        }
+    }
+}
--- test/Marsen.NetCore.Dojo.Tests/Kata/DependencyInject/DIToolTests.cs
+++ test/Marsen.NetCore.Dojo.Tests/Kata/DependencyInject/DIToolTests.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using FluentAssertions;
+﻿using FluentAssertions;
 using Xunit;
 
 namespace Marsen.NetCore.Dojo.Tests.Kata.DependencyInject
@@ -48,35 +45,6 @@ namespace Marsen.NetCore.Dojo.Tests.Kata.DependencyInject
 
     public interface IMockService
     {
-    }
-
-
-    public class DIService
-    {
-        readonly Dictionary<Type, object> instanceLookup = new();
-        readonly Dictionary<Type, Func<object>> instanceFuncLookup = new();
-
-        public void Register<T>()
-        {
-            //// instanceLookup.Add(typeof(T), Activator.CreateInstance(typeof(T)));
-            if (instanceFuncLookup.ContainsKey(typeof(T)) == false)
-            {
-                instanceFuncLookup.Add(typeof(T), () => Activator.CreateInstance(typeof(T)));
-            }
-        }
-
-        public void Register<S, T>()
-        {
-            //// instanceLookup.Add(typeof(S), Activator.CreateInstance(typeof(T)));
-            instanceFuncLookup.Add(typeof(S), () => Activator.CreateInstance(typeof(T)));
-        }
-
-        public T Resolve<T>()
-        {
-            var func = instanceFuncLookup.SingleOrDefault(x => x.Key == typeof(T)).Value;
-            return (T) func.Invoke();
-            ////  return (T) instanceLookup.SingleOrDefault(x => x.Key == typeof(T)).Value;
-        }
     }
 
     public class FakeService
