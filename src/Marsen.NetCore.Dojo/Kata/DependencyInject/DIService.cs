e09ca09f6ebacb0e07d591831e446194d96a4c3f
Marsen <admin@marsen.me>
2021-04-22 11:14:04 +0800

    refactor extract method

--- src/Marsen.NetCore.Dojo/Kata/DependencyInject/DIService.cs
+++ src/Marsen.NetCore.Dojo/Kata/DependencyInject/DIService.cs
@@ -20,9 +20,18 @@ namespace Marsen.NetCore.Dojo.Kata.DependencyInject
                 throw new Exception("Register abstract classes or interfaces, should use Register<Interface,Class>");
             }
 
+            if (type != null)
+            {
+                if (_instanceFuncLookup.ContainsKey(type))
+                    throw new Exception("We not support Register duplicate Type now");
+
+                _instanceFuncLookup.Add(type, () => Activator.CreateInstance(instanceType));
+                return;
+            }
 
             if (_instanceFuncLookup.ContainsKey(instanceType))
                 throw new Exception("We not support Register duplicate Type now");
+
             if (lifetime == ServiceLifetime.Singleton)
             {
                 var singletonInstance = Activator.CreateInstance(instanceType);
@@ -37,8 +46,16 @@ namespace Marsen.NetCore.Dojo.Kata.DependencyInject
         public void Register<TInterface, TService>(ServiceLifetime lifetime = ServiceLifetime.Transient)
             where TService : TInterface
         {
-            //Register(typeof(TService), lifetime, typeof(TInterface));
-            if (_instanceFuncLookup.ContainsKey(typeof(TInterface)))
+            Register(typeof(TService), lifetime, typeof(TInterface));
+            return;
+            var instanceType = typeof(TService);
+            var interfaceType = typeof(TInterface);
+            if (instanceType.IsInterface || instanceType.IsAbstract)
+            {
+                throw new Exception("Register abstract classes or interfaces, should use Register<Interface,Class>");
+            }
+
+            if (_instanceFuncLookup.ContainsKey(interfaceType))
                 throw new Exception("We not support Register duplicate Type now");
             _instanceFuncLookup.Add(typeof(TInterface), () => Activator.CreateInstance(typeof(TService)));
         }
