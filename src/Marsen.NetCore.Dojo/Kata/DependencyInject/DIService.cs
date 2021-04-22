e8e73556aa9115568b0edb762c761f7774b43d90
Marsen <admin@marsen.me>
2021-04-22 11:22:13 +0800

    refactor with type

--- src/Marsen.NetCore.Dojo/Kata/DependencyInject/DIService.cs
+++ src/Marsen.NetCore.Dojo/Kata/DependencyInject/DIService.cs
@@ -13,24 +13,28 @@ namespace Marsen.NetCore.Dojo.Kata.DependencyInject
             Register(typeof(TService), lifetime);
         }
 
-        private void Register(Type instanceType, ServiceLifetime lifetime, Type type = null)
+        private void Register(Type instanceType, ServiceLifetime lifetime, Type interfaceType = null)
         {
             if (instanceType.IsInterface || instanceType.IsAbstract)
             {
                 throw new Exception("Register abstract classes or interfaces, should use Register<Interface,Class>");
             }
 
-            if (type != null)
-            {
-                if (_instanceFuncLookup.ContainsKey(type))
-                    throw new Exception("We not support Register duplicate Type now");
+            var type = interfaceType ?? instanceType;
 
-                _instanceFuncLookup.Add(type, () => Activator.CreateInstance(instanceType));
+            if (_instanceFuncLookup.ContainsKey(type))
+                throw new Exception("We not support Register duplicate Type now");
+            if (interfaceType != null)
+            {
+                // if (_instanceFuncLookup.ContainsKey(interfaceType))
+                    // throw new Exception("We not support Register duplicate Type now");
+
+                _instanceFuncLookup.Add(interfaceType, () => Activator.CreateInstance(instanceType));
                 return;
             }
 
-            if (_instanceFuncLookup.ContainsKey(instanceType))
-                throw new Exception("We not support Register duplicate Type now");
+            // if (_instanceFuncLookup.ContainsKey(instanceType))
+            // throw new Exception("We not support Register duplicate Type now");
 
             if (lifetime == ServiceLifetime.Singleton)
             {
@@ -47,17 +51,6 @@ namespace Marsen.NetCore.Dojo.Kata.DependencyInject
             where TService : TInterface
         {
             Register(typeof(TService), lifetime, typeof(TInterface));
-            return;
-            var instanceType = typeof(TService);
-            var interfaceType = typeof(TInterface);
-            if (instanceType.IsInterface || instanceType.IsAbstract)
-            {
-                throw new Exception("Register abstract classes or interfaces, should use Register<Interface,Class>");
-            }
-
-            if (_instanceFuncLookup.ContainsKey(interfaceType))
-                throw new Exception("We not support Register duplicate Type now");
-            _instanceFuncLookup.Add(typeof(TInterface), () => Activator.CreateInstance(typeof(TService)));
         }
 
         public TService Resolve<TService>()
