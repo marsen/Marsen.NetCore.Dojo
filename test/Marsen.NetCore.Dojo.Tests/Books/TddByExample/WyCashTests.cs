013cbaaa26968f80c0bc76ecf7752071bb86524b
Marsen <thisismysoul@gmail.com>
2021-05-17 23:26:38 +0800

    first test multiple red

---
+++ test/Marsen.NetCore.Dojo.Tests/Books/TddByExample/WyCashTests.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace Marsen.NetCore.Dojo.Tests.Books.TddByExample
+{
+    public class WyCashTests
+    {
+        [Fact]
+        public void testMultiplication()
+        {
+            throw new NotImplementedException();
+        }
+      
+    }
+}
