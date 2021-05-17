2bad035529b0087210ce88588da6f9b95b698df8
Marsen <thisismysoul@gmail.com>
2021-05-18 02:18:09 +0800

    side effect

--- test/Marsen.NetCore.Dojo.Tests/Books/TddByExample/WyCashTests.cs
+++ test/Marsen.NetCore.Dojo.Tests/Books/TddByExample/WyCashTests.cs
@@ -11,6 +11,8 @@ namespace Marsen.NetCore.Dojo.Tests.Books.TddByExample
             Dollar five = new Dollar(5);
             five.times(2);
             Assert.Equal(10,five.amount);
+            five.times(3);
+            Assert.Equal(15,five.amount);
         }
       
     }
