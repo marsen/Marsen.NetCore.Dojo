d4e6ef58aed65f57cf5fedc9cc4be0e8284ced7f
Marsen <thisismysoul@gmail.com>
2021-05-17 23:45:03 +0800

    create Dollar class but still red

--- test/Marsen.NetCore.Dojo.Tests/Books/TddByExample/WyCashTests.cs
+++ test/Marsen.NetCore.Dojo.Tests/Books/TddByExample/WyCashTests.cs
@@ -8,8 +8,25 @@ namespace Marsen.NetCore.Dojo.Tests.Books.TddByExample
         [Fact]
         public void testMultiplication()
         {
-            throw new NotImplementedException();
+            Dollar five = new Dollar(5);
+            five.times(2);
+            Assert.Equal(10,five.amount);
         }
       
+    }
+
+    public class Dollar
+    {
+        public int amount;
+
+        public Dollar(int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void times(int i)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
