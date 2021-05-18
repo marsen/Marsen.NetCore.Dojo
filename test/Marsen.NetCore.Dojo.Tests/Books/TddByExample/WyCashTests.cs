ac0eb62d7dab2ca40864f9bc2f26c259947dc2cc
Marsen <thisismysoul@gmail.com>
2021-05-18 15:22:12 +0800

    add Franc Equal case

--- test/Marsen.NetCore.Dojo.Tests/Books/TddByExample/WyCashTests.cs
+++ test/Marsen.NetCore.Dojo.Tests/Books/TddByExample/WyCashTests.cs
@@ -26,6 +26,8 @@ namespace Marsen.NetCore.Dojo.Tests.Books.TddByExample
         {
             Assert.True(new Dollar(5).Equals(new Dollar(5)));
             Assert.False(new Dollar(5).Equals(new Dollar(6)));
+            Assert.True(new Franc(5).Equals(new Franc(5)));
+            Assert.False(new Franc(5).Equals(new Franc(6)));
         }
     }
 
