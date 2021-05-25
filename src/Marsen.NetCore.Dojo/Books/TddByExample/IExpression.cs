d3dfa8143888c2fffa133235a3743a11fefd0893
Marsen <thisismysoul@gmail.com>
2021-05-26 02:12:47 +0800

    Pull up Interface

--- src/Marsen.NetCore.Dojo/Books/TddByExample/IExpression.cs
+++ src/Marsen.NetCore.Dojo/Books/TddByExample/IExpression.cs
@@ -3,5 +3,7 @@ namespace Marsen.NetCore.Dojo.Books.TddByExample
     public interface IExpression
     {
         Money reduce(string to, Bank bank);
+        IExpression times(int multiplier);
+        IExpression plus(Money money);
     }
 }
