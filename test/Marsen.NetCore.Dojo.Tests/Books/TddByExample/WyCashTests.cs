78c20d9ae939c58ea3a0558507fc0b987d0e0e57
Marsen <thisismysoul@gmail.com>
2021-05-26 10:38:48 +0800

    replace five USD

--- test/Marsen.NetCore.Dojo.Tests/Books/TddByExample/WyCashTests.cs
+++ test/Marsen.NetCore.Dojo.Tests/Books/TddByExample/WyCashTests.cs
@@ -8,6 +8,7 @@ namespace Marsen.NetCore.Dojo.Tests.Books.TddByExample
         private readonly Bank _bank = new();
         private readonly Money _fiveBulks = Money.dollar(5);
         private readonly Money _fiveFrancs = Money.franc(5);
+        private Money _fourBulks = Money.dollar(4);
 
         [Fact]
         public void testMultiplication()
@@ -43,29 +44,24 @@ namespace Marsen.NetCore.Dojo.Tests.Books.TddByExample
         [Fact]
         public void testSimpleAddition()
         {
-            var four = Money.dollar(4);
-            var fivePlusFive = _fiveBulks.plus(_fiveBulks);
-            var fivePlusFour = _fiveBulks.plus(four);
-            Assert.Equal(Money.dollar(10), _bank.reduce(fivePlusFive, "USD"));
-            Assert.Equal(Money.dollar(9), _bank.reduce(fivePlusFour, "USD"));
+            Assert.Equal(Money.dollar(10), _bank.reduce(_fiveBulks.plus(_fiveBulks), "USD"));
+            Assert.Equal(Money.dollar(9), _bank.reduce(_fiveBulks.plus(_fourBulks), "USD"));
         }
 
         [Fact]
         public void testMixedAddition()
         {
-            var _5Dollars = Money.dollar(5);
             var _10Francs = Money.franc(10);
             _bank.addRate("CHF", "USD", 2);
-            Assert.Equal(Money.dollar(10), _bank.reduce(_5Dollars.plus(_10Francs), "USD"));
-            Assert.Equal(Money.dollar(10), _bank.reduce(_10Francs.plus(_5Dollars), "USD"));
+            Assert.Equal(Money.dollar(10), _bank.reduce(_fiveBulks.plus(_10Francs), "USD"));
+            Assert.Equal(Money.dollar(10), _bank.reduce(_10Francs.plus(_fiveBulks), "USD"));
         }
 
 
         [Fact]
         public void testPlusReturnsSum()
         {
-            var result = _fiveBulks.plus(_fiveBulks);
-            var sum = (Sum) result;
+            var sum = (Sum) _fiveBulks.plus(_fiveBulks);
             Assert.Equal(_fiveBulks, sum.Addend);
             Assert.Equal(_fiveBulks, sum.Augend);
         }
@@ -73,31 +69,28 @@ namespace Marsen.NetCore.Dojo.Tests.Books.TddByExample
         [Fact]
         public void testReduceMoneyExchangeCurrency()
         {
-            var bank = new Bank();
-            bank.addRate("CHF", "USD", 2);
-            Assert.Equal(Money.dollar(1), bank.reduce(Money.franc(2), "USD"));
-            Assert.Equal(Money.dollar(3), bank.reduce(Money.franc(6), "USD"));
-            Assert.Equal(Money.dollar(3), bank.reduce(Money.dollar(3), "USD"));
+            _bank.addRate("CHF", "USD", 2);
+            Assert.Equal(Money.dollar(1), _bank.reduce(Money.franc(2), "USD"));
+            Assert.Equal(Money.dollar(3), _bank.reduce(Money.franc(6), "USD"));
+            Assert.Equal(Money.dollar(3), _bank.reduce(Money.dollar(3), "USD"));
         }
 
         [Fact]
         public void testSumPlusMoney()
         {
-            var _5Dollars = Money.dollar(5);
             var _10Francs = Money.franc(10);
             _bank.addRate("CHF", "USD", 2);
-            var sum = new Sum(_5Dollars, _10Francs).plus(_5Dollars);
+            var sum = new Sum(_fiveBulks, _10Francs).plus(_fiveBulks);
             Assert.Equal(Money.dollar(15), sum.reduce("USD", _bank));
         }
 
         [Fact]
         public void testSumTimes()
         {
-             var _5Dollars = Money.dollar(5);
-             var _10Francs = Money.franc(10);
-             _bank.addRate("CHF", "USD", 2);
-             var sum = new Sum(_5Dollars, _10Francs).times(2);
-             Assert.Equal(Money.dollar(20), sum.reduce("USD", _bank));
+            var _10Francs = Money.franc(10);
+            _bank.addRate("CHF", "USD", 2);
+            var sum = new Sum(_fiveBulks, _10Francs).times(2);
+            Assert.Equal(Money.dollar(20), sum.reduce("USD", _bank));
         }
     }
 }
