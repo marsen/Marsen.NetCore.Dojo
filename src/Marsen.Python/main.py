92ff7403f5806e97b7d418ad3380d1768cd85103
Marsen <thisismysoul@gmail.com>
2021-05-29 08:35:42 +0800

    assign name on the TestCase class

--- src/Marsen.Python/main.py
+++ src/Marsen.Python/main.py
@@ -5,13 +5,14 @@
 
 # Press the green button in the gutter to run the script.
 class TestCase:
-    pass
+    def __init__(self, name):
+        self.name = name
 
 
 class WasRun(TestCase):
     def __init__(self, name):
         self.wasRun = None
-        self.name = name
+        TestCase.__init__(self, name)
 
     def testmethod(self):
         self.wasRun = 1
