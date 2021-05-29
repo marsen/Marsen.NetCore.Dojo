16e0c9a1fa487db05e29a65a55deb3dc520e5bac
Marsen <thisismysoul@gmail.com>
2021-05-29 08:40:42 +0800

    pull up member

--- src/Marsen.Python/main.py
+++ src/Marsen.Python/main.py
@@ -8,6 +8,10 @@ class TestCase:
     def __init__(self, name):
         self.name = name
 
+    def run(self):
+        method = getattr(self, self.name)
+        method()
+
 
 class WasRun(TestCase):
     def __init__(self, name):
@@ -16,10 +20,6 @@ class WasRun(TestCase):
 
     def testmethod(self):
         self.wasRun = 1
-
-    def run(self):
-        method = getattr(self, self.name)
-        method()
 
 
 if __name__ == '__main__':
