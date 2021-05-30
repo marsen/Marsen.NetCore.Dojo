1389f8e22d37e688e995d392d27bc19d73305258
Marsen <thisismysoul@gmail.com>
2021-05-31 01:26:16 +0800

    create TestSuite class and implement add and run

--- src/Marsen.Python/main.py
+++ src/Marsen.Python/main.py
@@ -58,7 +58,14 @@ class TestResult:
 
 
 class TestSuite:
-    pass
+    def __init__(self):
+        self.tests = []
+
+    def add(self, test):
+        self.tests.append(test)
+
+    def run(self):
+        pass
 
 
 class TestCaseTest(TestCase):
