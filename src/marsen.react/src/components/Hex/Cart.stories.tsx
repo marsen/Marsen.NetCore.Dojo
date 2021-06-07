7d470299830dac57ace83ccb016956b0924ec2cb
Marsen <thisismysoul@gmail.com>
2021-06-07 15:15:09 +0800

    change rank

--- src/marsen.react/src/components/Hex/Cart.stories.tsx
+++ src/marsen.react/src/components/Hex/Cart.stories.tsx
@@ -12,8 +12,7 @@ export default {
 };
 
 export const Default = () => <App />
-export const ThePropDemo = () => <PropDemo />
+export const TheProduct = () => <Product cartItem={[]} setCartItem />
 export const TheProductCard = () => <ProductCard name="Pizza" description="pizza 的樣子" price={250} cartItem={[]} setCartItem picture="https://images.unsplash.com/photo-1506354666786-959d6d497f1a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" />
 export const TheProductCarousel = () => <ProductCarousel />
-export const TheProduct = () => <Product cartItem={[]} setCartItem />
-
+export const ThePropDemo = () => <PropDemo />
