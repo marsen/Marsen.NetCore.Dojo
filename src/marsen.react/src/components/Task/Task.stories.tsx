9ae8c82c41009a1338fa803b98ebfd5ed03d926c
Marsen <thisismysoul@gmail.com>
2021-06-08 15:11:51 +0800

    PRETTIER

--- src/marsen.react/src/components/Task/Task.stories.tsx
+++ src/marsen.react/src/components/Task/Task.stories.tsx
@@ -3,33 +3,30 @@ import { Story } from '@storybook/react/types-6-0';
 import React from 'react';
 import Task, { TaskItem, TaskProps, TaskState } from './Task';
 
+export default { component: Task, title: 'Task' };
 
-export default {  component: Task,
-  title: 'Task',
-};
+const Template: Story<TaskProps> = (args) => <Task {...args} />;
 
-const Template:Story<TaskProps> = args => <Task {...args} />;
-
-var defaultItem:TaskItem = { 
-  id:'1',
-  title:'Test Task',
-  state:TaskState.Inbox,
+var defaultItem: TaskItem = {
+  id: '1',
+  title: 'Test Task',
+  state: TaskState.Inbox,
   updatedAt: new Date(2018, 0, 1, 9, 0),
 };
 
 export const Default = Template.bind({});
-Default.args = { item: defaultItem, }
+Default.args = { item: defaultItem };
 
 export const Pinned = Template.bind({});
 var pinnedItem = Copy(defaultItem);
-pinnedItem.state=TaskState.Pinned
+pinnedItem.state = TaskState.Pinned;
 Pinned.args = { item: pinnedItem };
 
 export const Archived = Template.bind({});
 var archivedItem = Copy(defaultItem);
-archivedItem.state=TaskState.Archived;
-Archived.args = {item: archivedItem};
+archivedItem.state = TaskState.Archived;
+Archived.args = { item: archivedItem };
 
-function Copy(obj:any) {  
-  return Object.assign({},obj);
+function Copy(obj: any) {
+  return Object.assign({}, obj);
 }
