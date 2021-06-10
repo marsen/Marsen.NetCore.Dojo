9e8cd5ef866dbe1a779b8aa307529cec02c0c1c7
Marsen <thisismysoul@gmail.com>
2021-06-10 10:42:10 +0800

    remove paddding

--- src/marsen.react/src/components/MaterialUI/Templates.stories.tsx
+++ src/marsen.react/src/components/MaterialUI/Templates.stories.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import { ThemeProvider } from '@material-ui/core';
-import App from './Copyright';
-import theme from './theme';
+
 import Album from './templates/album/Album';
 import Blog from './templates/blog/Blog';
 import Checkout from './templates/checkout/Checkout';
 import Dashboard from './templates/dashboard/Dashboard';
 import Pricing from './templates/pricing/Pricing';
-import SignIn from './templates/sign-in/SignIn';
 import SignInSide from './templates/sign-in-side/SignInSide';
+import SignIn from './templates/sign-in/SignIn';
 import SignUp from './templates/sign-up/SignUp';
 import StickFooter from './templates/sticky-footer/StickyFooter';
 
-
 export default {
-  component: Button,
+  component: Dashboard,
   title: 'Material/Templates',
-  decorators: [(story: () => React.ReactNode) => <div style={{ padding: '3rem' }}>{story()}</div>],
+  decorators: [(story: () => React.ReactNode) => <div>{story()}</div>],
   // Our exports that end in "Data" are not stories.
   excludeStories: /.*Data$/,
 };
