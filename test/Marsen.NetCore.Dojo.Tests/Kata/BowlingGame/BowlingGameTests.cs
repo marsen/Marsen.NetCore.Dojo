f700f14f453b7f6839a256c321058f0fba701cd8
Marsen <thisismysoul@gmail.com>
2021-06-23 09:46:32 +0800

    remove useless using

--- test/Marsen.NetCore.Dojo.Tests/Kata/BowlingGame/BowlingGameTests.cs
+++ test/Marsen.NetCore.Dojo.Tests/Kata/BowlingGame/BowlingGameTests.cs
@@ -1,7 +1,5 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 using Xunit;
 
 namespace Marsen.NetCore.Dojo.Tests.Kata.BowlingGame
