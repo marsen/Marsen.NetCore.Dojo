bdac9ec25d8146d55da8ee3a31940e48e97a5776
Marsen <thisismysoul@gmail.com>
2021-06-24 12:23:13 +0800

    clean up code

--- test/Marsen.NetCore.Dojo.Tests/Kata/BowlingGame/Frame.cs
+++ test/Marsen.NetCore.Dojo.Tests/Kata/BowlingGame/Frame.cs
@@ -4,52 +4,33 @@ namespace Marsen.NetCore.Dojo.Tests.Kata.BowlingGame
     {
         private readonly int? _firstTry;
         private readonly int? _secondTry;
-        private int? _score;
 
         public Frame(int? firstTry = null, int? secondTry = null)
         {
             _firstTry = firstTry;
             _secondTry = secondTry;
-            if (_firstTry + _secondTry != 10)
-            {
-                _score = _firstTry + _secondTry;
-            }
+            if (_firstTry + _secondTry != 10) Score = _firstTry + _secondTry;
         }
 
-        public int? Score
-        {
-            get { return _score; }
-        }
+        public int? Score { get; private set; }
+
+        private string BonusType { get; set; }
 
         public void SetBonus(int spareBonus, int? strikeBonus)
         {
-            if (BonusType == "Spare")
-            {
-                _score = _firstTry + _secondTry + spareBonus;
-                BonusCount--;
-            }
+            if (BonusType == "Spare") Score = _firstTry + _secondTry + spareBonus;
 
-            if (BonusType=="Strike")
-            {
-                _score = 10 + spareBonus + strikeBonus;
-                BonusCount--;
-            }
+            if (BonusType == "Strike") Score = 10 + spareBonus + strikeBonus;
         }
 
         public void Spare()
         {
             BonusType = "Spare";
-            BonusCount = 1;
         }
-
-        private string BonusType { get; set; }
-
-        public int BonusCount { get; private set; }
 
         public void Strike()
         {
             BonusType = "Strike";
-            BonusCount = 2;
         }
     }
 }
