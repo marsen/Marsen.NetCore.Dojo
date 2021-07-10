fbbc6b283e20a4c4274e4757a24c4fa980fe6e1f
Marsen <thisismysoul@gmail.com>
2021-07-10 10:03:17 +0800

    Add Test Case 4A0B

---
+++ test/Marsen.NetCore.Dojo.Tests/Kata/GuessNumber/GuessNumberTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Marsen.NetCore.Dojo.Tests.Kata.GuessNumber
+{
+    public class GuessNumberTests
+    {
+        [Fact]
+        public void TestItShouldGet4A0B()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
