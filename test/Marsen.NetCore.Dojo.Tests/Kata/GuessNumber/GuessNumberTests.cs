497f2717752ffcee5a3666b3de07b61cd7012886
Marsen <thisismysoul@gmail.com>
2021-07-10 10:07:47 +0800

    Create API

--- test/Marsen.NetCore.Dojo.Tests/Kata/GuessNumber/GuessNumberTests.cs
+++ test/Marsen.NetCore.Dojo.Tests/Kata/GuessNumber/GuessNumberTests.cs
@@ -8,6 +8,16 @@ namespace Marsen.NetCore.Dojo.Tests.Kata.GuessNumber
         [Fact]
         public void TestItShouldGet4A0B()
         {
+            var game = new Game();
+            string result = game.Guess("1234");
+            Assert.Equal("4A0B", result);
+        }
+    }
+
+    public class Game
+    {
+        public string Guess(string s)
+        {
             throw new NotImplementedException();
         }
     }
