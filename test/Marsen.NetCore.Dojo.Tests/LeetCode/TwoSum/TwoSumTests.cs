934c6ab924254f4aac1eae1584503c9907ee3371
Marsen <thisismysoul@gmail.com>
2021-08-20 23:10:49 +0800

    pretty test cases

--- test/Marsen.NetCore.Dojo.Tests/LeetCode/TwoSum/TwoSumTests.cs
+++ test/Marsen.NetCore.Dojo.Tests/LeetCode/TwoSum/TwoSumTests.cs
@@ -8,21 +8,48 @@ namespace Marsen.NetCore.Dojo.Tests.LeetCode.TwoSum
     public class TwoSumTests
     {
         private readonly Solution _solution = new();
+        private static int[] _nums;
+        private int _target;
 
         [Fact]
         public void Array_1_3_Target_4_Should_0_1()
         {
-            int[] nums = { 1, 3 };
-            int target = 4;
-            Assert.Equal(new[] { 0, 1 }, _solution.TwoSum(nums, target));
+            GivenArrayIs(1, 3);
+            GivenTargetIs(4);
+            ShouldBe(0, 1);
         }
+
 
         [Fact]
         public void Array_1_2_3_Target_4_Should_0_2()
         {
+            GivenArrayIs(1, 2, 3);
+            GivenTargetIs(4);
+            ShouldBe(0, 2);
+        }
+
+
+        [Fact(Skip = "Update Case")]
+        public void Array_1_2_3_Target_5_Should_1_2()
+        {
             int[] nums = { 1, 2, 3 };
-            int target = 4;
-            Assert.Equal(new[] { 0, 2 }, _solution.TwoSum(nums, target));
+            int target = 5;
+            Assert.Equal(new[] { 1, 2 }, _solution.TwoSum(nums, target));
+        }
+
+        private void GivenTargetIs(int target)
+        {
+            _target = target;
+        }
+
+        private void ShouldBe(params int[] args)
+        {
+            Assert.Equal(args, _solution.TwoSum(_nums, _target));
+        }
+
+        private static void GivenArrayIs(params int[] args)
+        {
+            _nums = args;
         }
     }
 
