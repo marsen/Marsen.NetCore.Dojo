5de2e82406cfdedee8f74d06b7d1f289e7ec0b27
Marsen <thisismysoul@gmail.com>
2021-08-20 22:55:00 +0800

    dynamic 2nd number case

--- test/Marsen.NetCore.Dojo.Tests/LeetCode/TwoSum/TwoSumTests.cs
+++ test/Marsen.NetCore.Dojo.Tests/LeetCode/TwoSum/TwoSumTests.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace Marsen.NetCore.Dojo.Tests.LeetCode.TwoSum
@@ -15,13 +16,23 @@ namespace Marsen.NetCore.Dojo.Tests.LeetCode.TwoSum
             int target = 4;
             Assert.Equal(new[] { 0, 1 }, _solution.TwoSum(nums, target));
         }
+
+        [Fact]
+        public void Array_1_2_3_Target_4_Should_0_2()
+        {
+            int[] nums = { 1, 2, 3 };
+            int target = 4;
+            Assert.Equal(new[] { 0, 2 }, _solution.TwoSum(nums, target));
+        }
     }
 
     public class Solution
     {
         public int[] TwoSum(int[] nums, int target)
         {
-            return new[] { 0, 1 };
+            var a = nums[0];
+            var index = nums.ToList().FindIndex(b => a + b == target);
+            return new[] { 0, index };
         }
     }
 }
