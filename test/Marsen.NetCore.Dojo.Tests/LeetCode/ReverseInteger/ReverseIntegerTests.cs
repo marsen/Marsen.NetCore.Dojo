29f156d83dd6a197d21eb15549d1b32245ec1c29
Marsen <thisismysoul@gmail.com>
2021-08-22 01:39:16 +0800

    first test case for interface

---
+++ test/Marsen.NetCore.Dojo.Tests/LeetCode/ReverseInteger/ReverseIntegerTests.cs
@@ -0,0 +1,22 @@
+using System;
+using Xunit;
+
+namespace Marsen.NetCore.Dojo.Tests.LeetCode.ReverseInteger
+{
+    public class ReverseIntegerTests
+    {
+        [Fact]
+        public void The_1_Reverse_1()
+        {
+            var solution = new Solution();
+            var actual = solution.Reverse(1);
+            Assert.Equal(1,actual);
+        }
+    }
+    
+    public class Solution {
+        public int Reverse(int x) {
+        
+        }
+    }
+}
