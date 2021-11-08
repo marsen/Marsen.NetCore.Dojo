455f34165ea1f0cacfd2be756a0a4782d799bbf2
Marsen <thisismysoul@gmail.com>
2021-11-09 02:55:54 +0800

    update 6.0

--- test/Marsen.NetCore.Dojo.Tests/LeetCode/ReverseInteger/ReverseIntegerTests.cs
+++ test/Marsen.NetCore.Dojo.Tests/LeetCode/ReverseInteger/ReverseIntegerTests.cs
@@ -44,6 +44,13 @@ namespace Marsen.NetCore.Dojo.Tests.LeetCode.ReverseInteger
             var actual = _solution.Reverse(120);
             Assert.Equal(21, actual);
         }
+
+        [Fact]
+        public void The_Negative_2147483648_Reverse_0()
+        {
+            var actual = _solution.Reverse(-2147483648);
+            Assert.Equal(0, actual);
+        }
     }
 
     public class Solution
@@ -51,8 +58,17 @@ namespace Marsen.NetCore.Dojo.Tests.LeetCode.ReverseInteger
         public int Reverse(int x)
         {
             int symbol = x < 0 ? -1 : 1;
-            int.TryParse(new string((symbol * x).ToString().Reverse().ToArray()), out var result);
-            return symbol * result;
+            if (x <= int.MinValue)
+            {
+                return 0;
+            }
+            long tmp = long.Parse(new string((symbol * x).ToString().Reverse().ToArray()));
+            if (tmp > int.MaxValue || tmp < int.MinValue)
+            {
+                return 0;
+            }
+
+            return symbol * (int)tmp;
         }
     }
 }
