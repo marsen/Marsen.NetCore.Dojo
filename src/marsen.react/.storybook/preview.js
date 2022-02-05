37e851d6d7026921d4d3a450334bb8c248f51831
Marsen <thisismysoul@gmail.com>
2022-02-05 17:59:36 +0800

    remove react

--- src/marsen.react/.storybook/preview.js
+++
@@ -1,4 +0,0 @@
-// .storybook/preview.js
-
-// import '../src/index.css';
-import "bootstrap/dist/css/bootstrap.min.css"; //./src/components/Hex/*.stories.tsx
--- src/marsen.react/src/components/Hex/HexCart.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function HexCart() {
-  return <h1 className="text-center">Hello Mark</h1>;
-}
