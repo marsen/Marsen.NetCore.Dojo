941597de3e7f02dc8828a84ab063fccb73d17bd5
Mark Lin <mark.lin@ttfb-lab.com>
2022-02-15 15:29:56 +0800

    fix warning

--- test/Marsen.NetCore.Dojo.Tests/Books/TddByExample/WyCashTests.cs
+++ test/Marsen.NetCore.Dojo.Tests/Books/TddByExample/WyCashTests.cs
@@ -5,104 +5,104 @@ namespace Marsen.NetCore.Dojo.Tests.Books.TddByExample
 {
     public class WyCashTests
     {
-        private readonly Bank bank = new();
-        private readonly Money fiveBulks = Money.dollar(5);
-        private readonly Money fiveFrancs = Money.franc(5);
-        private readonly Money fourBulks = Money.dollar(4);
-        private readonly Money temFrancs = Money.franc(10);
+        private readonly Bank _bank = new();
+        private readonly Money _fiveBulks = Money.dollar(5);
+        private readonly Money _fiveFrancs = Money.franc(5);
+        private readonly Money _fourBulks = Money.dollar(4);
+        private readonly Money _temFrancs = Money.franc(10);
 
         public WyCashTests()
         {
-            bank.addRate("CHF", "USD", 2);
+            _bank.addRate("CHF", "USD", 2);
         }
 
         [Fact]
-        public void testMultiplication()
+        public void TestMultiplication()
         {
-            Assert.Equal(Money.dollar(10), fiveBulks.Times(2));
-            Assert.Equal(Money.dollar(15), fiveBulks.Times(3));
+            Assert.Equal(Money.dollar(10), _fiveBulks.Times(2));
+            Assert.Equal(Money.dollar(15), _fiveBulks.Times(3));
         }
 
         [Fact]
-        public void testFrancMultiplication()
+        public void TestFrancMultiplication()
         {
-            Assert.Equal(Money.franc(10), fiveFrancs.Times(2));
-            Assert.Equal(Money.franc(15), fiveFrancs.Times(3));
+            Assert.Equal(Money.franc(10), _fiveFrancs.Times(2));
+            Assert.Equal(Money.franc(15), _fiveFrancs.Times(3));
         }
 
         [Fact]
-        public void testEquality()
+        public void TestEquality()
         {
-            Assert.True(Money.dollar(5).Equals(Money.dollar(5)));
+            Assert.Equal(Money.dollar(5),Money.dollar(5));
             Assert.False(Money.dollar(5).Equals(Money.dollar(6)));
-            Assert.True(Money.franc(5).Equals(Money.franc(5)));
+            Assert.Equal(Money.franc(5),Money.franc(5));
             Assert.False(Money.franc(5).Equals(Money.franc(6)));
             Assert.False(Money.franc(5).Equals(Money.dollar(5)));
         }
 
         [Fact]
-        public void testCurrency()
+        public void TestCurrency()
         {
             Assert.Equal("USD", Money.dollar(1).Currency);
             Assert.Equal("CHF", Money.franc(1).Currency);
         }
 
         [Fact]
-        public void testSimpleAddition()
+        public void TestSimpleAddition()
         {
-            Assert.Equal(Money.dollar(10), bank.reduce(fiveBulks.Plus(fiveBulks), "USD"));
-            Assert.Equal(Money.dollar(9), bank.reduce(fiveBulks.Plus(fourBulks), "USD"));
+            Assert.Equal(Money.dollar(10), _bank.reduce(_fiveBulks.Plus(_fiveBulks), "USD"));
+            Assert.Equal(Money.dollar(9), _bank.reduce(_fiveBulks.Plus(_fourBulks), "USD"));
         }
 
         [Fact]
-        public void testMixedAddition()
+        public void TestMixedAddition()
         {
-            Assert.Equal(Money.dollar(10), bank.reduce(fiveBulks.Plus(temFrancs), "USD"));
-            Assert.Equal(Money.dollar(10), bank.reduce(temFrancs.Plus(fiveBulks), "USD"));
+            Assert.Equal(Money.dollar(10), _bank.reduce(_fiveBulks.Plus(_temFrancs), "USD"));
+            Assert.Equal(Money.dollar(10), _bank.reduce(_temFrancs.Plus(_fiveBulks), "USD"));
         }
 
 
         [Fact]
-        public void testPlusReturnsSum()
+        public void TestPlusReturnsSum()
         {
-            var sum = (Sum)fiveBulks.Plus(fiveBulks);
-            Assert.Equal(fiveBulks, sum.Addend);
-            Assert.Equal(fiveBulks, sum.Augend);
+            var sum = (Sum)_fiveBulks.Plus(_fiveBulks);
+            Assert.Equal(_fiveBulks, sum.Addend);
+            Assert.Equal(_fiveBulks, sum.Augend);
         }
 
         [Fact]
-        public void testReduceMoneyExchangeCurrency()
+        public void TestReduceMoneyExchangeCurrency()
         {
-            Assert.Equal(Money.dollar(1), bank.reduce(Money.franc(2), "USD"));
-            Assert.Equal(Money.dollar(3), bank.reduce(Money.franc(6), "USD"));
-            Assert.Equal(Money.dollar(3), bank.reduce(Money.dollar(3), "USD"));
+            Assert.Equal(Money.dollar(1), _bank.reduce(Money.franc(2), "USD"));
+            Assert.Equal(Money.dollar(3), _bank.reduce(Money.franc(6), "USD"));
+            Assert.Equal(Money.dollar(3), _bank.reduce(Money.dollar(3), "USD"));
         }
 
         [Fact]
-        public void testSumPlusMoney()
+        public void TestSumPlusMoney()
         {
-            var sum = new Sum(fiveBulks, temFrancs).Plus(fiveBulks);
-            Assert.Equal(Money.dollar(15), sum.Reduce(bank, "USD"));
+            var sum = new Sum(_fiveBulks, _temFrancs).Plus(_fiveBulks);
+            Assert.Equal(Money.dollar(15), sum.Reduce(_bank, "USD"));
         }
 
         [Fact]
-        public void testSumTimes()
+        public void TestSumTimes()
         {
-            var sum = new Sum(fiveBulks, temFrancs).Times(2);
-            Assert.Equal(Money.dollar(20), sum.Reduce(bank, "USD"));
+            var sum = new Sum(_fiveBulks, _temFrancs).Times(2);
+            Assert.Equal(Money.dollar(20), sum.Reduce(_bank, "USD"));
         }
 
         [Fact]
-        public void testMoneyTimes()
+        public void TestMoneyTimes()
         {
-            var dollar = (Money)fiveBulks.Times(2);
-            var francs = (Money)temFrancs.Times(2);
-            Assert.Equal(Money.dollar(10), dollar.Reduce(bank, "USD"));
-            Assert.Equal(Money.franc(20), francs.Reduce(bank, "CHF"));
+            var dollar = (Money)_fiveBulks.Times(2);
+            var francs = (Money)_temFrancs.Times(2);
+            Assert.Equal(Money.dollar(10), dollar.Reduce(_bank, "USD"));
+            Assert.Equal(Money.franc(20), francs.Reduce(_bank, "CHF"));
         }
 
         [Fact]
-        public void testMoneyToString()
+        public void TestMoneyToString()
         {
             var dollar = Money.dollar(10);
             var francs = Money.franc(10);
