3e01333014afa602561f2697cd2492524d15a4c2
Mark Lin <mark.lin@ttfb-lab.com>
2022-02-16 15:04:03 +0800

    replace for with foreach

--- src/Marsen.NetCore.Dojo/LeetCode/TwoSum/Solution.cs
+++ src/Marsen.NetCore.Dojo/LeetCode/TwoSum/Solution.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Marsen.NetCore.Dojo.LeetCode.TwoSum
 {
@@ -8,19 +9,23 @@ namespace Marsen.NetCore.Dojo.LeetCode.TwoSum
         {
             var result = new int[2];
             var lookup = new Dictionary<int, int>();
-            for (var i = 0; i < nums.Length; i++)
+            int i = 0;
+            foreach (var n in nums)
             {
-                if (lookup.ContainsKey(target - nums[i]))
+                if (lookup.ContainsKey(target - n))
                 {
-                    result[0] = lookup[target - nums[i]];
+                    result[0] = lookup[target - n];
                     result[1] = i;
-                    break;
+                    return result;
                 }
 
                 if (!lookup.ContainsKey(nums[i]))
                 {
                     lookup.Add(nums[i], i);
                 }
+
+                i++;
+
             }
 
             return result;
