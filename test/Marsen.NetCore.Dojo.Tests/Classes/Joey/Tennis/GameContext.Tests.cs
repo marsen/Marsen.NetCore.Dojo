66cb5c6599f143354af74953bdbb3f8f8b8f7422
Marsen <thisismysoul@gmail.com>
2022-04-18 15:47:48 +0800

    file scope namespce

--- test/Marsen.NetCore.Dojo.Tests/Classes/Joey/Tennis/GameContext.Tests.cs
+++ test/Marsen.NetCore.Dojo.Tests/Classes/Joey/Tennis/GameContext.Tests.cs
@@ -1,189 +1,188 @@
 ﻿using Marsen.NetCore.Dojo.Classes.Joey.Tennis.StateV2;
 using Xunit;
 
-namespace Marsen.NetCore.Dojo.Tests.Classes.Joey.Tennis
+namespace Marsen.NetCore.Dojo.Tests.Classes.Joey.Tennis;
+
+public class GameContextTests
 {
-    public class GameContextTests
+    private readonly GameContext _context = new("Sam", "Ben");
+
+    [Fact]
+    public void LoveAll()
     {
-        private readonly GameContext _context = new("Sam", "Ben");
+        ScoreShouldBe("Love All");
+    }
 
-        [Fact]
-        public void LoveAll()
-        {
-            ScoreShouldBe("Love All");
-        }
+    [Fact]
+    public void LoveFifteen()
+    {
+        GiveReceiverScore(1);
+        ScoreShouldBe("Love Fifteen");
+    }
 
-        [Fact]
-        public void LoveFifteen()
+    [Fact]
+    public void LoveThirty()
+    {
+        GiveReceiverScore(2);
+        ScoreShouldBe("Love Thirty");
+    }
+
+    [Fact]
+    public void LoveForty()
+    {
+        GiveReceiverScore(3);
+        ScoreShouldBe("Love Forty");
+    }
+
+    [Fact]
+    public void ReceiverWin()
+    {
+        GiveReceiverScore(4);
+        ScoreShouldBe("Ben Win");
+    }
+
+
+    [Fact]
+    public void FifteenForty()
+    {
+        GiveReceiverScore(3);
+        GiveServerScore(1);
+        ScoreShouldBe("Fifteen Forty");
+    }
+
+
+    [Fact]
+    public void FifteenLove()
+    {
+        GiveServerScore(1);
+        ScoreShouldBe("Fifteen Love");
+    }
+
+    [Fact]
+    public void ThirtyLove()
+    {
+        GiveServerScore(2);
+        ScoreShouldBe("Thirty Love");
+    }
+
+    [Fact]
+    public void ThirtyFifteen()
+    {
+        GiveServerScore(2);
+        GiveReceiverScore(1);
+        ScoreShouldBe("Thirty Fifteen");
+    }
+
+
+    [Fact]
+    public void FortyLove()
+    {
+        GiveServerScore(3);
+        ScoreShouldBe("Forty Love");
+    }
+
+    [Fact]
+    public void ServerWin()
+    {
+        GiveServerScore(4);
+        ScoreShouldBe("Sam Win");
+    }
+
+
+    [Fact]
+    public void FifteenAll()
+    {
+        GiveServerScore(1);
+        GiveReceiverScore(1);
+        ScoreShouldBe("Fifteen All");
+    }
+
+    [Fact]
+    public void ThirtyAll()
+    {
+        GiveReceiverScore(2);
+        GiveServerScore(2);
+        ScoreShouldBe("Thirty All");
+    }
+
+    [Fact]
+    public void Deuce()
+    {
+        GivenDeuce(3);
+        ScoreShouldBe("Deuce");
+    }
+
+    [Fact]
+    public void ServerAdv()
+    {
+        GivenDeuce(3);
+        GiveServerScore(1);
+        ScoreShouldBe("Sam Adv");
+    }
+
+    [Fact]
+    public void ServerWinAfterDeuce()
+    {
+        GivenDeuce(3);
+        GiveServerScore(2);
+        ScoreShouldBe("Sam Win");
+    }
+
+
+    [Fact]
+    public void ReceiverAdv()
+    {
+        GivenDeuce(3);
+        GiveReceiverScore(1);
+        ScoreShouldBe("Ben Adv");
+    }
+
+    [Fact]
+    public void ReceiverWinAfterDeuce()
+    {
+        GivenDeuce(3);
+        GiveReceiverScore(2);
+        ScoreShouldBe("Ben Win");
+    }
+
+
+    [Fact]
+    public void Deuce_When_4_4()
+    {
+        GivenDeuce(3);
+        GiveReceiverScore(1);
+        GiveServerScore(1);
+        ScoreShouldBe("Deuce");
+    }
+
+    [Fact]
+    public void Deuce_When_100_100()
+    {
+        GivenDeuce(100);
+        ScoreShouldBe("Deuce");
+    }
+
+    private void GivenDeuce(int times)
+    {
+        for (var i = 0; i < times; i++)
         {
             GiveReceiverScore(1);
-            ScoreShouldBe("Love Fifteen");
-        }
-
-        [Fact]
-        public void LoveThirty()
-        {
-            GiveReceiverScore(2);
-            ScoreShouldBe("Love Thirty");
-        }
-
-        [Fact]
-        public void LoveForty()
-        {
-            GiveReceiverScore(3);
-            ScoreShouldBe("Love Forty");
-        }
-
-        [Fact]
-        public void ReceiverWin()
-        {
-            GiveReceiverScore(4);
-            ScoreShouldBe("Ben Win");
-        }
-
-
-        [Fact]
-        public void FifteenForty()
-        {
-            GiveReceiverScore(3);
             GiveServerScore(1);
-            ScoreShouldBe("Fifteen Forty");
         }
+    }
 
+    private void GiveServerScore(int times)
+    {
+        for (var i = 0; i < times; i++) _context.ServerScore();
+    }
 
-        [Fact]
-        public void FifteenLove()
-        {
-            GiveServerScore(1);
-            ScoreShouldBe("Fifteen Love");
-        }
+    private void GiveReceiverScore(int times)
+    {
+        for (var i = 0; i < times; i++) _context.ReceiverScore();
+    }
 
-        [Fact]
-        public void ThirtyLove()
-        {
-            GiveServerScore(2);
-            ScoreShouldBe("Thirty Love");
-        }
-
-        [Fact]
-        public void ThirtyFifteen()
-        {
-            GiveServerScore(2);
-            GiveReceiverScore(1);
-            ScoreShouldBe("Thirty Fifteen");
-        }
-
-
-        [Fact]
-        public void FortyLove()
-        {
-            GiveServerScore(3);
-            ScoreShouldBe("Forty Love");
-        }
-
-        [Fact]
-        public void ServerWin()
-        {
-            GiveServerScore(4);
-            ScoreShouldBe("Sam Win");
-        }
-
-
-        [Fact]
-        public void FifteenAll()
-        {
-            GiveServerScore(1);
-            GiveReceiverScore(1);
-            ScoreShouldBe("Fifteen All");
-        }
-
-        [Fact]
-        public void ThirtyAll()
-        {
-            GiveReceiverScore(2);
-            GiveServerScore(2);
-            ScoreShouldBe("Thirty All");
-        }
-
-        [Fact]
-        public void Deuce()
-        {
-            GivenDeuce(3);
-            ScoreShouldBe("Deuce");
-        }
-
-        [Fact]
-        public void ServerAdv()
-        {
-            GivenDeuce(3);
-            GiveServerScore(1);
-            ScoreShouldBe("Sam Adv");
-        }
-
-        [Fact]
-        public void ServerWinAfterDeuce()
-        {
-            GivenDeuce(3);
-            GiveServerScore(2);
-            ScoreShouldBe("Sam Win");
-        }
-
-
-        [Fact]
-        public void ReceiverAdv()
-        {
-            GivenDeuce(3);
-            GiveReceiverScore(1);
-            ScoreShouldBe("Ben Adv");
-        }
-
-        [Fact]
-        public void ReceiverWinAfterDeuce()
-        {
-            GivenDeuce(3);
-            GiveReceiverScore(2);
-            ScoreShouldBe("Ben Win");
-        }
-
-
-        [Fact]
-        public void Deuce_When_4_4()
-        {
-            GivenDeuce(3);
-            GiveReceiverScore(1);
-            GiveServerScore(1);
-            ScoreShouldBe("Deuce");
-        }
-
-        [Fact]
-        public void Deuce_When_100_100()
-        {
-            GivenDeuce(100);
-            ScoreShouldBe("Deuce");
-        }
-
-        private void GivenDeuce(int times)
-        {
-            for (var i = 0; i < times; i++)
-            {
-                GiveReceiverScore(1);
-                GiveServerScore(1);
-            }
-        }
-
-        private void GiveServerScore(int times)
-        {
-            for (var i = 0; i < times; i++) _context.ServerScore();
-        }
-
-        private void GiveReceiverScore(int times)
-        {
-            for (var i = 0; i < times; i++) _context.ReceiverScore();
-        }
-
-        private void ScoreShouldBe(string expected)
-        {
-            Assert.Equal(expected, _context.Score());
-        }
+    private void ScoreShouldBe(string expected)
+    {
+        Assert.Equal(expected, _context.Score());
     }
 }
--- test/Marsen.NetCore.Dojo.Tests/Classes/Joey/Tennis/TennisGameContextTests.cs
+++ test/Marsen.NetCore.Dojo.Tests/Classes/Joey/Tennis/TennisGameContextTests.cs
@@ -1,266 +1,265 @@
 ﻿using Marsen.NetCore.Dojo.Classes.Joey.Tennis;
 using Xunit;
 
-namespace Marsen.NetCore.Dojo.Tests.Classes.Joey.Tennis
+namespace Marsen.NetCore.Dojo.Tests.Classes.Joey.Tennis;
+
+public class TennisGameContextTests
 {
-    public class TennisGameContextTests
+    private readonly TennisGameContext _tennisGameContext;
+
+    public TennisGameContextTests()
     {
-        private readonly TennisGameContext _tennisGameContext;
+        _tennisGameContext = new TennisGameContext("Mark", "Iris");
+    }
 
-        public TennisGameContextTests()
-        {
-            _tennisGameContext = new TennisGameContext("Mark", "Iris");
-        }
+    [Fact]
+    public void Love_All()
+    {
+        ScoreShouldBe("Love All");
+    }
 
-        [Fact]
-        public void Love_All()
-        {
-            ScoreShouldBe("Love All");
-        }
+    [Fact]
+    public void Fifteen_Love()
+    {
+        GivenServerPoint(1);
+        ScoreShouldBe("Fifteen Love");
+    }
 
-        [Fact]
-        public void Fifteen_Love()
-        {
-            GivenServerPoint(1);
-            ScoreShouldBe("Fifteen Love");
-        }
+    [Fact]
+    public void Love_Fifteen()
+    {
+        GivenReceiverPoint(1);
+        ScoreShouldBe("Love Fifteen");
+    }
 
-        [Fact]
-        public void Love_Fifteen()
-        {
-            GivenReceiverPoint(1);
-            ScoreShouldBe("Love Fifteen");
-        }
+    [Fact]
+    public void Love_Thirty()
+    {
+        GivenReceiverPoint(2);
+        ScoreShouldBe("Love Thirty");
+    }
 
-        [Fact]
-        public void Love_Thirty()
-        {
-            GivenReceiverPoint(2);
-            ScoreShouldBe("Love Thirty");
-        }
+    [Fact]
+    public void Love_Forty()
+    {
+        GivenReceiverPoint(3);
+        ScoreShouldBe("Love Forty");
+    }
 
-        [Fact]
-        public void Love_Forty()
-        {
-            GivenReceiverPoint(3);
-            ScoreShouldBe("Love Forty");
-        }
+    [Fact]
+    public void Fifteen_All()
+    {
+        GivenServerPoint(1);
+        GivenReceiverPoint(1);
+        ScoreShouldBe("Fifteen All");
+    }
 
-        [Fact]
-        public void Fifteen_All()
-        {
-            GivenServerPoint(1);
-            GivenReceiverPoint(1);
-            ScoreShouldBe("Fifteen All");
-        }
-
-        [Fact]
-        public void LoveFifteen_To_FifteenAll()
-        {
-            GivenReceiverPoint(1);
-            GivenServerPoint(1);
-            ScoreShouldBe("Fifteen All");
-        }
+    [Fact]
+    public void LoveFifteen_To_FifteenAll()
+    {
+        GivenReceiverPoint(1);
+        GivenServerPoint(1);
+        ScoreShouldBe("Fifteen All");
+    }
 
 
-        [Fact]
-        public void Thirty_Love()
-        {
-            GivenServerPoint(2);
-            ScoreShouldBe("Thirty Love");
-        }
+    [Fact]
+    public void Thirty_Love()
+    {
+        GivenServerPoint(2);
+        ScoreShouldBe("Thirty Love");
+    }
 
-        [Fact]
-        public void Forty_Love()
-        {
-            GivenServerPoint(3);
-            ScoreShouldBe("Forty Love");
-        }
+    [Fact]
+    public void Forty_Love()
+    {
+        GivenServerPoint(3);
+        ScoreShouldBe("Forty Love");
+    }
 
-        [Fact]
-        public void Forty_Fifteen()
-        {
-            GivenServerPoint(3);
-            GivenReceiverPoint(1);
-            ScoreShouldBe("Forty Fifteen");
-        }
+    [Fact]
+    public void Forty_Fifteen()
+    {
+        GivenServerPoint(3);
+        GivenReceiverPoint(1);
+        ScoreShouldBe("Forty Fifteen");
+    }
 
-        [Fact]
-        public void Fifteen_Thirty()
-        {
-            GivenServerPoint(1);
-            GivenReceiverPoint(2);
-            ScoreShouldBe("Fifteen Thirty");
-        }
+    [Fact]
+    public void Fifteen_Thirty()
+    {
+        GivenServerPoint(1);
+        GivenReceiverPoint(2);
+        ScoreShouldBe("Fifteen Thirty");
+    }
 
-        [Fact]
-        public void Thirty_Fifteen()
-        {
-            GivenServerPoint(2);
-            GivenReceiverPoint(1);
-            ScoreShouldBe("Thirty Fifteen");
-        }
+    [Fact]
+    public void Thirty_Fifteen()
+    {
+        GivenServerPoint(2);
+        GivenReceiverPoint(1);
+        ScoreShouldBe("Thirty Fifteen");
+    }
 
-        [Fact]
-        public void FifteenAll_To_ThirtyFifteen()
-        {
-            GivenServerPoint(1);
-            GivenReceiverPoint(1);
-            GivenServerPoint(1);
-            ScoreShouldBe("Thirty Fifteen");
-        }
+    [Fact]
+    public void FifteenAll_To_ThirtyFifteen()
+    {
+        GivenServerPoint(1);
+        GivenReceiverPoint(1);
+        GivenServerPoint(1);
+        ScoreShouldBe("Thirty Fifteen");
+    }
 
-        [Fact]
-        public void FifteenAll_To_FifteenThirty()
-        {
-            GivenReceiverPoint(1);
-            GivenServerPoint(1);
-            GivenReceiverPoint(1);
-            ScoreShouldBe("Fifteen Thirty");
-        }
+    [Fact]
+    public void FifteenAll_To_FifteenThirty()
+    {
+        GivenReceiverPoint(1);
+        GivenServerPoint(1);
+        GivenReceiverPoint(1);
+        ScoreShouldBe("Fifteen Thirty");
+    }
 
-        [Fact]
-        public void ThirtyAll()
-        {
-            GivenReceiverPoint(2);
-            GivenServerPoint(2);
-            ScoreShouldBe("Thirty All");
-        }
+    [Fact]
+    public void ThirtyAll()
+    {
+        GivenReceiverPoint(2);
+        GivenServerPoint(2);
+        ScoreShouldBe("Thirty All");
+    }
 
-        [Fact]
-        public void FifteenForty()
-        {
-            GivenServerPoint(1);
-            GivenReceiverPoint(3);
-            ScoreShouldBe("Fifteen Forty");
-        }
+    [Fact]
+    public void FifteenForty()
+    {
+        GivenServerPoint(1);
+        GivenReceiverPoint(3);
+        ScoreShouldBe("Fifteen Forty");
+    }
 
-        [Fact]
-        public void FortyFifteen()
-        {
-            GivenServerPoint(3);
-            GivenReceiverPoint(1);
-            ScoreShouldBe("Forty Fifteen");
-        }
+    [Fact]
+    public void FortyFifteen()
+    {
+        GivenServerPoint(3);
+        GivenReceiverPoint(1);
+        ScoreShouldBe("Forty Fifteen");
+    }
 
-        [Fact]
-        public void FortyThirty()
-        {
-            GivenServerPoint(3);
-            GivenReceiverPoint(2);
-            ScoreShouldBe("Forty Thirty");
-        }
+    [Fact]
+    public void FortyThirty()
+    {
+        GivenServerPoint(3);
+        GivenReceiverPoint(2);
+        ScoreShouldBe("Forty Thirty");
+    }
 
-        [Fact]
-        public void ThirtyForty()
-        {
-            GivenServerPoint(2);
-            GivenReceiverPoint(3);
-            ScoreShouldBe("Thirty Forty");
-        }
+    [Fact]
+    public void ThirtyForty()
+    {
+        GivenServerPoint(2);
+        GivenReceiverPoint(3);
+        ScoreShouldBe("Thirty Forty");
+    }
 
-        [Fact]
-        public void Deuce()
-        {
-            GivenDeuce();
-            ScoreShouldBe("Deuce");
-        }
+    [Fact]
+    public void Deuce()
+    {
+        GivenDeuce();
+        ScoreShouldBe("Deuce");
+    }
 
-        private void GivenDeuce()
-        {
-            GivenServerPoint(3);
-            GivenReceiverPoint(3);
-        }
+    private void GivenDeuce()
+    {
+        GivenServerPoint(3);
+        GivenReceiverPoint(3);
+    }
 
-        [Fact]
-        public void Deuce_When_4_4_After_ServerScore()
-        {
-            GivenDeuce();
-            GivenReceiverPoint(1);
-            GivenServerPoint(1);
-            ScoreShouldBe("Deuce");
-        }
+    [Fact]
+    public void Deuce_When_4_4_After_ServerScore()
+    {
+        GivenDeuce();
+        GivenReceiverPoint(1);
+        GivenServerPoint(1);
+        ScoreShouldBe("Deuce");
+    }
 
-        [Fact]
-        public void Deuce_When_4_4_After_ReceiverScore()
-        {
-            GivenDeuce();
-            GivenServerPoint(1);
-            GivenReceiverPoint(1);
-            ScoreShouldBe("Deuce");
-        }
-
-
-        [Fact]
-        public void ServerAdv()
-        {
-            GivenDeuce();
-            GivenServerPoint(1);
-            ScoreShouldBe("Mark Adv");
-        }
-
-        [Fact]
-        public void ReceiverAdv()
-        {
-            GivenDeuce();
-            GivenReceiverPoint(1);
-            ScoreShouldBe("Iris Adv");
-        }
-
-        [Fact]
-        public void ReceiverWin()
-        {
-            GivenDeuce();
-            GivenReceiverPoint(2);
-            ScoreShouldBe("Iris Win");
-        }
-
-        [Fact]
-        public void ServerWin()
-        {
-            GivenDeuce();
-            GivenServerPoint(2);
-            ScoreShouldBe("Mark Win");
-        }
-
-        [Fact]
-        public void Server_Win_When_4_0()
-        {
-            GivenServerPoint(4);
-            ScoreShouldBe("Mark Win");
-        }
-
-        [Fact]
-        public void Receiver_Win_When_0_4()
-        {
-            GivenReceiverPoint(4);
-            ScoreShouldBe("Iris Win");
-        }
-
-        [Fact]
-        public void Receiver_Win_When_1_4()
-        {
-            GivenServerPoint(1);
-            GivenReceiverPoint(4);
-            ScoreShouldBe("Iris Win");
-        }
+    [Fact]
+    public void Deuce_When_4_4_After_ReceiverScore()
+    {
+        GivenDeuce();
+        GivenServerPoint(1);
+        GivenReceiverPoint(1);
+        ScoreShouldBe("Deuce");
+    }
 
 
-        private void GivenServerPoint(int times)
-        {
-            for (var i = 0; i < times; i++) _tennisGameContext.State.ServerScore();
-        }
+    [Fact]
+    public void ServerAdv()
+    {
+        GivenDeuce();
+        GivenServerPoint(1);
+        ScoreShouldBe("Mark Adv");
+    }
+
+    [Fact]
+    public void ReceiverAdv()
+    {
+        GivenDeuce();
+        GivenReceiverPoint(1);
+        ScoreShouldBe("Iris Adv");
+    }
+
+    [Fact]
+    public void ReceiverWin()
+    {
+        GivenDeuce();
+        GivenReceiverPoint(2);
+        ScoreShouldBe("Iris Win");
+    }
+
+    [Fact]
+    public void ServerWin()
+    {
+        GivenDeuce();
+        GivenServerPoint(2);
+        ScoreShouldBe("Mark Win");
+    }
+
+    [Fact]
+    public void Server_Win_When_4_0()
+    {
+        GivenServerPoint(4);
+        ScoreShouldBe("Mark Win");
+    }
+
+    [Fact]
+    public void Receiver_Win_When_0_4()
+    {
+        GivenReceiverPoint(4);
+        ScoreShouldBe("Iris Win");
+    }
+
+    [Fact]
+    public void Receiver_Win_When_1_4()
+    {
+        GivenServerPoint(1);
+        GivenReceiverPoint(4);
+        ScoreShouldBe("Iris Win");
+    }
 
 
-        private void GivenReceiverPoint(int times)
-        {
-            for (var i = 0; i < times; i++) _tennisGameContext.State.ReceiverScore();
-        }
+    private void GivenServerPoint(int times)
+    {
+        for (var i = 0; i < times; i++) _tennisGameContext.State.ServerScore();
+    }
 
 
-        private void ScoreShouldBe(string expected)
-        {
-            Assert.Equal(expected, _tennisGameContext.Score());
-        }
+    private void GivenReceiverPoint(int times)
+    {
+        for (var i = 0; i < times; i++) _tennisGameContext.State.ReceiverScore();
+    }
+
+
+    private void ScoreShouldBe(string expected)
+    {
+        Assert.Equal(expected, _tennisGameContext.Score());
     }
 }
