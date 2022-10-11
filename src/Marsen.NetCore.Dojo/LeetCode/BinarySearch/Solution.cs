fa70df2fa025216112d249ffe86c1dda6077e7eb
Marsen <thisismysoul@gmail.com>
2022-10-11 18:56:47 +0800

    add test case when array is [1] target is 1 return 0

--- src/Marsen.NetCore.Dojo/LeetCode/BinarySearch/Solution.cs
+++ src/Marsen.NetCore.Dojo/LeetCode/BinarySearch/Solution.cs
@@ -1,9 +1,16 @@
+using System.Linq;
+
 namespace Marsen.NetCore.Dojo.LeetCode.BinarySearch;
 
 public class Solution
 {
     public int Search(int[] array, int target)
     {
+        if (array.Contains(target))
+        {
+            return 0;
+        }
+
         return -1;
     }
 }
--- test/Marsen.NetCore.Dojo.Tests/LeetCode/BinarySearch/BinarySearchTests.cs
+++ test/Marsen.NetCore.Dojo.Tests/LeetCode/BinarySearch/BinarySearchTests.cs
@@ -6,6 +6,7 @@ namespace Marsen.NetCore.Dojo.Tests.LeetCode.BinarySearch;
 public class BinarySearchTests
 {
     private readonly Solution _sol = new();
+
     [Fact]
     public void You_Got_Minus_1_with_Empty_Array()
     {
@@ -15,8 +16,22 @@ public class BinarySearchTests
 
         // Act
         var ans = _sol.Search(array, target);
-        
+
         // Assert
-        Assert.Equal(-1,ans);
+        Assert.Equal(-1, ans);
+    }
+
+    [Fact]
+    public void You_Got_0_with_1_Array()
+    {
+        // Arrange
+        int[] array = {1};
+        int target = 1;
+
+        // Act
+        var ans = _sol.Search(array, target);
+
+        // Assert
+        Assert.Equal(0, ans);
     }
 }
