5e953a04f43a3040d07fae5cb614b3ba87d130e9
Marsen <thisismysoul@gmail.com>
2022-10-12 19:43:51 +0800

    remove test case

--- test/Marsen.NetCore.Dojo.Tests/LeetCode/BinarySearch/BinarySearchTests.cs
+++ test/Marsen.NetCore.Dojo.Tests/LeetCode/BinarySearch/BinarySearchTests.cs
@@ -22,23 +22,10 @@ public class BinarySearchTests
     }
 
     [Fact]
-    public void You_Got_0_with_1_Array()
-    {
-        // Arrange
-        int[] array = {1};
-        int target = 1;
-
-        // Act
-        var ans = _sol.Search(array, target);
-
-        // Assert
-        Assert.Equal(0, ans);
-    }
-    [Fact]
     public void You_Got_1_with_0_1_Array()
     {
         // Arrange
-        int[] array = {0,1};
+        int[] array = {0, 1};
         int target = 1;
 
         // Act
