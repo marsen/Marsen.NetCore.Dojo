825fa175dc681071186622be03dd9d5a38275c90
Marsen <thisismysoul@gmail.com>
2022-10-22 17:51:57 +0800

    clear code

--- test/Marsen.NetCore.Dojo.Tests/LeetCode/BinarySearch/BinarySearchTests.cs
+++ test/Marsen.NetCore.Dojo.Tests/LeetCode/BinarySearch/BinarySearchTests.cs
@@ -18,36 +18,36 @@ public class BinarySearchTests
     [Fact]
     public void Search_5_in_5_Array()
     {
-        _sol.Search(new int[] {5}, 5).Should().Be(0);
+        _sol.Search(new[] {5}, 5).Should().Be(0);
     }
 
     [Fact]
     public void Search_5_in_5_6_Array()
     {
-        _sol.Search(new int[] {5, 6}, 5).Should().Be(0);
+        _sol.Search(new[] {5, 6}, 5).Should().Be(0);
     }
 
     [Fact]
     public void Search_5_in_4_5_Array()
     {
-        _sol.Search(new int[] {4, 5}, 5).Should().Be(1);
+        _sol.Search(new[] {4, 5}, 5).Should().Be(1);
     }
 
     [Fact]
     public void Search_5_in_3_4_5_Array()
     {
-        _sol.Search(new int[] {3, 4, 5}, 5).Should().Be(2);
+        _sol.Search(new[] {3, 4, 5}, 5).Should().Be(2);
     }
 
     [Fact]
     public void Search_5_in_5_6_7_Array()
     {
-        _sol.Search(new int[] {5, 6, 7}, 5).Should().Be(0);
+        _sol.Search(new[] {5, 6, 7}, 5).Should().Be(0);
     }
-    
+
     [Fact]
     public void Search_9_in_m1_0_3_5_9_12_Array()
     {
-        _sol.Search(new int[] {-1,0,3,5,9,12}, 9).Should().Be(4);
+        _sol.Search(new[] {-1, 0, 3, 5, 9, 12}, 9).Should().Be(4);
     }
 }
