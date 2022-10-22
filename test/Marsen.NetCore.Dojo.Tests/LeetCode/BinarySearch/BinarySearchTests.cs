f041df71490c0d1bd985dda37ccea71ea75eb0ca
Marsen <thisismysoul@gmail.com>
2022-10-22 15:43:34 +0800

    remove useless test case

--- test/Marsen.NetCore.Dojo.Tests/LeetCode/BinarySearch/BinarySearchTests.cs
+++ test/Marsen.NetCore.Dojo.Tests/LeetCode/BinarySearch/BinarySearchTests.cs
@@ -1,3 +1,4 @@
+using System;
 using FluentAssertions;
 using Marsen.NetCore.Dojo.LeetCode.BinarySearch;
 using Xunit;
@@ -11,12 +12,7 @@ public class BinarySearchTests
     [Fact]
     public void You_Got_Minus_1_with_Empty_Array()
     {
-        _sol.Search(new int[] { }, 1).Should().Be(-1);
+        _sol.Search(Array.Empty<int>(), 1).Should().Be(-1);
     }
 
-    [Fact]
-    public void You_Got_0_When_Looking_4_in_Array_4()
-    {
-        _sol.Search(new int[] {4}, 4).Should().Be(0);
-    }
 }
