07e91c1bbd93fcab2be972642456d9a284575f80
Marsen <thisismysoul@gmail.com>
2022-10-30 18:34:53 +0800

    refactor

--- src/Marsen.NetCore.Dojo/LeetCode/BinarySearch/Solution.cs
+++ src/Marsen.NetCore.Dojo/LeetCode/BinarySearch/Solution.cs
@@ -4,26 +4,20 @@ public class Solution
 {
     public int Search(int[] nums, int target)
     {
-        int left = 0;
-        int right = nums.Length - 1;
+        int left = 0, right = nums.Length - 1;
         while (right >= left)
         {
-            int mid = (right - left) / 2 + left;
+            var mid = (right - left) / 2 + left;
             if (nums[mid] == target)
-            {
                 return mid;
-            }
 
             if (nums[mid] < target)
-            {
                 left = mid + 1;
-            }
 
             if (nums[mid] > target)
-            {
                 right = mid - 1;
-            }
         }
+
         return -1;
     }
 }
