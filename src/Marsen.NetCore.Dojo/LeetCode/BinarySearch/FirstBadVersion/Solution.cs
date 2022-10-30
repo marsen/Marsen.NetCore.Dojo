64fb01ce75d3e6ab8d794e643c10837f577e2e76
Marsen <thisismysoul@gmail.com>
2022-10-30 16:04:04 +0800

    add test case from right side

--- src/Marsen.NetCore.Dojo/LeetCode/BinarySearch/FirstBadVersion/Solution.cs
+++ src/Marsen.NetCore.Dojo/LeetCode/BinarySearch/FirstBadVersion/Solution.cs
@@ -6,6 +6,20 @@ public abstract class Solution : VersionControl
 {
     public int FirstBadVersion(int n)
     {
-        return IsBadVersion(n) ? 1 : 0;
+        int left = 1, right = n;
+        while (left < right)
+        {
+            int mid = (left + right) / 2;
+            if (IsBadVersion(mid))
+            {
+                right = mid;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+
+        return left;
     }
 }
--- test/Marsen.NetCore.Dojo.Tests/LeetCode/BinarySearch/FirstBadVersion/FirstBadVersionTests.cs
+++ test/Marsen.NetCore.Dojo.Tests/LeetCode/BinarySearch/FirstBadVersion/FirstBadVersionTests.cs
@@ -11,11 +11,19 @@ public class FirstBadVersionTests
 
     [Theory]
     [InlineData(1, 1)]
-    //[InlineData(2, 1)]
-    //[InlineData(3, 1)]
+    [InlineData(2, 1)]
+    [InlineData(3, 1)]
     public void The_1_Version_is_Bad_Version(int input, int output)
     {
         _sol.IsBadVersion(Arg.Is<int>(x => x >= 1)).Returns(true);
+        _sol.FirstBadVersion(input).Should().Be(output);
+    }
+
+    [Theory]
+    [InlineData(3, 3)]
+    public void The_3_Version_is_Bad_Version(int input, int output)
+    {
+        _sol.IsBadVersion(Arg.Is<int>(x => x >= 3)).Returns(true);
         _sol.FirstBadVersion(input).Should().Be(output);
     }
 }
