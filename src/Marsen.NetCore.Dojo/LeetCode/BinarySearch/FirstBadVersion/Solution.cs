095f38a27633dc8f9d5ba66d547a0202714e0815
Marsen <thisismysoul@gmail.com>
2022-10-30 17:58:56 +0800

    refactor

--- src/Marsen.NetCore.Dojo/LeetCode/BinarySearch/FirstBadVersion/Solution.cs
+++ src/Marsen.NetCore.Dojo/LeetCode/BinarySearch/FirstBadVersion/Solution.cs
@@ -9,15 +9,11 @@ public abstract class Solution : VersionControl
         int left = 1, right = n;
         while (left < right)
         {
-            int mid = left + (right - left) / 2;
+            var mid = left + (right - left) / 2;
             if (IsBadVersion(mid))
-            {
                 right = mid;
-            }
             else
-            {
                 left = mid + 1;
-            }
         }
 
         return left;
