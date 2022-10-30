a70f45155ec900d0470fbb32a480be47915a1a8e
Marsen <thisismysoul@gmail.com>
2022-10-31 01:31:18 +0800

    get green

--- src/Marsen.NetCore.Dojo/LeetCode/BinarySearch/SearchInsertPosition/Solution.cs
+++ src/Marsen.NetCore.Dojo/LeetCode/BinarySearch/SearchInsertPosition/Solution.cs
@@ -4,6 +4,14 @@ public class Solution
 {
     public int SearchInsert(int[] nums, int target)
     {
+        int left = 0, right = nums.Length;
+        while (right > left)
+        {
+            var mid = left + (right - left) / 2;
+            if (nums[mid] < target)
+                return 1;
+        }
+
         return 0;
     }
 }
