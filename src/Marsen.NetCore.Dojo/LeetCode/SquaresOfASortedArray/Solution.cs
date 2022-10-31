b168a66a0efa7de9149dbbf6f1444a23feb64b96
Marsen <thisismysoul@gmail.com>
2022-11-01 00:51:29 +0800

    fix with two points

--- src/Marsen.NetCore.Dojo/LeetCode/SquaresOfASortedArray/Solution.cs
+++ src/Marsen.NetCore.Dojo/LeetCode/SquaresOfASortedArray/Solution.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 
 namespace Marsen.NetCore.Dojo.LeetCode.SquaresOfASortedArray;
@@ -6,6 +7,24 @@ public class Solution
 {
     public int[] SortedSquares(int[] nums)
     {
-        return nums.Select(n => n * n).OrderBy(x => x).ToArray();
+        var result = new int[nums.Length];
+        int left = 0, right = nums.Length - 1, end = nums.Length - 1;
+        while (left <= right)
+        {
+            var sqrtLeft = nums[left] * nums[left];
+            var sqrtRight = nums[right] * nums[right];
+            if (sqrtLeft >= sqrtRight)
+            {
+                result[end--] = sqrtLeft;
+                left++;
+            }
+            else
+            {
+                result[end--] = sqrtRight;
+                right--;
+            }
+        }
+
+        return result;
     }
 }
