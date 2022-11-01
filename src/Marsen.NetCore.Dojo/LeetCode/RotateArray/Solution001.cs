535062367d65c7156623235b15039ce48bd72ece
Marsen <thisismysoul@gmail.com>
2022-11-02 02:57:27 +0800

    remove repeat

--- src/Marsen.NetCore.Dojo/LeetCode/RotateArray/Solution001.cs
+++ src/Marsen.NetCore.Dojo/LeetCode/RotateArray/Solution001.cs
@@ -8,23 +8,11 @@ public class Solution001
     public int[] Rotate(int[] nums, int rotateNum)
     {
         var r = new int[nums.Length];
-        if (rotateNum == 0)
-        {
-            for (int i = 0; i < nums.Length - rotateNum; i++)
-            {
-                r[i] = rotateNum == 0 ? nums[i] : nums[nums.Length - 1 - i];
-                r[rotateNum + i] = nums[i];
-            }
-
-            return r;
-        }
-
         for (int i = 0; i < nums.Length - rotateNum; i++)
         {
             r[i] = rotateNum == 0 ? nums[i] : nums[nums.Length - 1 - i];
             r[rotateNum + i] = nums[i];
         }
-
 
         return r;
     }
