cb3088f9035ff0092959b05283ea485f5a5e7963
Marsen <thisismysoul@gmail.com>
2022-11-04 02:16:49 +0800

    rename as leetcode

--- src/Marsen.NetCore.Dojo/LeetCode/RotateArray/Solution001.cs
+++ src/Marsen.NetCore.Dojo/LeetCode/RotateArray/Solution001.cs
@@ -4,17 +4,17 @@ namespace Marsen.NetCore.Dojo.LeetCode.RotateArray;
 
 public class Solution001
 {
-    public void Rotate(int[] nums, int rotateNum)
+    public void Rotate(int[] nums, int k)
     {
         var r = new int[nums.Length];
-        for (var i = 0; i < nums.Length - rotateNum; i++)
+        for (var i = 0; i < nums.Length - k; i++)
         {
-            r[i] = rotateNum == 0 ? nums[i] : nums[nums.Length - 1 - i];
+            r[i] = k == 0 ? nums[i] : nums[nums.Length - 1 - i];
         }
 
         for (var i = nums.Length - 1; i > 0; i--)
         {
-            r[i] = nums[i-rotateNum];
+            r[i] = nums[i-k];
         }
 
 
