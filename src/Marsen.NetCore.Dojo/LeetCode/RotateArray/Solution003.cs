6ed12e47e8ff7850a3e86eb4ebdea2daa84bd30a
Marsen <thisismysoul@gmail.com>
2022-11-06 02:33:58 +0800

    refactor

--- src/Marsen.NetCore.Dojo/LeetCode/RotateArray/Solution003.cs
+++ src/Marsen.NetCore.Dojo/LeetCode/RotateArray/Solution003.cs
@@ -7,12 +7,12 @@ internal class Solution003
         if (nums == null || nums.Length == 0 || k == 0)
             return;
 
-        int n = nums.Length;
-        int[] tmp = new int[n];
-        for (int i = 0; i < n; i++)
+        var n = nums.Length;
+        var tmp = new int[n];
+        for (var i = 0; i < n; i++)
             tmp[(i + k) % n] = nums[i];
 
-        for (int i = 0; i < n; i++)
+        for (var i = 0; i < n; i++)
             nums[i] = tmp[i];
     }
 }
