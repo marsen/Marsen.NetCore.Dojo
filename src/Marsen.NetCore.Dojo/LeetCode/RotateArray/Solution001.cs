884768c7973b9bc628781c9cca7426cba2c00395
Marsen <thisismysoul@gmail.com>
2022-11-06 01:53:01 +0800

    pass

--- src/Marsen.NetCore.Dojo/LeetCode/RotateArray/Solution001.cs
+++ src/Marsen.NetCore.Dojo/LeetCode/RotateArray/Solution001.cs
@@ -6,15 +6,16 @@ public class Solution001
 {
     public void Rotate(int[] nums, int k)
     {
+        var rotateTimes = nums.Length == 0 ? k : k % nums.Length;
         var r = new int[nums.Length];
-        for (var i = 0; i < k; i++)
+        for (var i = 0; i < rotateTimes; i++)
         {
-            r[i] = k == 0 ? nums[i] : nums[nums.Length - k + i];
+            r[i] = rotateTimes == 0 ? nums[i] : nums[nums.Length - rotateTimes + i];
         }
 
-        for (var i = nums.Length - 1; i >= k; i--)
+        for (var i = nums.Length - 1; i >= rotateTimes; i--)
         {
-            r[i] = nums[i - k];
+            r[i] = nums[i - rotateTimes];
         }
 
         Array.Copy(r, nums, nums.Length);
