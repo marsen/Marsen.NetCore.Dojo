ab1df96e73eaa0fe1c18da0af5f39674304979d2
Marsen <thisismysoul@gmail.com>
2022-11-13 18:19:22 +0800

    pass

--- src/Marsen.NetCore.Dojo/LeetCode/MinimumAbsoluteDifference/Solution.cs
+++ src/Marsen.NetCore.Dojo/LeetCode/MinimumAbsoluteDifference/Solution.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -10,8 +11,16 @@ public class Solution
         var result = new List<IList<int>>();
         var list = arr.ToList();
         list.Sort();
+        var min = Int32.MaxValue;
         for (var i = 0; i < list.Count - 1; i++)
         {
+            var diff = list[i + 1] - list[i];
+            if (diff < min)
+            {
+                min = diff;
+                result = new List<IList<int>>();
+            }
+
             result.Add(new List<int> { list[i], list[i + 1] });
         }
 
