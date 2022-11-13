2a2e9860a578f141800aa1e526a4b23b126d9cd5
Marsen <thisismysoul@gmail.com>
2022-11-13 17:27:26 +0800

    pass

--- test/Marsen.NetCore.Dojo.Tests/LeetCode/MinimumAbsoluteDifference/SolutionTests.cs
+++ test/Marsen.NetCore.Dojo.Tests/LeetCode/MinimumAbsoluteDifference/SolutionTests.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using FluentAssertions;
 using Xunit;
 
@@ -15,12 +16,28 @@ public class SolutionTest
         _sol.MinimumAbsDifference(Array.Empty<int>())
             .Should().BeEquivalentTo(new List<List<int>>());
     }
+
+    [Fact]
+    public void Array_1_2_Return_1_2()
+    {
+        _sol.MinimumAbsDifference(new[] { 1, 2 })
+            .Should().BeEquivalentTo(new List<List<int>>
+            {
+                new() { 1, 2 }
+            });
+    }
 }
 
 public class Solution
 {
     public IList<IList<int>> MinimumAbsDifference(int[] arr)
     {
-        return new List<IList<int>>();
+        var result = new List<IList<int>>();
+        if (arr.Length > 0)
+        {
+            return new List<IList<int>> { arr.ToList() };
+        }
+
+        return result;
     }
 }
