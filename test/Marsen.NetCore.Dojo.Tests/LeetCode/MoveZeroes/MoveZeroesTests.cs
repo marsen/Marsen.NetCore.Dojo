8b3d3c3a8df69eb1a0ee56c759035c95b3a34f30
Marsen <thisismysoul@gmail.com>
2022-11-14 19:29:44 +0800

    add more case to test

--- test/Marsen.NetCore.Dojo.Tests/LeetCode/MoveZeroes/MoveZeroesTests.cs
+++ test/Marsen.NetCore.Dojo.Tests/LeetCode/MoveZeroes/MoveZeroesTests.cs
@@ -35,4 +35,18 @@ public class MoveZeroesTests
         _sol.MoveZeroes(new[] {3, 1, 2})
             .Should().Equal(new[] {3, 1, 2});
     }
+
+    [Fact]
+    public void When_3_0_2_Get_3_2_0()
+    {
+        _sol.MoveZeroes(new[] {3, 0, 2})
+            .Should().Equal(new[] {3, 2, 0});
+    }
+
+    [Fact]
+    public void When_0_1_0_3_12_Get_1_3_12_0_0()
+    {
+        _sol.MoveZeroes(new[] {0, 1, 0, 3, 12})
+            .Should().Equal(new[] {1, 3, 12, 0, 0});
+    }
 }
