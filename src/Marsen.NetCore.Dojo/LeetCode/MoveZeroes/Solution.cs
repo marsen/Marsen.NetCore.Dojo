3b7af7e8f29a339a7f312398223823fb10d976ec
Marsen <thisismysoul@gmail.com>
2022-11-17 01:27:21 +0800

    refactor

--- src/Marsen.NetCore.Dojo/LeetCode/MoveZeroes/Solution.cs
+++ src/Marsen.NetCore.Dojo/LeetCode/MoveZeroes/Solution.cs
@@ -6,23 +6,20 @@ public class Solution
 {
     public void MoveZeroes(int[] nums)
     {
-        var nonZeroIdx = 0;
-        var zeroIdx = 0;
-        for (int i = 0; i < nums.Length; i++)
+        int nonZeroIdx = 0, zeroIdx = 0;
+        for (var i = 0; i < nums.Length; i++)
         {
             if (nums[i] == 0)
-            {
                 zeroIdx++;
-            }
-
-            if (nums[i] != 0)
+            else
             {
                 nums[nonZeroIdx] = nums[i];
                 nonZeroIdx++;
             }
         }
 
-        for (int i = 0; i < zeroIdx; i++)
+        //fill 0
+        for (var i = 0; i < zeroIdx; i++)
         {
             nums[i + nonZeroIdx] = 0;
         }
