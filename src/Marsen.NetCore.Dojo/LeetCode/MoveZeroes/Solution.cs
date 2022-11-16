604eca139d4d1229268139e27072821810ca75b5
Marsen <thisismysoul@gmail.com>
2022-11-17 00:04:23 +0800

    change method signature

--- src/Marsen.NetCore.Dojo/LeetCode/MoveZeroes/Solution.cs
+++ src/Marsen.NetCore.Dojo/LeetCode/MoveZeroes/Solution.cs
@@ -4,24 +4,27 @@ namespace Marsen.NetCore.Dojo.LeetCode.MoveZeroes;
 
 public class Solution
 {
-    public int[] MoveZeroes(int[] nums)
+    public void MoveZeroes(int[] nums)
     {
         var result = new int[nums.Length];
         int j = nums.Length - 1;
         int k = 0;
-        for (int i = 0; i < nums.Length; i++)
+        foreach (var t in nums)
         {
-            if (nums[i] == 0)
+            if (t == 0)
             {
                 result[j] = 0;
             }
             else
             {
-                result[k] = nums[i];
+                result[k] = t;
                 k++;
             }
         }
 
-        return result;
+        for (var i = 0; i < result.Length; i++)
+        {
+            nums[i] = result[i];
+        }
     }
 }
--- test/Marsen.NetCore.Dojo.Tests/LeetCode/MoveZeroes/MoveZeroesTests.cs
+++ test/Marsen.NetCore.Dojo.Tests/LeetCode/MoveZeroes/MoveZeroesTests.cs
@@ -11,42 +11,48 @@ public class MoveZeroesTests
     [Fact]
     public void When_0_Get_0()
     {
-        _sol.MoveZeroes(new[] {0})
-            .Should().Equal(new[] {0});
+        var nums = new[] {0};
+        _sol.MoveZeroes(new[] {0});
+        nums.Should().Equal(new[] {0});
     }
 
     [Fact]
     public void When_1_0_Get_1_0()
     {
-        _sol.MoveZeroes(new[] {1, 0})
-            .Should().Equal(new[] {1, 0});
+        var nums = new[] {1, 0};
+        _sol.MoveZeroes(nums);
+        nums.Should().Equal(new[] {1, 0});
     }
 
     [Fact]
     public void When_0_1_Get_1_0()
     {
-        _sol.MoveZeroes(new[] {0, 1})
-            .Should().Equal(new[] {1, 0});
+        var nums = new[] {0, 1};
+        _sol.MoveZeroes(nums);
+        nums.Should().Equal(1, 0);
     }
 
     [Fact]
     public void When_3_1_2_Get_3_1_2()
     {
-        _sol.MoveZeroes(new[] {3, 1, 2})
-            .Should().Equal(new[] {3, 1, 2});
+        var nums = new[] {3, 1, 2};
+        _sol.MoveZeroes(nums);
+        nums.Should().Equal(new[] {3, 1, 2});
     }
 
     [Fact]
     public void When_3_0_2_Get_3_2_0()
     {
-        _sol.MoveZeroes(new[] {3, 0, 2})
-            .Should().Equal(new[] {3, 2, 0});
+        var nums = new[] {3, 0, 2};
+        _sol.MoveZeroes(nums);
+        nums.Should().Equal(new[] {3, 2, 0});
     }
 
     [Fact]
     public void When_0_1_0_3_12_Get_1_3_12_0_0()
     {
-        _sol.MoveZeroes(new[] {0, 1, 0, 3, 12})
-            .Should().Equal(new[] {1, 3, 12, 0, 0});
+        var nums = new[] {0, 1, 0, 3, 12};
+        _sol.MoveZeroes(nums);
+        nums.Should().Equal(1, 3, 12, 0, 0);
     }
 }
