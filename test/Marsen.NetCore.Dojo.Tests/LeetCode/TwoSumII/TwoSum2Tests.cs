77db108f6ba6d9c0691bbc74ce1f477386718da0
Marsen <thisismysoul@gmail.com>
2022-11-18 03:14:17 +0800

    add test case

---
+++ test/Marsen.NetCore.Dojo.Tests/LeetCode/TwoSumII/TwoSum2Tests.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace Marsen.NetCore.Dojo.Tests.LeetCode.TwoSumII;
+
+public class TwoSum2Tests
+{
+    [Fact]
+    public void METHOD()
+    {
+        throw new NotImplementedException();
+    }
+}
