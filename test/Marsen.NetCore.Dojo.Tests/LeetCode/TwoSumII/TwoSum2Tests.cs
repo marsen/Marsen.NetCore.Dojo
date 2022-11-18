781230275ab6ba37b5c0a70c4ffabdf259068ef4
Marsen <thisismysoul@gmail.com>
2022-11-19 02:35:04 +0800

    1 2 target 3 get 1 2 passed

--- test/Marsen.NetCore.Dojo.Tests/LeetCode/TwoSumII/TwoSum2Tests.cs
+++ test/Marsen.NetCore.Dojo.Tests/LeetCode/TwoSumII/TwoSum2Tests.cs
@@ -1,4 +1,4 @@
-using System;
+using FluentAssertions;
 using Xunit;
 
 namespace Marsen.NetCore.Dojo.Tests.LeetCode.TwoSumII;
@@ -6,8 +6,18 @@ namespace Marsen.NetCore.Dojo.Tests.LeetCode.TwoSumII;
 public class TwoSum2Tests
 {
     [Fact]
-    public void METHOD()
+    public void Given_1_2_target_3_return_1_2()
     {
-        throw new NotImplementedException();
+        var sol = new Solution();
+        int[] result = sol.TwoSum(new[] { 1, 2 }, 3);
+        result.Should().Equal(1, 2);
+    }
+}
+
+public class Solution
+{
+    public int[] TwoSum(int[] numbers, int target)
+    {
+        return new[] { 1, 2 };
     }
 }
