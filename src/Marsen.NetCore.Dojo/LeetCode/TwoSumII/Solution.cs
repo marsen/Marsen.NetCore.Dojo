d93103c17b5c1d540dedb4d8b5d6648346a8069c
Marsen <thisismysoul@gmail.com>
2022-11-19 02:36:20 +0800

    refactor

---
+++ src/Marsen.NetCore.Dojo/LeetCode/TwoSumII/Solution.cs
@@ -0,0 +1,9 @@
+namespace Marsen.NetCore.Dojo.LeetCode.TwoSumII;
+
+public class Solution
+{
+    public int[] TwoSum(int[] numbers, int target)
+    {
+        return new[] { 1, 2 };
+    }
+}
--- test/Marsen.NetCore.Dojo.Tests/LeetCode/TwoSumII/TwoSum2Tests.cs
+++ test/Marsen.NetCore.Dojo.Tests/LeetCode/TwoSumII/TwoSum2Tests.cs
@@ -1,23 +1,16 @@
 using FluentAssertions;
+using Marsen.NetCore.Dojo.LeetCode.TwoSumII;
 using Xunit;
 
 namespace Marsen.NetCore.Dojo.Tests.LeetCode.TwoSumII;
 
 public class TwoSum2Tests
 {
+    readonly Solution _sol = new();
+
     [Fact]
     public void Given_1_2_target_3_return_1_2()
     {
-        var sol = new Solution();
-        int[] result = sol.TwoSum(new[] { 1, 2 }, 3);
-        result.Should().Equal(1, 2);
-    }
-}
-
-public class Solution
-{
-    public int[] TwoSum(int[] numbers, int target)
-    {
-        return new[] { 1, 2 };
+        _sol.TwoSum(new[] { 1, 2 }, 3).Should().Equal(1, 2);
     }
 }
