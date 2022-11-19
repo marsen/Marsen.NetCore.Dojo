c6c44b344957c1dc533e1be3e4213a1e4af89b39
Marsen <thisismysoul@gmail.com>
2022-11-19 16:37:12 +0800

    remove index1 and got and magic number 1

--- src/Marsen.NetCore.Dojo/LeetCode/TwoSumII/Solution.cs
+++ src/Marsen.NetCore.Dojo/LeetCode/TwoSumII/Solution.cs
@@ -5,12 +5,11 @@ public class Solution
     public int[] TwoSum(int[] numbers, int target)
     {
         var result = new int[2];
-        int index1 = 1, index2 = 2;
         for (int i = 0; i < numbers.Length - 1; i++)
         {
-            if (numbers[index1 - 1] + numbers[index2 - 1 + i] == target)
+            if (numbers[1 - 1] + numbers[i + 1] == target)
             {
-                result = new[] { index1, index2 + i };
+                result = new[] {1, i + 2};
             }
         }
 
