7147825d1952c7bbd71c53681efc6160b362fa51
Marsen <thisismysoul@gmail.com>
2022-11-26 22:00:47 +0800

    refactor with ToHashSet method

--- src/Marsen.NetCore.Dojo/LeetCode/LongestConsecutiveSequence/Solution.cs
+++ src/Marsen.NetCore.Dojo/LeetCode/LongestConsecutiveSequence/Solution.cs
@@ -1,5 +1,5 @@
 using System;
-using System.Collections.Generic;
+using System.Linq;
 
 namespace Marsen.NetCore.Dojo.LeetCode.LongestConsecutiveSequence;
 
@@ -7,48 +7,20 @@ public class Solution
 {
     public int LongestConsecutive(int[] nums)
     {
-        var hashSet = new HashSet<int>();
-        foreach (var num in nums)
-        {
-            if (!hashSet.Contains(num))
-                hashSet.Add(num);
-        }
-
-        var max = 0;
+        var hashSet = nums.ToHashSet();
+        var result = 0;
         foreach (var num in hashSet)
         {
-            if (!hashSet.Contains(num + 1))
+            if (hashSet.Contains(num + 1)) continue;
+            var length = 0;
+            var number = num;
+            while (hashSet.Contains(number))
             {
-                var length = 0;
-                var number = num;
-                while (hashSet.Contains(number))
-                {
-                    length++;
-                    number--;
-                }
-
-                max = Math.Max(max, length);
+                length++;
+                number--;
             }
-        }
 
-        return max;
-        var result = nums.Length == 0 ? 0 : 1;
-        var current = result;
-        Array.Sort(nums);
-        for (var i = 0; i < nums.Length - 1; i++)
-        {
-            switch (nums[i + 1] - nums[i])
-            {
-                case 0:
-                    continue;
-                case 1:
-                    current++;
-                    if (current > result) result = current;
-                    break;
-                default:
-                    current = 1;
-                    break;
-            }
+            result = Math.Max(result, length);
         }
 
         return result;
