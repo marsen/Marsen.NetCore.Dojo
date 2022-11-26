69d9fba21c727f07e22bb04b8f629c43e2a0c357
Marsen <thisismysoul@gmail.com>
2022-11-26 14:36:25 +0800

    passed 1 2 4 should be 2

--- test/Marsen.NetCore.Dojo.Tests/LeetCode/LongestConsecutiveSequence/LongestConsecutiveSequenceTests.cs
+++ test/Marsen.NetCore.Dojo.Tests/LeetCode/LongestConsecutiveSequence/LongestConsecutiveSequenceTests.cs
@@ -20,16 +20,24 @@ public class LongestConsecutiveSequenceTests
         _sol.LongestConsecutive(new[] { 1, 2 })
             .Should().Be(2);
     }
+
+    [Fact]
+    public void Given_1_2_4_Should_Get_2()
+    {
+        _sol.LongestConsecutive(new[] { 1, 2, 4 })
+            .Should().Be(2);
+    }
 }
 
 public class Solution
 {
     public int LongestConsecutive(int[] nums)
     {
-        var result = 0;
-        for (int i = 0; i < nums.Length; i++)
+        var result = 1;
+        for (int i = 0; i < nums.Length -1 ; i++)
         {
-            result++;
+            if (nums[i + 1] - nums[i] == 1)
+                result++;
         }
 
         return result;
