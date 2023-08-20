349a5504350cfb6f0929fedb15de8c3e4a0b75fb
Marsen <thisismysoul@gmail.com>
2023-08-20 13:56:46 +0800

    add word dictionary

--- src/Marsen.NetCore.Dojo/LeetCode/MinimumAbsoluteDifference/Solution.cs
+++ src/Marsen.NetCore.Dojo/LeetCode/MinimumAbsoluteDifference/Solution.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -6,20 +5,19 @@ namespace Marsen.NetCore.Dojo.LeetCode.MinimumAbsoluteDifference;
 
 public class Solution
 {
-    public IList<IList<int>> MinimumAbsDifference(int[] arr)
+    public IEnumerable<IList<int>> MinimumAbsDifference(int[] arr)
     {
         var result = new List<IList<int>>();
         var list = arr.ToList();
         list.Sort();
-        var min = Int32.MaxValue;
+        var min = int.MaxValue;
         for (var i = 0; i < list.Count - 1; i++)
         {
             var diff = list[i + 1] - list[i];
             if (diff < min)
             {
                 min = diff;
-                result = new List<IList<int>>();
-                result.Add(new List<int> {list[i], list[i + 1]});
+                result = new List<IList<int>> { new List<int> {list[i], list[i + 1]} };
             }
             else if (diff == min)
                 result.Add(new List<int> {list[i], list[i + 1]});
