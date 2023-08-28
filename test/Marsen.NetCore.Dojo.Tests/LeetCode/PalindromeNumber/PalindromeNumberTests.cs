be0517c94a583c1ab79f049f4a21bae3f0f94417
Marsen <admin@marsen.me>
2023-08-28 17:41:51 +0800

    refactor

--- test/Marsen.NetCore.Dojo.Tests/LeetCode/PalindromeNumber/PalindromeNumberTests.cs
+++ test/Marsen.NetCore.Dojo.Tests/LeetCode/PalindromeNumber/PalindromeNumberTests.cs
@@ -5,14 +5,11 @@ namespace Marsen.NetCore.Dojo.Tests.LeetCode.PalindromeNumber;
 
 public class PalindromeNumberTests
 {
+    readonly Solution _sol = new ();
     [Fact]
     public void Give_1_Should_Be_True()
     {
-        // Arrange
-        var target = new Solution();
-        // Act
-        bool result = target.IsPalindrome(1);
-        // Assert
-        Assert.True(result);
+        // Arrange, Act, Assert
+        Assert.True(_sol.IsPalindrome(1));
     }
 }
