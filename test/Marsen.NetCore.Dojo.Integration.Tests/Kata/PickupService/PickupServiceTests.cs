ceb7d81b2e23e9effadb1dcb4ece5e37ab14e0ab
marsen <admin@marsen.me>
2025-01-16 17:46:58 +0800

    舊的 url 已經失效，重建測試用的 mock api

--- test/Marsen.NetCore.Dojo.Integration.Tests/Kata/PickupService/PickupServiceTests.cs
+++ test/Marsen.NetCore.Dojo.Integration.Tests/Kata/PickupService/PickupServiceTests.cs
@@ -15,14 +15,15 @@ namespace Marsen.NetCore.Dojo.Integration.Tests.Kata.PickupService
         private readonly IConfigService _configService;
         private Dojo.Kata.PickupService.Services.PickupService _target;
         private const long TestStoreId = 1;
-        private readonly List<string> _testWaybillNo = new() {"TEST2002181800010"};
-        private const string MockDone = "https://demo3287250.mockable.io/DONE";
-        private const string MockShipping = "https://demo3287250.mockable.io/Shipping";
-        private const string MockFail = "https://demo3287250.mockable.io/FAIL";
-        private const string MockExpiry = "https://demo3287250.mockable.io/Expiry";
-        private const string MockArrived = "https://demo3287250.mockable.io/Arrived";
-        private const string MockResultError = "https://demo3287250.mockable.io/Error";
-        private const string MockContentError = "https://demo3287250.mockable.io/ContentError";
+        // your can get mock response from readme3.md
+        private readonly List<string> _testWaybillNo = ["TEST2002181800010"];
+        private const string MockDone = "https://run.mocky.io/v3/78b1a1a1-73df-438f-bdc2-a9dc44eb831d";
+        private const string MockShipping = "https://run.mocky.io/v3/9b45256d-9a97-4c1b-a47d-02d48936f430";
+        private const string MockFail = "https://run.mocky.io/v3/08f3a00d-422c-4857-8013-461aa83e6fe5";
+        private const string MockExpiry = "https://run.mocky.io/v3/eebd3428-ba81-446b-aa12-50b0911025c7";
+        private const string MockArrived = "https://run.mocky.io/v3/d573611f-f313-463f-b1f9-962ff997de6c";
+        private const string MockResultError = "https://run.mocky.io/v3/e5d70d4a-952e-44cd-872a-83d67df0f27c";
+        private const string MockContentError = "https://run.mocky.io/v3/92e7d9d4-9890-417a-9d02-ddf373a4a3c2";
         private const string MockException = "https://demo3287250.mockable.io/Exception";
         private readonly IStoreSettingService _storeSettingService;
         private readonly ILogger _logger;
@@ -81,7 +82,7 @@ namespace Marsen.NetCore.Dojo.Integration.Tests.Kata.PickupService
             GetPickupServiceWith(MockResultError);
             Action act = () => _target.GetUpdateStatus(TestStoreId, _testWaybillNo);
             act.Should().Throw<Exception>();
-            _logger.ReceivedWithAnyArgs().LogError(default);
+            _logger.ReceivedWithAnyArgs().LogError(message: null);
         }
 
 
@@ -99,7 +100,7 @@ namespace Marsen.NetCore.Dojo.Integration.Tests.Kata.PickupService
             GetPickupServiceWith(MockException);
             Action act = () => _target.GetUpdateStatus(TestStoreId, _testWaybillNo);
             act.Should().Throw<Exception>();
-            _logger.ReceivedWithAnyArgs().LogError(default);
+            _logger.ReceivedWithAnyArgs().LogError(message: null);
         }
 
         private void GetPickupServiceWith(string url)
